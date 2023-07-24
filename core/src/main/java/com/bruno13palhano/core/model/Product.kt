cc727761bb6ad863f2eb5c23198d7b70681e16d0
bruno13palhano <bruno13palhano@gmail.com>
2023-07-24 03:14:04 -0300

    Add product external model

---
+++ core/src/main/java/com/bruno13palhano/core/model/Product.kt
@@ -0,0 +1,25 @@
+package com.bruno13palhano.core.model
+
+/**
+* Product model.
+*
+* A class to model a product.
+* @property id product's id.
+* @property name product's name.
+* @property description product's description.
+* @property company the company that produces the product.
+* @property purchasePrice product purchase price.
+* @property salePrice product sale price.
+* @property isPaid defines if the product is paid.
+* @property isSold defines if the product is sold.
+*/
+data class Product(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val company: String,
+    val purchasePrice: Float,
+    val salePrice: Float,
+    val isPaid: Boolean,
+    val isSold: Boolean
+)
