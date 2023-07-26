9237a2509b2d57579830f8eb199e0e31961736fd
bruno13palhano <bruno13palhano@gmail.com>
2023-07-25 21:32:16 -0300

    Add Main preview

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 import com.bruno13palhano.shopdanimanagement.ui.theme.components.DrawerMenu
@@ -40,6 +42,34 @@ class MainActivity : ComponentActivity() {
                                 drawerState.open()
                             }
                         }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainPreview() {
+    ShopDaniManagementTheme {
+        val navController = rememberNavController()
+        val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            DrawerMenu(
+                navController = navController,
+                drawerState = drawerState
+            ) {
+                val coroutineScope = rememberCoroutineScope()
+                MainNavGraph(
+                    navController = navController
+                ) {
+                    coroutineScope.launch {
+                        drawerState.open()
                     }
                 }
             }
