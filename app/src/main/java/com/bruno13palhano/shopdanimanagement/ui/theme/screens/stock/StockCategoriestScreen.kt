4be2e2f77b54a36fdd2a002d625b73caa68b9e80
bruno13palhano <bruno13palhano@gmail.com>
2023-07-25 22:43:20 -0300

    Add cateroy id

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/theme/screens/stock/StockCategoriestScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/theme/screens/stock/StockCategoriestScreen.kt
@@ -30,9 +30,12 @@ import com.bruno13palhano.shopdanimanagement.ui.theme.components.StockItem
 
 @Composable
 fun StockCategoriesScreen(
+    onItemClick: (route: String) -> Unit,
     navigateBack: () -> Unit,
 ) {
     val categories = listOf(
+        StockCategory.Gits,
+        StockCategory.Infant,
         StockCategory.Perfumes,
         StockCategory.Soaps,
         StockCategory.AntiperspirantDeodorants,
@@ -47,7 +50,8 @@ fun StockCategoriesScreen(
 
     StockCategoriesContent(
         navigateBack = navigateBack,
-        categories = categories
+        categories = categories,
+        onItemClick = onItemClick
     )
 }
 
@@ -55,12 +59,13 @@ fun StockCategoriesScreen(
 @Composable
 private fun StockCategoriesContent(
     categories: List<StockCategory>,
+    onItemClick: (route: String) -> Unit,
     navigateBack: () -> Unit
 ) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.stock_list_label)) },
+                title = { Text(text = stringResource(id = R.string.stock_categories_label)) },
                 navigationIcon = {
                     IconButton(onClick = navigateBack) {
                         Icon(
@@ -78,7 +83,7 @@ private fun StockCategoriesContent(
         ) {
             items(categories) { stockCategory ->
                 StockItem(category = stringResource(id = stockCategory.category)) {
-
+                    onItemClick(stockCategory.categoryId)
                 }
             }
         }
@@ -95,6 +100,8 @@ private fun StockCategoriesListPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             val categories = listOf(
+                StockCategory.Gits,
+                StockCategory.Infant,
                 StockCategory.Perfumes,
                 StockCategory.Soaps,
                 StockCategory.AntiperspirantDeodorants,
@@ -109,21 +116,39 @@ private fun StockCategoriesListPreview() {
 
             StockCategoriesContent(
                 navigateBack = {},
-                categories = categories
+                categories = categories,
+                onItemClick = {}
             )
         }
     }
 }
 
-sealed class StockCategory(@StringRes val category: Int, val icon: ImageVector) {
-    object Perfumes: StockCategory(R.string.perfumes_label, Icons.Filled.Image)
-    object Soaps: StockCategory(R.string.soaps_label, Icons.Filled.Image)
-    object AntiperspirantDeodorants: StockCategory(R.string.antiperspirant_deodorants_label, Icons.Filled.Image)
-    object DeodorantsCologne: StockCategory(R.string.deodorants_cologne_label, Icons.Filled.Image)
-    object Moisturizers: StockCategory(R.string.moisturizers_label, Icons.Filled.Image)
-    object Sunscreens: StockCategory(R.string.sunscreens_label, Icons.Filled.Image)
-    object Makeup: StockCategory(R.string.makeup_label, Icons.Filled.Image)
-    object Face: StockCategory(R.string.face_label, Icons.Filled.Image)
-    object Skin: StockCategory(R.string.skin_label, Icons.Filled.Image)
-    object Hair: StockCategory(R.string.hair_label, Icons.Filled.Image)
+sealed class StockCategory(val categoryId: String, @StringRes val category: Int, val icon: ImageVector) {
+    object Gits: StockCategory(CategoryId.GIFTS_ID, R.string.gifts_label, Icons.Filled.Image)
+    object Infant: StockCategory(CategoryId.INFANT_ID, R.string.infant_label, Icons.Filled.Image)
+    object Perfumes: StockCategory(CategoryId.PERFUMES_ID, R.string.perfumes_label, Icons.Filled.Image)
+    object Soaps: StockCategory(CategoryId.SOAPS_ID, R.string.soaps_label, Icons.Filled.Image)
+    object AntiperspirantDeodorants: StockCategory(CategoryId.ANTIPERSPIRANT_DEODORANTS_ID, R.string.antiperspirant_deodorants_label, Icons.Filled.Image)
+    object DeodorantsCologne: StockCategory(CategoryId.DEODORANTS_COLOGNE_ID, R.string.deodorants_cologne_label, Icons.Filled.Image)
+    object Moisturizers: StockCategory(CategoryId.MOISTURIZERS_ID, R.string.moisturizers_label, Icons.Filled.Image)
+    object Sunscreens: StockCategory(CategoryId.SUNSCREENS_ID, R.string.sunscreens_label, Icons.Filled.Image)
+    object Makeup: StockCategory(CategoryId.MAKEUP_ID, R.string.makeup_label, Icons.Filled.Image)
+    object Face: StockCategory(CategoryId.FACE_ID, R.string.face_label, Icons.Filled.Image)
+    object Skin: StockCategory(CategoryId.SKIN_ID, R.string.skin_label, Icons.Filled.Image)
+    object Hair: StockCategory(CategoryId.HAIR_ID, R.string.hair_label, Icons.Filled.Image)
+}
+
+object CategoryId {
+    const val GIFTS_ID = "gifts_products"
+    const val INFANT_ID = "infant_products"
+    const val PERFUMES_ID = "perfumes_products"
+    const val SOAPS_ID = "soaps_products"
+    const val ANTIPERSPIRANT_DEODORANTS_ID = "antiperspirant_deodorants_products"
+    const val DEODORANTS_COLOGNE_ID = "deodorants_cologne_products"
+    const val MOISTURIZERS_ID = "moisturizers_products"
+    const val SUNSCREENS_ID = "sunscreens_products"
+    const val MAKEUP_ID = "makeup_products"
+    const val FACE_ID = "face_products"
+    const val SKIN_ID = "skin_products"
+    const val HAIR_ID = "hair_products"
 }
