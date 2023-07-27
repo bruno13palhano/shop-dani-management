61f62a9a733214fbb7022a235af4dc7d2a316e24
bruno13palhano <bruno13palhano@gmail.com>
2023-07-27 16:15:02 -0300

    Add CategoryDao

--- core/src/main/java/com/bruno13palhano/core/data/database/AppDatabase.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/AppDatabase.kt
@@ -3,12 +3,15 @@ package com.bruno13palhano.core.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.bruno13palhano.core.data.database.dao.CategoryDao
 import com.bruno13palhano.core.data.database.dao.ProductDao
+import com.bruno13palhano.core.data.database.model.CategoryEntity
 import com.bruno13palhano.core.data.database.model.ProductEntity
 
 @Database(
     entities = [
-        ProductEntity::class
+        ProductEntity::class,
+        CategoryEntity::class
     ],
     version = 1,
     exportSchema = false
@@ -16,4 +19,5 @@ import com.bruno13palhano.core.data.database.model.ProductEntity
 @TypeConverters(CategoryConverter::class)
 internal abstract class AppDatabase : RoomDatabase() {
     abstract val productDao: ProductDao
+    abstract val categoryDao: CategoryDao
 }
---
+++ core/src/main/java/com/bruno13palhano/core/data/database/dao/CategoryDao.kt
@@ -0,0 +1,72 @@
+package com.bruno13palhano.core.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.bruno13palhano.core.data.DataOperations
+import com.bruno13palhano.core.data.database.model.CategoryEntity
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * [CategoryEntity] Dao interface.
+ *
+ * A Data Access Object for the [CategoryEntity].
+ * This interface is responsible for handling [CategoryEntity] access to Room database.
+ */
+@Dao
+internal interface CategoryDao : DataOperations<CategoryEntity> {
+
+    /**
+     * Inserts a [CategoryEntity] into the database.
+     * @param model the new [CategoryEntity].
+     * @return the id of the new [CategoryEntity].
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    override suspend fun insert(model: CategoryEntity): Long
+
+    /**
+     * Updates the [CategoryEntity] int the database.
+     * @param model the [CategoryEntity] to be updated.
+     */
+    @Update
+    override suspend fun update(model: CategoryEntity)
+
+    /**
+     * Deletes the [CategoryEntity] int the database.
+     * @param model the [CategoryEntity] to be deleted.
+     */
+    @Delete
+    override suspend fun delete(model: CategoryEntity)
+
+    /**
+     * Deletes the [CategoryEntity] specified by this [id].
+     * @param id the [id] for this [CategoryEntity].
+     */
+    @Query("DELETE FROM category_table WHERE id = :id")
+    override suspend fun deleteById(id: Long)
+
+    /**
+     * Gets all [CategoryEntity].
+     * @return a [Flow] containing a [List] of all [CategoryEntity].
+     */
+    @Query("SELECT * FROM category_table")
+    override fun getAll(): Flow<List<CategoryEntity>>
+
+    /**
+     * Gets the [CategoryEntity] specified by this [id].
+     * @param id the [id] for this [CategoryEntity].
+     * @return a [Flow] of [CategoryEntity].
+     */
+    @Query("SELECT * FROM category_table WHERE id = :id")
+    override fun getById(id: Long): Flow<CategoryEntity>
+
+    /**
+     * Gets the last [CategoryEntity] inserted into the database.
+     * @return a [Flow] of [CategoryEntity].
+     */
+    @Query("SELECT * FROM category_table WHERE id = (SELECT max(id) FROM category_table)")
+    override fun getLast(): Flow<CategoryEntity>
+}
