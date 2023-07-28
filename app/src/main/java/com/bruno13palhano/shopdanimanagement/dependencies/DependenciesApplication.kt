8e195dc83b352634b6e45e4e13c6f72a57b25111
bruno13palhano <bruno13palhano@gmail.com>
2023-07-28 00:20:45 -0300

    Add application class

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/dependencies/DependenciesApplication.kt
@@ -0,0 +1,7 @@
+package com.bruno13palhano.shopdanimanagement.dependencies
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DependenciesApplication : Application()
