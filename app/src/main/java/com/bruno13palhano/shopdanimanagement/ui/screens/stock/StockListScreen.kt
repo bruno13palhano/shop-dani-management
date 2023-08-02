dc9862d3ce5d9608213900017a8bea119316f542
bruno13palhano <bruno13palhano@gmail.com>
2023-08-02 00:14:28 -0300

    get items in stock

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/StockListScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/StockListScreen.kt
@@ -1,8 +1,9 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.stock
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -22,25 +24,49 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.bruno13palhano.core.model.Category
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.core.model.Stock
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 import com.bruno13palhano.shopdanimanagement.ui.components.StockItem
+import com.bruno13palhano.shopdanimanagement.ui.screens.stock.viewmodel.StockListViewModel
 
 @Composable
 fun StockListScreen(
     categoryId: String,
     onAddButtonClick: () -> Unit,
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    viewModel: StockListViewModel = hiltViewModel()
 ) {
+    val stockList by viewModel.stock.collectAsStateWithLifecycle()
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getCategory(categoryId.toLong())
+    }
+    var showCategoryDialog by remember { mutableStateOf(false) }
+
     StockListContent(
-        categoryId = categoryId,
-        stockList = emptyList(),
+        categoryId = viewModel.name,
+        showCategoryDialog = showCategoryDialog,
+        stockList = stockList,
+        onCategoryChange = viewModel::updateName,
+        onOkClick = { viewModel.updateCategory(categoryId.toLong()) },
+        onDismissRequest = { showCategoryDialog = false },
+        onEditItemClick = {
+            showCategoryDialog = true
+        },
         onAddButtonClick = onAddButtonClick,
         navigateUp = navigateUp
     )
@@ -50,20 +76,58 @@ fun StockListScreen(
 @Composable
 fun StockListContent(
     categoryId: String,
+    showCategoryDialog: Boolean,
     stockList: List<Stock>,
+    onCategoryChange: (category: String) -> Unit,
+    onOkClick: () -> Unit,
+    onDismissRequest: () -> Unit,
+    onEditItemClick: () -> Unit,
     onAddButtonClick: () -> Unit,
     navigateUp: () -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false) }
+    val menuOptions = arrayOf(
+        stringResource(id = R.string.edit_label)
+    )
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = getTitle(categoryId))) },
+                title = { Text(text = categoryId) },
                 navigationIcon = {
                     IconButton(onClick = navigateUp) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
                             contentDescription = stringResource(id = R.string.up_button_label)
                         )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.more_options_label)
+                            )
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MoreOptionsMenu(
+                                    items = menuOptions,
+                                    expanded = expanded,
+                                    onDismissRequest = { expanded = it },
+                                    onClick = { index ->
+                                        when (index) {
+                                            0 -> {
+                                                onEditItemClick()
+                                            }
+                                        }
+                                    }
+                                )
+                            }
+                        }
                     }
                 }
             )
@@ -89,51 +153,36 @@ fun StockListContent(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(8.dp),
+                    photo = stock.photo,
                     name = stock.name,
-                    price = stock.purchasePrice.toString(),
+                    price = stock.purchasePrice,
                     quantity = stock.quantity,
-                ) {
-
-                }
+                    onClick = {}
+                )
             }
-         }
+        }
+        if (showCategoryDialog) {
+            CategoryDialog(
+                newCategory = categoryId,
+                onCategoryChange = onCategoryChange,
+                onOkClick = onOkClick,
+                onDismissRequest = onDismissRequest
+            )
+        }
     }
 }
 
-@StringRes
-fun getTitle(categoryId: String): Int {
-    return when (categoryId) {
-        Category.GIFTS.category -> R.string.gifts_label
-        Category.INFANT.category -> R.string.infant_label
-        Category.PERFUMES.category -> R.string.perfumes_label
-        Category.SOAPS.category -> R.string.soaps_label
-        Category.ANTIPERSPIRANT_DEODORANTS.category -> R.string.antiperspirant_deodorants_label
-        Category.DEODORANTS_COLOGNE.category -> R.string.deodorants_cologne_label
-        Category.MOISTURIZERS.category -> R.string.moisturizers_label
-        Category.SUNSCREENS.category -> R.string.sunscreens_label
-        Category.MAKEUP.category -> R.string.makeup_label
-        Category.FACE.category -> R.string.face_label
-        Category.SKIN.category -> R.string.skin_label
-        Category.HAIR.category -> R.string.hair_label
-        else -> R.string.stock_product_list
-    }
-}
-
-@Preview(showBackground = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
 @Composable
-private fun ProductListPreview() {
+private fun ProductListDynamicPreview() {
     val items = listOf(
-        Stock(id= 1L, name = "Product 1", purchasePrice = 120.45F, quantity = 12),
-        Stock(id= 2L, name = "Product 2", purchasePrice = 40.33F, quantity = 2),
-        Stock(id= 3L, name = "Product 3", purchasePrice = 99.99F, quantity = 7),
-        Stock(id= 4L, name = "Product 4", purchasePrice = 12.39F, quantity = 2),
-        Stock(id= 5L, name = "Product 5", purchasePrice = 56.78F, quantity = 1),
-        Stock(id= 6L, name = "Product 6", purchasePrice = 12.12F, quantity = 2),
-        Stock(id= 7L, name = "Product 7", purchasePrice = 67.99F, quantity = 4),
-        Stock(id= 8L, name = "Product 8", purchasePrice = 81.90F, quantity = 6),
-        Stock(id= 9L, name = "Product 9", purchasePrice = 77.90F, quantity = 2),
-        Stock(id= 10L, name = "Product 10", purchasePrice = 99.00F, quantity = 5),
+        Stock(id= 1L, name = "Product 1", photo = "", purchasePrice = 120.45F, quantity = 12),
+        Stock(id= 2L, name = "Product 2", photo = "", purchasePrice = 40.33F, quantity = 2),
+        Stock(id= 3L, name = "Product 3", photo = "", purchasePrice = 99.99F, quantity = 7),
+        Stock(id= 4L, name = "Product 4", photo = "", purchasePrice = 12.39F, quantity = 2),
+        Stock(id= 5L, name = "Product 5", photo = "", purchasePrice = 56.78F, quantity = 1),
+        Stock(id= 6L, name = "Product 6", photo = "", purchasePrice = 12.12F, quantity = 2),
     )
 
     ShopDaniManagementTheme {
@@ -142,8 +191,48 @@ private fun ProductListPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             StockListContent(
+                showCategoryDialog = true,
                 categoryId = "Perfume",
                 stockList = items,
+                onCategoryChange = {},
+                onOkClick = {},
+                onDismissRequest = {},
+                onEditItemClick = {},
+                onAddButtonClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+private fun ProductListPreview() {
+    val items = listOf(
+        Stock(id= 1L, name = "Product 1", photo = "", purchasePrice = 120.45F, quantity = 12),
+        Stock(id= 2L, name = "Product 2", photo = "", purchasePrice = 40.33F, quantity = 2),
+        Stock(id= 3L, name = "Product 3", photo = "", purchasePrice = 99.99F, quantity = 7),
+        Stock(id= 4L, name = "Product 4", photo = "", purchasePrice = 12.39F, quantity = 2),
+        Stock(id= 5L, name = "Product 5", photo = "", purchasePrice = 56.78F, quantity = 1),
+        Stock(id= 6L, name = "Product 6", photo = "", purchasePrice = 12.12F, quantity = 2),
+    )
+
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            StockListContent(
+                showCategoryDialog = true,
+                categoryId = "Perfume",
+                stockList = items,
+                onCategoryChange = {},
+                onOkClick = {},
+                onDismissRequest = {},
+                onEditItemClick = {},
                 onAddButtonClick = {},
                 navigateUp = {}
             )
