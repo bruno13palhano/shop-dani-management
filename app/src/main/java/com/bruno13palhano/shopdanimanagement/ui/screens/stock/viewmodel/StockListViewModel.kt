5c54188fc12d41de5744c54a9bdbf28ae249b4a7
bruno13palhano <bruno13palhano@gmail.com>
2023-08-02 00:14:00 -0300

    Get all products

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/viewmodel/StockListViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/viewmodel/StockListViewModel.kt
@@ -7,16 +7,41 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.DataOperations
 import com.bruno13palhano.core.data.di.DefaultCategoryRepository
+import com.bruno13palhano.core.data.di.DefaultProductRepository
 import com.bruno13palhano.core.model.Category
+import com.bruno13palhano.core.model.Product
+import com.bruno13palhano.core.model.Stock
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StockListViewModel @Inject constructor(
-    @DefaultCategoryRepository private val categoryRepository: DataOperations<Category>
+    @DefaultCategoryRepository private val categoryRepository: DataOperations<Category>,
+    @DefaultProductRepository private val productRepository: DataOperations<Product>
 ): ViewModel() {
+    val stock = productRepository.getAll()
+        .map {
+            it.map { product ->
+                Stock(
+                    id = product.id,
+                    name = product.name,
+                    photo = product.photo,
+                    purchasePrice = product.purchasePrice,
+                    quantity = product.quantity
+                )
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
+
     var name by mutableStateOf("")
         private set
 
