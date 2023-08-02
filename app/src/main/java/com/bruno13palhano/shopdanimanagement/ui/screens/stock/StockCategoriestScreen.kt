a56006e0f7dc23406258e86e58bfb54d62710d46
bruno13palhano <bruno13palhano@gmail.com>
2023-08-02 00:15:07 -0300

    Populate StockCategoriesScren

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/StockCategoriestScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/StockCategoriestScreen.kt
@@ -1,65 +1,89 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.stock
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.core.model.Category
 import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 import com.bruno13palhano.shopdanimanagement.ui.components.StockItem
+import com.bruno13palhano.shopdanimanagement.ui.screens.stock.viewmodel.StockCategoriesViewModel
 
 @Composable
 fun StockCategoriesScreen(
     onItemClick: (categoryId: String) -> Unit,
     navigateBack: () -> Unit,
+    viewModel: StockCategoriesViewModel = hiltViewModel()
 ) {
-    val categories = listOf(
-        StockCategory.Gifts,
-        StockCategory.Infant,
-        StockCategory.Perfumes,
-        StockCategory.Soaps,
-        StockCategory.AntiperspirantDeodorants,
-        StockCategory.DeodorantsCologne,
-        StockCategory.Sunscreens,
-        StockCategory.Makeup,
-        StockCategory.Face,
-        StockCategory.Skin,
-        StockCategory.Hair,
-        StockCategory.Moisturizers
-    )
+    val categories by viewModel.categories.collectAsStateWithLifecycle()
+
+    var showCategoryDialog by remember { mutableStateOf(false) }
 
     StockCategoriesContent(
-        navigateBack = navigateBack,
+        newCategory = viewModel.newName,
+        showCategoryDialog = showCategoryDialog,
         categories = categories,
-        onItemClick = onItemClick
+        onAddButtonClick = {
+            showCategoryDialog = true
+        },
+        onCategoryChange = viewModel::updateName,
+        onOkClick = {
+            viewModel.insertCategory()
+        },
+        onDismissRequest = {
+            showCategoryDialog = false
+        },
+        onItemClick = onItemClick,
+        navigateBack = navigateBack,
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun StockCategoriesContent(
-    categories: List<StockCategory>,
+    newCategory: String,
+    showCategoryDialog: Boolean,
+    categories: List<Category>,
+    onAddButtonClick: () -> Unit,
+    onCategoryChange: (newCategory: String) -> Unit,
+    onDismissRequest: () -> Unit,
+    onOkClick: () -> Unit,
     onItemClick: (categoryId: String) -> Unit,
     navigateBack: () -> Unit
 ) {
@@ -76,18 +100,34 @@ private fun StockCategoriesContent(
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_label)
+                )
+            }
         }
     ) {
         LazyColumn(
             modifier = Modifier.padding(it),
             contentPadding = PaddingValues(8.dp),
         ) {
-            items(categories) { stockCategory ->
-                StockItem(category = stringResource(id = stockCategory.category)) {
-                    onItemClick(stockCategory.categoryId.category)
+            items(categories) { category ->
+                StockItem(category = category.name) {
+                    onItemClick(category.id.toString())
                 }
             }
         }
+    }
+    if(showCategoryDialog) {
+        CategoryDialog(
+            newCategory = newCategory,
+            onCategoryChange = onCategoryChange,
+            onOkClick = onOkClick,
+            onDismissRequest = onDismissRequest
+        )
     }
 }
 
@@ -101,40 +141,91 @@ private fun StockCategoriesListPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             val categories = listOf(
-                StockCategory.Gifts,
-                StockCategory.Infant,
-                StockCategory.Perfumes,
-                StockCategory.Soaps,
-                StockCategory.AntiperspirantDeodorants,
-                StockCategory.DeodorantsCologne,
-                StockCategory.Sunscreens,
-                StockCategory.Makeup,
-                StockCategory.Face,
-                StockCategory.Skin,
-                StockCategory.Hair,
-                StockCategory.Moisturizers
+                Category(1L, "Gifts"),
+                Category(2L, "Infant"),
+                Category(3L, "Perfumes"),
+                Category(4L, "Soaps"),
+                Category(5L, "Antiperspirant Deodorants"),
+                Category(6L, "Deodorants Cologne"),
+                Category(7L, "Sunscreens"),
+                Category(8L, "Makeup"),
+                Category(9L, "Face"),
+                Category(10L, "Skin"),
+                Category(11L, "Hair"),
+                Category(12L, "Moisturizers")
             )
 
             StockCategoriesContent(
-                navigateBack = {},
+                newCategory = "",
+                showCategoryDialog = true,
                 categories = categories,
-                onItemClick = {}
+                onAddButtonClick = {},
+                onCategoryChange = {},
+                onDismissRequest = {},
+                onOkClick = {},
+                onItemClick = {},
+                navigateBack = {},
             )
         }
     }
 }
 
-sealed class StockCategory(val categoryId: Category, @StringRes val category: Int, val icon: ImageVector) {
-    object Gifts: StockCategory(Category.GIFTS, R.string.gifts_label, Icons.Filled.Image)
-    object Infant: StockCategory(Category.INFANT, R.string.infant_label, Icons.Filled.Image)
-    object Perfumes: StockCategory(Category.PERFUMES, R.string.perfumes_label, Icons.Filled.Image)
-    object Soaps: StockCategory(Category.SOAPS, R.string.soaps_label, Icons.Filled.Image)
-    object AntiperspirantDeodorants: StockCategory(Category.ANTIPERSPIRANT_DEODORANTS, R.string.antiperspirant_deodorants_label, Icons.Filled.Image)
-    object DeodorantsCologne: StockCategory(Category.DEODORANTS_COLOGNE, R.string.deodorants_cologne_label, Icons.Filled.Image)
-    object Moisturizers: StockCategory(Category.MOISTURIZERS, R.string.moisturizers_label, Icons.Filled.Image)
-    object Sunscreens: StockCategory(Category.SUNSCREENS, R.string.sunscreens_label, Icons.Filled.Image)
-    object Makeup: StockCategory(Category.MAKEUP, R.string.makeup_label, Icons.Filled.Image)
-    object Face: StockCategory(Category.FACE, R.string.face_label, Icons.Filled.Image)
-    object Skin: StockCategory(Category.SKIN, R.string.skin_label, Icons.Filled.Image)
-    object Hair: StockCategory(Category.HAIR, R.string.hair_label, Icons.Filled.Image)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CategoryDialog(
+    newCategory: String,
+    onCategoryChange: (newCategory: String) -> Unit,
+    onOkClick: () -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    AlertDialog(
+        modifier = Modifier
+            .clip(RoundedCornerShape(10)),
+        onDismissRequest = onDismissRequest
+    ) {
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                horizontalAlignment = Alignment.End
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.new_category_label),
+                    textAlign = TextAlign.Start
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                        .fillMaxWidth(),
+                    value = newCategory,
+                    onValueChange = onCategoryChange,
+                    label = { Text(text = stringResource(id = R.string.name_label)) },
+                    placeholder = { Text(text = stringResource(id = R.string.enter_name_label)) }
+                )
+                Row(
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                ) {
+                    Button(
+                        modifier = Modifier.padding(end = 4.dp),
+                        onClick = {
+                            onDismissRequest()
+                            onOkClick()
+                        }
+                    ) {
+                        Text(text = stringResource(id = R.string.ok_label))
+                    }
+                    Button(
+                        modifier = Modifier.padding(start = 4.dp),
+                        onClick = onDismissRequest
+                    ) {
+                        Text(text = stringResource(id = R.string.cancel_label))
+                    }
+                }
+            }
+        }
+    }
 }
