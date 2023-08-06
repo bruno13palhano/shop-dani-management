91d893ebb631bc5ca5bccf8ddd5917a0e5b70ca3
bruno13palhano <bruno13palhano@gmail.com>
2023-08-05 23:16:23 -0300

    Add SearchProducst viewModel

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/viewmodel/SearchProductViewModel.kt
@@ -0,0 +1,35 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.stock.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.ProductData
+import com.bruno13palhano.core.data.di.DefaultProductRepository
+import com.bruno13palhano.core.model.Product
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchProductViewModel @Inject constructor(
+    @DefaultProductRepository private val productRepository: ProductData<Product>
+) : ViewModel() {
+    private val _products = MutableStateFlow(emptyList<Product>())
+    val products = _products.asStateFlow()
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(),
+            initialValue = emptyList()
+        )
+
+    fun search(search: String) {
+        viewModelScope.launch {
+            productRepository.search(search).collect {
+                _products.value = it
+            }
+        }
+    }
+}
