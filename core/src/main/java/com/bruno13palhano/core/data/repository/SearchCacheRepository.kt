bdf6c8aa89d749da5d55c1ec249374fb11868fc2
bruno13palhano <bruno13palhano@gmail.com>
2023-08-06 15:10:31 -0300

    Add SearchCacheRepository

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/SearchCacheRepository.kt
@@ -0,0 +1,27 @@
+package com.bruno13palhano.core.data.repository
+
+import com.bruno13palhano.core.data.SearchCacheData
+import com.bruno13palhano.core.data.database.dao.SearchCacheDao
+import com.bruno13palhano.core.data.database.model.asExternalModel
+import com.bruno13palhano.core.data.database.model.asInternalModel
+import com.bruno13palhano.core.model.SearchCache
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+internal class SearchCacheRepository @Inject constructor(
+    private val searchCacheDao: SearchCacheDao
+) : SearchCacheData<SearchCache> {
+    override suspend fun insert(model: SearchCache): Long {
+        return searchCacheDao.insert(model.asInternalModel())
+    }
+
+    override suspend fun deleteById(id: Long) {
+        searchCacheDao.deleteById(id)
+    }
+
+    override fun getAll(): Flow<List<SearchCache>> {
+        return searchCacheDao.getAll()
+            .map { it.map { entity -> entity.asExternalModel() } }
+    }
+}
