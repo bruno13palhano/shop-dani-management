3412a50bf4084aa128568bb037df12c832f00919
bruno13palhano <bruno13palhano@gmail.com>
2023-08-06 20:34:43 -0300

    Add methods to SearchProductsViewModel to isert and get SearchCache

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/viewmodel/SearchProductsViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/viewmodel/SearchProductsViewModel.kt
@@ -3,8 +3,11 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.stock.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.ProductData
+import com.bruno13palhano.core.data.SearchCacheData
 import com.bruno13palhano.core.data.di.DefaultProductRepository
+import com.bruno13palhano.core.data.di.DefaultSearchCacheRepository
 import com.bruno13palhano.core.model.Product
+import com.bruno13palhano.core.model.SearchCache
 import com.bruno13palhano.core.model.Stock
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,8 +19,16 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchProductsViewModel @Inject constructor(
-    @DefaultProductRepository private val productRepository: ProductData<Product>
+    @DefaultProductRepository private val productRepository: ProductData<Product>,
+    @DefaultSearchCacheRepository private val searchCacheRepository: SearchCacheData<SearchCache>
 ) : ViewModel() {
+    val searchCache = searchCacheRepository.getAll()
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
+
     private val _stockProducts = MutableStateFlow(emptyList<Stock>())
     val stockProducts = _stockProducts.asStateFlow()
         .stateIn(
@@ -27,17 +38,27 @@ class SearchProductsViewModel @Inject constructor(
         )
 
     fun search(search: String) {
-        viewModelScope.launch {
-            productRepository.search(search.trim()).collect {
-                _stockProducts.value = it.map { product ->
-                    Stock(
-                        id = product.id,
-                        name = product.name,
-                        photo = product.photo,
-                        purchasePrice = product.purchasePrice,
-                        quantity = product.quantity
-                    )
+        if (search.trim().isNotEmpty()) {
+            viewModelScope.launch {
+                productRepository.search(search.trim()).collect {
+                    _stockProducts.value = it.map { product ->
+                        Stock(
+                            id = product.id,
+                            name = product.name,
+                            photo = product.photo,
+                            purchasePrice = product.purchasePrice,
+                            quantity = product.quantity
+                        )
+                    }
                 }
+            }
+        }
+    }
+
+    fun insertSearch(search: String) {
+        if (search.trim().isNotEmpty()) {
+            viewModelScope.launch {
+                searchCacheRepository.insert(SearchCache(search.trim()))
             }
         }
     }
