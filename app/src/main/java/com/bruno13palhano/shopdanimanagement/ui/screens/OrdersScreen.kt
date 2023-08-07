20e59666607043146bf1d3f8336461dac323f4b6
bruno13palhano <bruno13palhano@gmail.com>
2023-08-07 19:29:49 -0300

    Add OrdersScren content

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/OrdersScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/OrdersScreen.kt
@@ -1,11 +1,19 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Analytics
+import androidx.compose.material.icons.filled.Category
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,25 +23,31 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun OrdersScreen(
-    onMenuClick: () -> Unit
+    onMenuClick: () -> Unit,
+    onCategoriesClick: () -> Unit,
 ) {
     OrdersContent(
-        onMenuClick = onMenuClick
+        onMenuClick = onMenuClick,
+        onCategoriesClick = onCategoriesClick
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OrdersContent(
-    onMenuClick: () -> Unit
+    onMenuClick: () -> Unit,
+    onCategoriesClick: () -> Unit
 ) {
     Scaffold(
         topBar = {
@@ -46,12 +60,66 @@ fun OrdersContent(
                             contentDescription = stringResource(id = R.string.drawer_menu_label)
                         )
                     }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = stringResource(id = R.string.search_label)
+                        )
+                    }
                 }
             )
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
-            Text(text = "Requests Screen")
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5F)
+                    .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp),
+                onClick = onCategoriesClick
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        modifier = Modifier.size(128.dp),
+                        imageVector = Icons.Filled.Category,
+                        contentDescription = stringResource(id = R.string.category_image_label)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.categories_label),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(1F)
+                    .padding( start = 16.dp, top = 8.dp, end = 16.dp, bottom = 16.dp),
+                onClick = {}
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        modifier = Modifier.size(128.dp),
+                        imageVector = Icons.Filled.Analytics,
+                        contentDescription = stringResource(id = R.string.analytics_image_label)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.analytics_label),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
@@ -66,7 +134,8 @@ fun RequestsDynamicPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             OrdersContent(
-                onMenuClick = {}
+                onMenuClick = {},
+                onCategoriesClick = {}
             )
         }
     }
@@ -84,7 +153,8 @@ fun OrdersPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             OrdersContent(
-                onMenuClick = {}
+                onMenuClick = {},
+                onCategoriesClick = {}
             )
         }
     }
