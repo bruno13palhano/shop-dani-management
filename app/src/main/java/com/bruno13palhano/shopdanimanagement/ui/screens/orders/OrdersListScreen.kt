64dc2895609847e394976384b334ad52dc53bfcd
bruno13palhano <bruno13palhano@gmail.com>
2023-08-07 19:28:14 -0300

    Add OrdersListScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/orders/OrdersListScreen.kt
@@ -0,0 +1,48 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.orders
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.ProductListContent
+import com.bruno13palhano.shopdanimanagement.ui.screens.orders.viewmodel.OrdersListViewModel
+
+@Composable
+fun OrdersListScreen(
+    categoryId: Long,
+    onItemClick: (id: Long) -> Unit,
+    onAddButtonClick: () -> Unit,
+    navigateUp: () -> Unit,
+    viewModel: OrdersListViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getCategory(categoryId)
+    }
+    LaunchedEffect(key1 = viewModel.name) {
+        viewModel.getProductsByCategory(viewModel.name)
+    }
+
+    val orderList by viewModel.orders.collectAsStateWithLifecycle()
+    var showCategoryDialog by remember { mutableStateOf(false) }
+
+    ProductListContent(
+        categoryId = viewModel.name.ifEmpty {
+            stringResource(id = R.string.all_products_label)
+        },
+        showCategoryDialog = showCategoryDialog,
+        itemList = orderList,
+        onCategoryChange = viewModel::updateName,
+        onOkClick = { viewModel.updateCategory(categoryId) },
+        onDismissRequest = { showCategoryDialog = false },
+        onItemClick = onItemClick,
+        onEditItemClick = { showCategoryDialog = true },
+        onAddButtonClick = onAddButtonClick,
+        navigateUp = navigateUp
+    )
+}
