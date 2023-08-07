c3b63085827af8f8a0e7f621efa51197bf0f3810
bruno13palhano <bruno13palhano@gmail.com>
2023-08-07 19:28:56 -0300

    Add OrdersCategoriesScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/orders/OrdersCategoriesScreen.kt
@@ -0,0 +1,41 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.orders
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bruno13palhano.shopdanimanagement.ui.components.CategoriesContent
+import com.bruno13palhano.shopdanimanagement.ui.components.CategoriesItems
+import com.bruno13palhano.shopdanimanagement.ui.screens.orders.viewmodel.OrdersCategoriesViewModel
+
+@Composable
+fun OrdersCategoriesScreen(
+    onItemClick: (categoryId: String) -> Unit,
+    navigateUp: () -> Unit,
+    viewModel: OrdersCategoriesViewModel = hiltViewModel()
+) {
+    val categories by viewModel.categories.collectAsStateWithLifecycle()
+    var showCategoryDialog by remember { mutableStateOf(false) }
+
+    CategoriesContent(
+        newCategory = viewModel.newName,
+        showCategoryDialog = showCategoryDialog,
+        categories = categories,
+        onAddButtonClick = { showCategoryDialog = true },
+        onCategoryChange = viewModel::updateName,
+        onDismissRequest = { showCategoryDialog = false },
+        onOkClick = { viewModel.insertCategory() },
+        onItemClick = onItemClick,
+        onMoreOptionsItemClick = { index ->
+            when (index) {
+                CategoriesItems.allProducts -> {
+
+                }
+            }
+        },
+        navigateBack = navigateUp
+    )
+}
