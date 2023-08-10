1192ff02e2bc80d7031cdddf3468b4c97c5ca1fe
bruno13palhano <bruno13palhano@gmail.com>
2023-08-10 19:42:49 -0300

    Add ProductsScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/products/Products.kt
@@ -0,0 +1,139 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.products
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Analytics
+import androidx.compose.material.icons.filled.Category
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductsScreen(
+    onMenuClick: () -> Unit,
+    onCategoriesClick: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.products_label)) },
+                navigationIcon = {
+                    IconButton(onClick = onMenuClick) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = stringResource(id = R.string.drawer_menu_label)
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5F)
+                    .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 4.dp),
+                onClick = onCategoriesClick
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        modifier = Modifier.size(128.dp),
+                        imageVector = Icons.Filled.Category,
+                        contentDescription = stringResource(id = R.string.category_image_label)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.categories_label),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(1F)
+                    .padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 8.dp),
+                onClick = {}
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        modifier = Modifier.size(128.dp),
+                        imageVector = Icons.Filled.Analytics,
+                        contentDescription = stringResource(id = R.string.analytics_image_label)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.analytics_label),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun ProductsDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ProductsScreen(
+                onMenuClick = {},
+                onCategoriesClick = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun ProductsPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ProductsScreen(
+                onMenuClick = {},
+                onCategoriesClick = {}
+            )
+        }
+    }
+}
