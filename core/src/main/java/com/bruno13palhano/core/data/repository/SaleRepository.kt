2006af71028de26902f0d4a3269334bf46d31b40
bruno13palhano <bruno13palhano@gmail.com>
2023-08-09 22:52:50 -0300

    Add Sale, Shopping and StockOrder Repositories

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/SaleRepository.kt
@@ -0,0 +1,50 @@
+package com.bruno13palhano.core.data.repository
+
+import com.bruno13palhano.core.data.SaleData
+import com.bruno13palhano.core.data.database.dao.SaleDao
+import com.bruno13palhano.core.data.database.model.asExternalModel
+import com.bruno13palhano.core.data.database.model.asInternalModel
+import com.bruno13palhano.core.model.Sale
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+internal class SaleRepository @Inject constructor(
+    private val saleDao: SaleDao
+) : SaleData<Sale> {
+    override suspend fun insert(model: Sale): Long {
+        return saleDao.insert(model.asInternalModel())
+    }
+
+    override suspend fun update(model: Sale) {
+        saleDao.update(model.asInternalModel())
+    }
+
+    override suspend fun delete(model: Sale) {
+        saleDao.delete(model.asInternalModel())
+    }
+
+    override suspend fun deleteById(id: Long) {
+        saleDao.deleteById(id)
+    }
+
+    override fun getAll(): Flow<List<Sale>> {
+        return saleDao.getAll()
+            .map {
+                it.map { entity -> entity.asExternalModel() }
+            }
+    }
+
+    override fun getById(id: Long): Flow<Sale> {
+        return saleDao.getById(id)
+            .map { it.asExternalModel() }
+            .catch { it.printStackTrace() }
+    }
+
+    override fun getLast(): Flow<Sale> {
+        return saleDao.getLast()
+            .map { it.asExternalModel() }
+            .catch { it.printStackTrace() }
+    }
+}
---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/ShoppingCacheRepository.kt
@@ -0,0 +1,50 @@
+package com.bruno13palhano.core.data.repository
+
+import com.bruno13palhano.core.data.ShoppingData
+import com.bruno13palhano.core.data.database.dao.ShoppingDao
+import com.bruno13palhano.core.data.database.model.asExternalModel
+import com.bruno13palhano.core.data.database.model.asInternalModel
+import com.bruno13palhano.core.model.Shopping
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+internal class ShoppingCacheRepository @Inject constructor(
+    private val shoppingDao: ShoppingDao
+) : ShoppingData<Shopping> {
+    override suspend fun insert(model: Shopping): Long {
+        return shoppingDao.insert(model.asInternalModel())
+    }
+
+    override suspend fun update(model: Shopping) {
+        shoppingDao.update(model.asInternalModel())
+    }
+
+    override suspend fun delete(model: Shopping) {
+        shoppingDao.delete(model.asInternalModel())
+    }
+
+    override suspend fun deleteById(id: Long) {
+        shoppingDao.deleteById(id)
+    }
+
+    override fun getAll(): Flow<List<Shopping>> {
+        return shoppingDao.getAll()
+            .map {
+                it.map { entity -> entity.asExternalModel()}
+            }
+    }
+
+    override fun getById(id: Long): Flow<Shopping> {
+        return shoppingDao.getLast()
+            .map { it.asExternalModel() }
+            .catch { it.printStackTrace() }
+    }
+
+    override fun getLast(): Flow<Shopping> {
+        return shoppingDao.getLast()
+            .map { it.asExternalModel() }
+            .catch { it.printStackTrace() }
+    }
+}
