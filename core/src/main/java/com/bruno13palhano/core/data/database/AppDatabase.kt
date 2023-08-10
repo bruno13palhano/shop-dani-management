da23ce78a99790e54048fc387aedfe81ba5e7e89
bruno13palhano <bruno13palhano@gmail.com>
2023-08-09 22:51:52 -0300

    Add daos and entities

--- core/src/main/java/com/bruno13palhano/core/data/database/AppDatabase.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/AppDatabase.kt
@@ -5,16 +5,25 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.bruno13palhano.core.data.database.dao.CategoryDao
 import com.bruno13palhano.core.data.database.dao.ProductDao
+import com.bruno13palhano.core.data.database.dao.SaleDao
 import com.bruno13palhano.core.data.database.dao.SearchCacheDao
+import com.bruno13palhano.core.data.database.dao.ShoppingDao
+import com.bruno13palhano.core.data.database.dao.StockOrderDao
 import com.bruno13palhano.core.data.database.model.CategoryEntity
 import com.bruno13palhano.core.data.database.model.ProductEntity
+import com.bruno13palhano.core.data.database.model.SaleEntity
 import com.bruno13palhano.core.data.database.model.SearchCacheEntity
+import com.bruno13palhano.core.data.database.model.ShoppingEntity
+import com.bruno13palhano.core.data.database.model.StockOrderEntity
 
 @Database(
     entities = [
         ProductEntity::class,
         CategoryEntity::class,
-        SearchCacheEntity::class
+        SearchCacheEntity::class,
+        SaleEntity::class,
+        ShoppingEntity::class,
+        StockOrderEntity::class
     ],
     version = 1,
     exportSchema = false
@@ -24,4 +33,7 @@ internal abstract class AppDatabase : RoomDatabase() {
     abstract val productDao: ProductDao
     abstract val categoryDao: CategoryDao
     abstract val searchCacheDao: SearchCacheDao
+    abstract val saleDao: SaleDao
+    abstract val shoppingDao: ShoppingDao
+    abstract val stockOrderDao: StockOrderDao
 }
