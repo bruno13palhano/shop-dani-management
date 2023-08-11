469531dc1e7f0ff1a1af7116377896cac5988dfe
bruno13palhano <bruno13palhano@gmail.com>
2023-08-10 22:44:15 -0300

    Change StockItem to HorizontalStockItem

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/ProductList.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/ProductList.kt
@@ -7,9 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
@@ -108,22 +107,16 @@ fun ProductListContent(
             }
         }
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(160.dp),
-            contentPadding = PaddingValues(4.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalArrangement = Arrangement.Center,
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
             modifier = Modifier.padding(it)
         ) {
             items(itemList) { stock ->
-                StockItem(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(4.dp),
-                    photo = stock.photo,
+                HorizontalStockItem(
+                    modifier = Modifier.padding(vertical = 4.dp),
                     name = stock.name,
+                    photo = stock.photo,
                     price = stock.purchasePrice,
-                    quantity = stock.quantity,
                     onClick = { onItemClick(stock.id) }
                 )
             }
