3b15c4a3dddc26d0ffbebcd0fa7b264b2615254c
bruno13palhano <bruno13palhano@gmail.com>
2023-08-11 22:00:17 -0300

    Add StockListViewModel

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/StockListScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/StockListScreen.kt
@@ -1,16 +1,23 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.stock
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.ui.components.StockListContent
+import com.bruno13palhano.shopdanimanagement.ui.screens.stock.viewmodel.StockListViewModel
 
 @Composable
 fun StockListScreen(
     onItemClick: (id: Long) -> Unit,
     onAddButtonClick: () -> Unit,
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    viewModel: StockListViewModel = hiltViewModel()
 ) {
+    val stockList by viewModel.stockList.collectAsStateWithLifecycle()
+
     StockListContent(
-        itemList = emptyList(),
+        itemList = stockList,
         onItemClick = onItemClick,
         onAddButtonClick = onAddButtonClick,
         navigateUp = navigateUp
