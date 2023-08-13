0d7f4531b5bcd5fc5b45f29bc03f6eb2911167b8
bruno13palhano <bruno13palhano@gmail.com>
2023-08-13 00:37:43 -0300

    Add content to EditStockItemScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/EditStockItemScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stock/EditStockItemScreen.kt
@@ -1,25 +1,114 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.stock
 
+import android.content.res.Configuration
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DisplayMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.components.StockOrderContent
+import com.bruno13palhano.shopdanimanagement.ui.screens.stock.viewmodel.EditStockItemViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun EditStockItemScreen(
-    productId: Long,
-    navigateUp: () -> Unit
+    stockItemId: Long,
+    navigateUp: () -> Unit,
+    viewModel: EditStockItemViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getStockItem(stockItemId)
+    }
+    val isStockItemNotEmpty by viewModel.isStockItemNotEmpty.collectAsStateWithLifecycle()
+    val configuration = LocalConfiguration.current
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var datePickerState = rememberDatePickerState()
+    var showDatePickerDialog by remember { mutableStateOf(false) }
+
+    if (showDatePickerDialog) {
+        DatePickerDialog(
+            onDismissRequest = {
+                showDatePickerDialog = false
+                focusManager.clearFocus(force = true)
+            },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        datePickerState.selectedDateMillis?.let {
+                            viewModel.updateDate(it)
+                        }
+                        showDatePickerDialog = false
+                        focusManager.clearFocus(force = true)
+                    }
+                ) {
+                    Text(text = stringResource(id = R.string.date_label))
+                }
+            }
+        ) {
+            datePickerState = rememberDatePickerState(
+                initialSelectedDateMillis = viewModel.dateInMillis,
+                initialDisplayMode = if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                    DisplayMode.Picker
+                } else {
+                    DisplayMode.Input
+                }
+            )
+            DatePicker(
+                state = datePickerState,
+                showModeToggle = configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+            )
+        }
+    }
+
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val errorMessage = stringResource(id = R.string.empty_fields_error)
+
     StockOrderContent(
-        screenTitle = "Edit Stock Item",
-        name = "",
-        quantity = "",
-        isOrderedByCustomer = true,
-        photo = "",
-        date = "",
-        onQuantityChange = {},
-        onIsOrderedByCustomerChange = {},
-        onDateClick = {},
-        onOutsideClick = {},
-        onDoneClick = {},
+        screenTitle = stringResource(id = R.string.edit_item_label),
+        snackbarHostState = snackbarHostState,
+        name = viewModel.name,
+        purchasePrice = viewModel.purchasePrice,
+        quantity = viewModel.quantity,
+        photo = viewModel.photo,
+        date = viewModel.date,
+        onPurchasePriceChange = viewModel::updatePurchasePrice,
+        onQuantityChange = viewModel::updateQuantity,
+        onDateClick = { showDatePickerDialog = true },
+        onOutsideClick = {
+            keyboardController?.hide()
+            focusManager.clearFocus(force = true)
+        },
+        onDoneClick = {
+            if (isStockItemNotEmpty) {
+                viewModel.updateStockItem(stockItemId)
+                navigateUp()
+            } else {
+                scope.launch {
+                    snackbarHostState.showSnackbar(errorMessage)
+                }
+            }
+        },
         navigateUp = navigateUp
     )
 }
