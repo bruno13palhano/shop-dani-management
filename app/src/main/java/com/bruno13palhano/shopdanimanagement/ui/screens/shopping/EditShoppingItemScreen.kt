9b01b3d24ddbaa216a264d07aeb9448a9ae74fda
bruno13palhano <bruno13palhano@gmail.com>
2023-08-14 17:05:08 -0300

    Add EditShoppingItemScree

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/shopping/EditShoppingItemScreen.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.shopping
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EditShoppingItemScreen() {
+    Text(text = "Edit Shopping Item")
+}
