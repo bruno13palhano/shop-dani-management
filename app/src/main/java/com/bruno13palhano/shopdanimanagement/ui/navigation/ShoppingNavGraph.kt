236ba6c8f0602fa284136a0980f2dc22258b8ecc
bruno13palhano <bruno13palhano@gmail.com>
2023-08-16 15:44:13 -0300

    Update ShoppingNavGraph destinations

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/ShoppingNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/ShoppingNavGraph.kt
@@ -1,12 +1,15 @@
 package com.bruno13palhano.shopdanimanagement.ui.navigation
 
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.screens.ShoppingScreen
-import com.bruno13palhano.shopdanimanagement.ui.screens.common.CategoriesScreen
-import com.bruno13palhano.shopdanimanagement.ui.screens.products.ProductListScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.shopping.EditShoppingItemScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.NewItemScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.shopping.ShoppingProductListScreen
 
 private const val ITEM_ID = "item_Id"
 
@@ -20,30 +23,41 @@ fun NavGraphBuilder.shoppingNavGraph(
     ) {
         composable(route = ShoppingDestinations.MAIN_SHOPPING_ROUTE) {
             ShoppingScreen(
+                onItemClick = { shoppingItemId ->
+                    navController.navigate(
+                        route = "${ShoppingDestinations.SHOPPING_EDIT_SHOPPING_PRODUCT_ROUTE}$shoppingItemId"
+                    )
+                },
                 onMenuClick = onMenuClick,
-                onCategoriesClick = {
-                    navController.navigate(route = ShoppingDestinations.SHOPPING_CATEGORIES_ROUTE)
+                onAddButtonClick = {
+                    navController.navigate(route = ShoppingDestinations.SHOPPING_PRODUCT_LIST_ROUTE)
                 }
             )
         }
-        composable(route = ShoppingDestinations.SHOPPING_CATEGORIES_ROUTE) {
-            CategoriesScreen(
-                onItemClick = { categoryId ->
-                    navController.navigate(route = "${ShoppingDestinations.SHOPPING_LIST_ROUTE}$categoryId")
+        composable(route = ShoppingDestinations.SHOPPING_PRODUCT_LIST_ROUTE) {
+            ShoppingProductListScreen(
+                onItemClick = { productId ->
+                    navController.navigate(
+                        route = "${ShoppingDestinations.SHOPPING_NEW_SHOPPING_PRODUCT_ROUTE}$productId"
+                    )
                 },
                 navigateUp = { navController.navigateUp() }
             )
         }
-        composable(route = ShoppingDestinations.SHOPPING_LIST_WITH_ID_ROUTE) { backStackEntry ->
-            backStackEntry.arguments?.getString(ITEM_ID)?.let { categoryId ->
-                ProductListScreen(
-                    categoryId = categoryId.toLong(),
-                    onItemClick = { productId ->
-
-                    },
-                    onAddButtonClick = {
-
-                    },
+        composable(route = ShoppingDestinations.SHOPPING_NEW_SHOPPING_PRODUCT_WITH_ID_ROUTE) { backStackEntry ->
+            backStackEntry.arguments?.getString(ITEM_ID)?.let { productId ->
+                NewItemScreen(
+                    isOrderedByCustomer = false,
+                    screenTitle = stringResource(id = R.string.new_shopping_item_label),
+                    productId = productId.toLong(),
+                    navigateUp = { navController.navigateUp() }
+                )
+            }
+        }
+        composable(route = ShoppingDestinations.SHOPPING_EDIT_SHOPPING_PRODUCT_WITH_ID_ROUTE) { backStackEntry ->
+            backStackEntry.arguments?.getString(ITEM_ID)?.let { shoppingItemId ->
+                EditShoppingItemScreen(
+                    shoppingItemId = shoppingItemId.toLong(),
                     navigateUp = { navController.navigateUp() }
                 )
             }
@@ -53,8 +67,9 @@ fun NavGraphBuilder.shoppingNavGraph(
 
 object ShoppingDestinations {
     const val MAIN_SHOPPING_ROUTE = "main_shopping_route"
-    const val SHOPPING_CATEGORIES_ROUTE = "shopping_categories_route"
-    const val SHOPPING_LIST_ROUTE = "shopping_list_route/"
-    const val SHOPPING_LIST_WITH_ID_ROUTE = "$SHOPPING_LIST_ROUTE{$ITEM_ID}"
-    const val SHOPPING_PRODUCT_ROUTE = "shopping_product_route"
+    const val SHOPPING_PRODUCT_LIST_ROUTE = "shopping_product_list_route"
+    const val SHOPPING_NEW_SHOPPING_PRODUCT_ROUTE = "shopping_new_shopping_product_route"
+    const val SHOPPING_NEW_SHOPPING_PRODUCT_WITH_ID_ROUTE = "$SHOPPING_NEW_SHOPPING_PRODUCT_ROUTE{$ITEM_ID}"
+    const val SHOPPING_EDIT_SHOPPING_PRODUCT_ROUTE = "shopping_edit_shopping_product_route"
+    const val SHOPPING_EDIT_SHOPPING_PRODUCT_WITH_ID_ROUTE = "$SHOPPING_EDIT_SHOPPING_PRODUCT_ROUTE{$ITEM_ID}"
 }
