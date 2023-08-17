4f991efacf49eb735174642ec567ed595835a2ce
bruno13palhano <bruno13palhano@gmail.com>
2023-08-17 16:38:27 -0300

    Add SalesOptinosScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/SalesOptionsScreen.kt
@@ -0,0 +1,126 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.sales
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.PlaylistAdd
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SalesOptionsScreen(
+    onOrdersOptionClick: () -> Unit,
+    onStockOptionClick: () -> Unit,
+    navigateUp: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Sales Options") },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(it)
+        ) {
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.5F)
+                    .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 4.dp),
+                onClick = onOrdersOptionClick
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        modifier = Modifier.size(128.dp),
+                        imageVector = Icons.Filled.Checklist,
+                        contentDescription = stringResource(id = R.string.category_image_label)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.orders_label),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(1F)
+                    .padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 8.dp),
+                onClick = onStockOptionClick
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        modifier = Modifier.size(128.dp),
+                        imageVector = Icons.Filled.PlaylistAdd,
+                        contentDescription = stringResource(id = R.string.category_image_label)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.stock_label),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun SalesOptionsDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            SalesOptionsScreen(
+                onOrdersOptionClick = {},
+                onStockOptionClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
