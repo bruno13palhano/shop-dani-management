0c23f7278e7a06259866e6f318a7e868e4903718
bruno13palhano <bruno13palhano@gmail.com>
2023-08-17 16:55:14 -0300

    Add content to SalesScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/SalesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/SalesScreen.kt
@@ -1,12 +1,16 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -15,25 +19,43 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bruno13palhano.core.model.Sale
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.SimpleItemList
+import com.bruno13palhano.shopdanimanagement.ui.screens.sales.viewmodel.SalesViewModel
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun SalesScreen(
-    onMenuClick: () -> Unit
+    onItemClick: (id: Long) -> Unit,
+    onMenuClick: () -> Unit,
+    onAddButtonClick: () -> Unit,
+    viewModel: SalesViewModel = hiltViewModel()
 ) {
+    val saleList by viewModel.saleList.collectAsStateWithLifecycle()
+
     SalesContent(
-        onMenuClick = onMenuClick
+        saleList = saleList,
+        onItemClick = onItemClick,
+        onMenuClick = onMenuClick,
+        onAddButtonClick = onAddButtonClick
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SalesContent(
-    onMenuClick: () -> Unit
+    saleList: List<Sale>,
+    onItemClick: (id: Long) -> Unit,
+    onMenuClick: () -> Unit,
+    onAddButtonClick: () -> Unit
 ) {
     Scaffold(
         topBar = {
@@ -48,10 +70,25 @@ fun SalesContent(
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_label)
+                )
+            }
         }
     ) {
-        Column(modifier = Modifier.padding(it)) {
-            Text(text = "Sales Screen")
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(items = saleList, key = { sale -> sale.id }) { sale ->
+                SimpleItemList(
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                    itemName = sale.name,
+                    imageVector = Icons.Filled.ArrowForward,
+                    onClick = { onItemClick(sale.id) }
+                )
+            }
         }
     }
 }
@@ -66,7 +103,9 @@ fun SalesDynamicPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             SalesScreen(
-                onMenuClick = {}
+                onItemClick = {},
+                onMenuClick = {},
+                onAddButtonClick = {}
             )
         }
     }
@@ -84,7 +123,9 @@ fun SalesPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             SalesScreen(
-                onMenuClick = {}
+                onItemClick = {},
+                onMenuClick = {},
+                onAddButtonClick = {}
             )
         }
     }
