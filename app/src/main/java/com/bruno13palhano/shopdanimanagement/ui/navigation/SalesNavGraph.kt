3cf967401212d0a2b7a460208fa932539f6b0e9b
bruno13palhano <bruno13palhano@gmail.com>
2023-08-18 12:18:10 -0300

    Add routes to salesNavGraph

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/SalesNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/SalesNavGraph.kt
@@ -7,6 +7,8 @@ import androidx.navigation.navigation
 import com.bruno13palhano.shopdanimanagement.ui.screens.SalesScreen
 import com.bruno13palhano.shopdanimanagement.ui.screens.sales.SaleProductsScreen
 import com.bruno13palhano.shopdanimanagement.ui.screens.sales.SaleScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.sales.SalesOptionsScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.sales.SalesStockListScreen
 
 private const val ITEM_ID = "item_Id"
 
@@ -25,22 +27,53 @@ fun NavGraphBuilder.salesNavGraph(
                 },
                 onMenuClick = onMenuClick,
                 onAddButtonClick = {
-                    navController.navigate(route = SalesDestinations.SALES_PRODUCTS_LIST_ROUTE)
+                    navController.navigate(route = SalesDestinations.SALES_OPTIONS_ROUTE)
                 }
+            )
+        }
+        composable(route = SalesDestinations.SALES_OPTIONS_ROUTE) {
+            SalesOptionsScreen(
+                onOrdersOptionClick = {
+                    navController.navigate(route = SalesDestinations.SALES_PRODUCTS_LIST_ROUTE)
+                },
+                onStockOptionClick = {
+                    navController.navigate(route = SalesDestinations.SALES_STOCK_LIST_ROUTE)
+                },
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+        composable(route = SalesDestinations.SALES_STOCK_LIST_ROUTE) {
+            SalesStockListScreen(
+                onItemClick = { stockItemId ->
+                    navController.navigate(
+                        route = "${SalesDestinations.SALES_NEW_SALE_STOCK_ROUTE}$stockItemId"
+                    )
+                },
+                navigateUp = { navController.navigateUp() }
             )
         }
         composable(route = SalesDestinations.SALES_PRODUCTS_LIST_ROUTE) {
             SaleProductsScreen(
                 onItemClick = { productId ->
-                    navController.navigate(route = "${SalesDestinations.SALES_NEW_SALE_ROUTE}$productId")
+                    navController.navigate(route = "${SalesDestinations.SALES_NEW_SALE_ORDERS_ROUTE}$productId")
                 },
                 navigateUp = { navController.navigateUp() }
             )
         }
-        composable(route = SalesDestinations.SALES_NEW_SALE_WITH_ID_ROUTE) { backStackEntry ->
+        composable(route = SalesDestinations.SALES_NEW_SALE_ORDERS_WITH_ID_ROUTE) { backStackEntry ->
             backStackEntry.arguments?.getString(ITEM_ID)?.let { productId ->
                 SaleScreen(
+                    isOrderedByCustomer = true,
                     productId = productId.toLong(),
+                    navigateUp = { navController.navigateUp() }
+                )
+            }
+        }
+        composable(route = SalesDestinations.SALES_NEW_SALE_STOCK_WITH_ID_ROUTE) { backStackEntry ->
+            backStackEntry.arguments?.getString(ITEM_ID)?.let { stockItemId ->
+                SaleScreen(
+                    isOrderedByCustomer = false,
+                    productId = stockItemId.toLong(),
                     navigateUp = { navController.navigateUp() }
                 )
             }
@@ -55,10 +88,13 @@ fun NavGraphBuilder.salesNavGraph(
 
 object SalesDestinations {
     const val MAIN_SALES_ROUTE = "main_sales_route"
+    const val SALES_OPTIONS_ROUTE = "sales_options_route"
+    const val SALES_STOCK_LIST_ROUTE = "sales_stock_list_route"
     const val SALES_PRODUCTS_LIST_ROUTE = "sales_products_list_route"
-    const val SALES_PRODUCTS_LIST_WITH_ID_ROUTE = "$SALES_PRODUCTS_LIST_ROUTE{$ITEM_ID}"
-    const val SALES_NEW_SALE_ROUTE = "sales_new_sale_route"
-    const val SALES_NEW_SALE_WITH_ID_ROUTE = "$SALES_NEW_SALE_ROUTE{$ITEM_ID}"
+    const val SALES_NEW_SALE_ORDERS_ROUTE = "sales_new_sale_orders_route"
+    const val SALES_NEW_SALE_ORDERS_WITH_ID_ROUTE = "$SALES_NEW_SALE_ORDERS_ROUTE{$ITEM_ID}"
+    const val SALES_NEW_SALE_STOCK_ROUTE = "sales_new_sale_stock_route"
+    const val SALES_NEW_SALE_STOCK_WITH_ID_ROUTE = "$SALES_NEW_SALE_STOCK_ROUTE{$ITEM_ID}"
     const val SALES_EDIT_SALE_ROUTE = "sales_edit_sale_route"
     const val SALES_EDIT_SALE_WITH_ID_ROUTE = "$SALES_EDIT_SALE_ROUTE{$ITEM_ID}"
 }
