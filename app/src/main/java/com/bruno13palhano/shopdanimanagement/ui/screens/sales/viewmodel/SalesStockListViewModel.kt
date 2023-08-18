d6d4c312d22aa9f0356e64ce1d35f30878f2e340
bruno13palhano <bruno13palhano@gmail.com>
2023-08-18 12:15:52 -0300

    Remove unused imports

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/viewmodel/SalesStockListViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/viewmodel/SalesStockListViewModel.kt
@@ -2,11 +2,8 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.sales.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bruno13palhano.core.data.SaleData
 import com.bruno13palhano.core.data.StockOrderData
-import com.bruno13palhano.core.data.di.DefaultSaleRepository
 import com.bruno13palhano.core.data.di.DefaultStockOrderRepository
-import com.bruno13palhano.core.model.Sale
 import com.bruno13palhano.core.model.Stock
 import com.bruno13palhano.core.model.StockOrder
 import dagger.hilt.android.lifecycle.HiltViewModel
