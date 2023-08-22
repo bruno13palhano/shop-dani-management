e4f10f0b221a026926dde4f13ec613f4f27888c0
bruno13palhano <bruno13palhano@gmail.com>
2023-08-22 00:03:59 -0300

    Wrapp Product with CommonItem

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/viewmodel/SaleProductsViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/viewmodel/SaleProductsViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.ProductData
 import com.bruno13palhano.core.data.di.DefaultProductRepository
 import com.bruno13palhano.core.model.Product
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -15,6 +17,16 @@ class SaleProductsViewModel @Inject constructor(
     @DefaultProductRepository private val productRepository: ProductData<Product>
 ) : ViewModel() {
     val products = productRepository.getAll()
+        .map {
+            it.map { product ->
+                CommonItem(
+                    id = product.id,
+                    photo = product.photo,
+                    title = product.name,
+                    subtitle = product.company
+                )
+            }
+        }
         .stateIn(
             scope = viewModelScope,
             started = WhileSubscribed(5_000),
