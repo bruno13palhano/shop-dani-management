821aa04726188debaf44a8d63d600372603dab9a
bruno13palhano <bruno13palhano@gmail.com>
2023-08-22 14:44:19 -0300

    Change SaleItemList order

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/Lists.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/Lists.kt
@@ -242,12 +242,12 @@ fun SaleItemList(
             ) {
                 Text(
                     modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp),
-                    text = customerName,
+                    text = productName,
                     style = MaterialTheme.typography.titleMedium
                 )
                 Text(
                     modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                    text = productName,
+                    text = customerName,
                     fontStyle = FontStyle.Italic,
                     style = MaterialTheme.typography.bodyMedium
                 )
