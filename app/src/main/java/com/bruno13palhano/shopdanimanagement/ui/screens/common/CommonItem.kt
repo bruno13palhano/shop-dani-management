15bc689a3a47ecd176bf6fd7170acb83610921cc
bruno13palhano <bruno13palhano@gmail.com>
2023-08-21 23:38:27 -0300

    Add a model for common item list

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/CommonItem.kt
@@ -0,0 +1,8 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.common
+
+data class CommonItem(
+    val id: Long,
+    val photo: String,
+    val title: String,
+    val subtitle: String,
+)
