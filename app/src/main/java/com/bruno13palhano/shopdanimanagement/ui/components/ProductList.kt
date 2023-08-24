d18f3f1e39a7eca9d8514434c74683adece3320e
bruno13palhano <bruno13palhano@gmail.com>
2023-08-23 23:12:39 -0300

    Remove more vert menu and add edit button

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/ProductList.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/ProductList.kt
@@ -1,9 +1,6 @@
 package com.bruno13palhano.shopdanimanagement.ui.components
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,7 +9,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,11 +20,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,15 +43,10 @@ fun ProductListContent(
     onAddButtonClick: () -> Unit,
     navigateUp: () -> Unit
 ) {
-    var expanded by remember { mutableStateOf(false) }
-    val menuOptions = arrayOf(
-        stringResource(id = R.string.edit_label)
-    )
-
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = categoryId) },
+                title = { Text(text = stringResource(id = R.string.products_label)) },
                 navigationIcon = {
                     IconButton(onClick = navigateUp) {
                         Icon(
@@ -69,31 +56,11 @@ fun ProductListContent(
                     }
                 },
                 actions = {
-                    IconButton(onClick = { expanded = true }) {
-                        Box(contentAlignment = Alignment.Center) {
-                            Icon(
-                                imageVector = Icons.Filled.MoreVert,
-                                contentDescription = stringResource(id = R.string.more_options_label)
-                            )
-                            Column(
-                                modifier = Modifier.fillMaxSize(),
-                                verticalArrangement = Arrangement.Center,
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ) {
-                                MoreOptionsMenu(
-                                    items = menuOptions,
-                                    expanded = expanded,
-                                    onDismissRequest = { expanded = it },
-                                    onClick = { index ->
-                                        when (index) {
-                                            0 -> {
-                                                onEditItemClick()
-                                            }
-                                        }
-                                    }
-                                )
-                            }
-                        }
+                    IconButton(onClick = onEditItemClick) {
+                        Icon(
+                            imageVector = Icons.Filled.Edit,
+                            contentDescription = stringResource(id = R.string.edit_label)
+                        )
                     }
                 }
             )
