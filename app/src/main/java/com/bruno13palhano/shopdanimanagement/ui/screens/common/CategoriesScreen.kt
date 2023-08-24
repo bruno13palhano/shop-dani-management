8fc45daa1d1063dd1bee922693b2f02a93c2a0e9
bruno13palhano <bruno13palhano@gmail.com>
2023-08-23 21:35:53 -0300

    Remove Product from CategoriesScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/CategoriesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/CategoriesScreen.kt
@@ -1,7 +1,6 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.common
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -9,7 +8,6 @@ import androidx.compose.runtime.setValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.ui.components.CategoriesContent
-import com.bruno13palhano.shopdanimanagement.ui.components.CategoriesItems
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.viewmodel.CategoriesViewModel
 
 @Composable
@@ -18,36 +16,18 @@ fun CategoriesScreen(
     navigateUp: () -> Unit,
     viewModel: CategoriesViewModel = hiltViewModel()
 ) {
-    LaunchedEffect(key1 = Unit) {
-        viewModel.getAllProducts()
-    }
-
     val categories by viewModel.categories.collectAsStateWithLifecycle()
-    val stockList by viewModel.products.collectAsStateWithLifecycle()
     var showCategoryDialog by remember { mutableStateOf(false) }
-    var showAllProducts by remember { mutableStateOf(false) }
 
     CategoriesContent(
-        showAllProducts = showAllProducts,
         newCategory = viewModel.newName,
         showCategoryDialog = showCategoryDialog,
         categories = categories,
-        stockList = stockList,
         onAddButtonClick = { showCategoryDialog = true },
         onCategoryChange = viewModel::updateName,
         onDismissRequest = { showCategoryDialog = false },
         onOkClick = { viewModel.insertCategory() },
         onItemClick = onItemClick,
-        onMoreOptionsItemClick = { index ->
-            when (index) {
-                CategoriesItems.allProducts -> {
-                    showAllProducts = true
-                }
-                CategoriesItems.categories -> {
-                    showAllProducts = false
-                }
-            }
-        },
         navigateBack = navigateUp
     )
 }
--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/viewmodel/CategoriesViewModel.kt
@@ -6,34 +6,19 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.CategoryData
-import com.bruno13palhano.core.data.ProductData
 import com.bruno13palhano.core.data.di.DefaultCategoryRepository
-import com.bruno13palhano.core.data.di.DefaultProductRepository
 import com.bruno13palhano.core.model.Category
-import com.bruno13palhano.core.model.Product
-import com.bruno13palhano.core.model.Stock
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CategoriesViewModel @Inject constructor(
-    @DefaultCategoryRepository private val categoryRepository: CategoryData<Category>,
-    @DefaultProductRepository private val productRepository: ProductData<Product>
+    @DefaultCategoryRepository private val categoryRepository: CategoryData<Category>
 ): ViewModel() {
-    private val _products = MutableStateFlow<List<Stock>>(emptyList())
-    val products = _products.asStateFlow()
-        .stateIn(
-            scope = viewModelScope,
-            started = WhileSubscribed(5_000),
-            initialValue = emptyList()
-        )
-
-    var categories = categoryRepository.getAll()
+    val categories = categoryRepository.getAll()
         .stateIn(
             scope = viewModelScope,
             started = WhileSubscribed(5_000),
@@ -53,12 +38,6 @@ class CategoriesViewModel @Inject constructor(
             categoryRepository.insert(category)
         }
         restoreValue()
-    }
-
-    fun getAllProducts() {
-        viewModelScope.launch {
-
-        }
     }
 
     private fun restoreValue() {
