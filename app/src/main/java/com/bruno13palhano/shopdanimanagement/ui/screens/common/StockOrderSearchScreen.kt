49e6ee54c28f0433d66a32b7a4ed06ab417abb69
bruno13palhano <bruno13palhano@gmail.com>
2023-08-24 21:38:56 -0300

    Add StockOrderSearchScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/StockOrderSearchScreen.kt
@@ -0,0 +1,35 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bruno13palhano.shopdanimanagement.ui.components.SearchContent
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.viewmodel.StockOrderSearchViewModel
+
+@Composable
+fun StockOrderSearchScreen(
+    isOrderedByCustomer: Boolean,
+    onItemClick: (id: Long) -> Unit,
+    navigateUp: () -> Unit,
+    viewModel: StockOrderSearchViewModel = hiltViewModel()
+) {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getSearchCache()
+    }
+
+    val stockOrderItems by viewModel.stockOrderItems.collectAsStateWithLifecycle()
+    val searchCacheList by viewModel.searchCache.collectAsStateWithLifecycle()
+
+    SearchContent(
+        stockProducts = stockOrderItems,
+        searchCacheList = searchCacheList,
+        onSearchClick = { search ->
+            viewModel.search(search, isOrderedByCustomer)
+            viewModel.insertSearch(search)
+        },
+        onItemClick = onItemClick,
+        navigateUp = navigateUp
+    )
+}
