acb9d739151564ccdfdbab834cfe3004a253644c
bruno13palhano <bruno13palhano@gmail.com>
2023-08-28 20:24:46 -0300

    Add new label

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.home
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PointOfSale
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,12 +23,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bruno13palhano.shopdanimanagement.R
-import com.bruno13palhano.shopdanimanagement.ui.components.Screen
 import com.bruno13palhano.shopdanimanagement.ui.components.SimpleItemList
+import com.bruno13palhano.shopdanimanagement.ui.navigation.HomeDestinations
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,10 +54,10 @@ fun HomeScreen(
         }
     ) {
         val options = listOf(
-            Screen.Sales,
-            Screen.Stock,
-            Screen.Orders,
-            Screen.Shopping
+            HomeTopScreen.Sales,
+            HomeTopScreen.Stock,
+            HomeTopScreen.Orders,
+            HomeTopScreen.Shopping
         )
         Column(modifier = Modifier
             .padding(it)
@@ -68,6 +74,13 @@ fun HomeScreen(
             }
         }
     }
+}
+
+sealed class HomeTopScreen(val route: String, val icon: ImageVector, @StringRes val resourceId: Int) {
+    object Stock: HomeTopScreen(HomeDestinations.HOME_STOCK_ROUTE, Icons.Filled.List, R.string.stock_label)
+    object Shopping: HomeTopScreen(HomeDestinations.HOME_SHOPPING_ROUTE, Icons.Filled.ShoppingCart, R.string.shopping_label)
+    object Sales: HomeTopScreen(HomeDestinations.HOME_SALES_ROUTE, Icons.Filled.PointOfSale, R.string.sales_label)
+    object Orders: HomeTopScreen(HomeDestinations.HOME_ORDERS_ROUTE, Icons.Filled.Checklist, R.string.orders_label)
 }
 
 @Preview(showBackground = true, showSystemUi = true)
