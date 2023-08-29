a70eea15ec645a7c9b5e8b614f5ff8cfc413f858
bruno13palhano <bruno13palhano@gmail.com>
2023-08-29 00:08:33 -0300

    Add list of items to InsightsScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/insights/InsightsScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/insights/InsightsScreen.kt
@@ -2,10 +2,14 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.insights
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.activity.compose.BackHandler
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BarChart
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -17,17 +21,23 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.SimpleItemList
+import com.bruno13palhano.shopdanimanagement.ui.navigation.InsightsDestinations
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun InsightsScreen(
+    onItemClick: (route: String) -> Unit,
     onIconMenuClick: () -> Unit,
     goHome: () -> Unit
 ) {
     InsightsContent(
+        onItemClick = onItemClick,
         onIconMenuClick = onIconMenuClick,
         goHome = goHome
     )
@@ -36,6 +46,7 @@ fun InsightsScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InsightsContent(
+    onItemClick: (route: String) -> Unit,
     onIconMenuClick: () -> Unit,
     goHome: () -> Unit
 ) {
@@ -55,10 +66,29 @@ fun InsightsContent(
             )
         }
     ) {
-        Column(modifier = Modifier.padding(it)) {
-
+        val items = listOf(
+            InsightsInnerScreen.Charts
+        )
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            items.forEach { screen ->
+                SimpleItemList(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    itemName = stringResource(id = screen.resourceId),
+                    imageVector = screen.icon,
+                    onClick = { onItemClick(screen.route) }
+                )
+            }
         }
     }
+}
+
+sealed class InsightsInnerScreen(val route: String, val icon: ImageVector, @StringRes val resourceId: Int) {
+    object Charts: InsightsInnerScreen(InsightsDestinations.INSIGHTS_CHARTS_ROUTE, Icons.Filled.BarChart, R.string.charts_label)
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -71,6 +101,7 @@ fun InsightsDynamicPreview() {
             color = MaterialTheme.colorScheme.background)
         {
             InsightsContent(
+                onItemClick = {},
                 onIconMenuClick = {},
                 goHome = {}
             )
@@ -90,6 +121,7 @@ fun InsightsPreview() {
             color = MaterialTheme.colorScheme.background)
         {
             InsightsContent(
+                onItemClick = {},
                 onIconMenuClick = {},
                 goHome = {}
             )
