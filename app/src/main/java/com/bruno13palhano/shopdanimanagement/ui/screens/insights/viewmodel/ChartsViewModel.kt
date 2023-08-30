006b0cb20dbcef7bc5dada6417f5c888d366c963
bruno13palhano <bruno13palhano@gmail.com>
2023-08-29 23:26:47 -0300

    Add a ViewModel for ChartsScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/insights/viewmodel/ChartsViewModel.kt
@@ -0,0 +1,15 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.insights.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.bruno13palhano.core.data.SaleData
+import com.bruno13palhano.core.data.di.DefaultSaleRepository
+import com.bruno13palhano.core.model.Sale
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChartsViewModel @Inject constructor(
+    @DefaultSaleRepository private val saleRepository: SaleData<Sale>
+) : ViewModel() {
+
+}
