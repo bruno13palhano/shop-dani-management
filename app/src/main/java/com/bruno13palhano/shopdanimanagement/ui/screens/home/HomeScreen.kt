52f3890abce16ce92062dac3a7c18e29515e783f
bruno13palhano <bruno13palhano@gmail.com>
2023-08-29 22:52:20 -0300

    Show a chart with last sales

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/home/HomeScreen.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -22,22 +24,39 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.components.SimpleItemList
+import com.bruno13palhano.shopdanimanagement.ui.components.rememberMarker
 import com.bruno13palhano.shopdanimanagement.ui.navigation.HomeDestinations
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.DateChartEntry
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.column.columnChart
+import com.patrykandpatrick.vico.compose.chart.edges.rememberFadingEdges
+import com.patrykandpatrick.vico.compose.m3.style.m3ChartStyle
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     onOptionsItemClick: (route: String) -> Unit,
     onMenuClick: () -> Unit,
+    viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val lastSalesEntry by viewModel.lastSales.collectAsStateWithLifecycle()
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -59,11 +78,48 @@ fun HomeScreen(
             HomeInnerScreen.Orders,
             HomeInnerScreen.Shopping
         )
+        val axisValuesFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+            try {
+                (chartValues.chartEntryModel.entries.first()
+                    .getOrNull(value.toInt()) as? DateChartEntry)
+                    ?.date
+                    .orEmpty()
+            } catch (ignored: Exception) { "0" }
+        }
+
         Column(modifier = Modifier
             .padding(it)
             .padding(horizontal = 8.dp, vertical = 4.dp)
             .verticalScroll(rememberScrollState())
         ) {
+            Text(
+                modifier = Modifier.padding(horizontal = 8.dp),
+                text = stringResource(id = R.string.last_sales_label)
+            )
+            ProvideChartStyle(
+                chartStyle = m3ChartStyle(
+                    entityColors = listOf(MaterialTheme.colorScheme.primary)
+                )
+            ) {
+                val marker = rememberMarker()
+                Chart(
+                    modifier = Modifier
+                        .padding(bottom = 8.dp)
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    chart = columnChart(),
+                    runInitialAnimation = true,
+                    chartModelProducer = lastSalesEntry,
+                    marker = marker,
+                    fadingEdges = rememberFadingEdges(),
+                    startAxis = startAxis(),
+                    bottomAxis = if (lastSalesEntry.getModel().entries.isEmpty()) {
+                        bottomAxis()
+                    } else {
+                        bottomAxis(guideline = null, valueFormatter = axisValuesFormatter)
+                    }
+                )
+            }
             options.forEach { screen ->
                 SimpleItemList(
                     modifier = Modifier.padding(vertical = 4.dp),
