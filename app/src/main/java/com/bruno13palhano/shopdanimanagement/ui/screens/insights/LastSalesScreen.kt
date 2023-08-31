0c04ab64608a6a8393ef6c1c39daf341f78b6820
bruno13palhano <bruno13palhano@gmail.com>
2023-08-30 23:16:08 -0300

    Add LastSalesScreen to insights

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/insights/LastSalesScreen.kt
@@ -0,0 +1,135 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.insights
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.rememberMarker
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.DateChartEntry
+import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.column.columnChart
+import com.patrykandpatrick.vico.compose.chart.edges.rememberFadingEdges
+import com.patrykandpatrick.vico.compose.m3.style.m3ChartStyle
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+
+@Composable
+fun LastSalesScreen(
+    navigateUp: () -> Unit
+) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LastSalesContent(
+    lastSalesEntry: ChartEntryModelProducer,
+    navigateUp: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.last_sales_label))},
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        val axisValuesFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+            try {
+                (chartValues.chartEntryModel.entries.first()
+                    .getOrNull(value.toInt()) as? DateChartEntry)
+                    ?.date.orEmpty()
+            } catch (ignored: Exception) { "0" }
+        }
+        Column(modifier = Modifier.padding(it)) {
+            ProvideChartStyle(
+                chartStyle = m3ChartStyle(
+                    entityColors = listOf(MaterialTheme.colorScheme.primary)
+                )
+            ) {
+                val marker = rememberMarker()
+                Chart(
+                    modifier = Modifier.fillMaxSize(),
+                    chart = columnChart(),
+                    runInitialAnimation = true,
+                    chartModelProducer = lastSalesEntry,
+                    marker = marker,
+                    fadingEdges = rememberFadingEdges(),
+                    startAxis = startAxis(),
+                    bottomAxis = if (lastSalesEntry.getModel().entries.isEmpty()) {
+                        bottomAxis()
+                    } else {
+                        bottomAxis(guideline = null, valueFormatter = axisValuesFormatter)
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun LastSalesDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            LastSalesContent(
+                lastSalesEntry = ChartEntryModelProducer(),
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun LastSalesPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            LastSalesContent(
+                lastSalesEntry = ChartEntryModelProducer(),
+                navigateUp = {}
+            )
+        }
+    }
+}
