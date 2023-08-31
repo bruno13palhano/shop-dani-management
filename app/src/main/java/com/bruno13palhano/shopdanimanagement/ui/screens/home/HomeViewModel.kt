dc12fa8fe1d21ff126d8aea2939f223d1057345f
bruno13palhano <bruno13palhano@gmail.com>
2023-08-30 21:55:14 -0300

    Refactor HomeViewModel

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/home/HomeViewModel.kt
@@ -21,7 +21,7 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     @DefaultSaleRepository private val saleRepository: SaleData<Sale>
 ) : ViewModel() {
-    private val currentDay = LocalDate.now(ZoneId.of("UTC"))
+    private val currentDay = LocalDate.now()
 
     val lastSales = saleRepository.getLastSales(0, 100)
         .map {
@@ -44,39 +44,17 @@ class HomeViewModel @Inject constructor(
         )
 
     private fun setDay(days: Array<Int>, date: Long, quantity: Int) {
-        when (LocalDateTime.ofInstant(Instant.ofEpochMilli(date), ZoneId.of("UTC")).toLocalDate()) {
-            currentDay -> {
-                days[0] += quantity
+        for (i in days.indices) {
+            if (LocalDateTime.ofInstant(Instant.ofEpochMilli(date), ZoneId.of("UTC")).toLocalDate()
+                == currentDay.minusDays(i.toLong())) {
+                days[i] += quantity
             }
-            currentDay.minusDays(1) -> {
-                days[1] += quantity
-            }
-            currentDay.minusDays(2) -> {
-                days[2] += quantity
-            }
-            currentDay.minusDays(3) -> {
-                days[3] += quantity
-            }
-            currentDay.minusDays(4) -> {
-                days[4] += quantity
-            }
-            currentDay.minusDays(5) -> {
-                days[5] += quantity
-            }
-            currentDay.minusDays(6) -> {
-                days[6] += quantity
-            }
-            else -> {}
         }
     }
 
     private fun setChartEntries(chart: MutableList<Pair<String, Float>>, days: Array<Int>) {
-        chart.add(Pair(currentDay.minusDays(6).dayOfWeek.name, days[6].toFloat()))
-        chart.add(Pair(currentDay.minusDays(5).dayOfWeek.name, days[5].toFloat()))
-        chart.add(Pair(currentDay.minusDays(4).dayOfWeek.name, days[4].toFloat()))
-        chart.add(Pair(currentDay.minusDays(3).dayOfWeek.name, days[3].toFloat()))
-        chart.add(Pair(currentDay.minusDays(2).dayOfWeek.name, days[2].toFloat()))
-        chart.add(Pair(currentDay.minusDays(1).dayOfWeek.name, days[1].toFloat()))
-        chart.add(Pair(currentDay.dayOfWeek.name, days[0].toFloat()))
+        for (i in days.size-1 downTo 0) {
+            chart.add(Pair(currentDay.minusDays(i.toLong()).dayOfWeek.name, days[i].toFloat()))
+        }
     }
 }
