a65f4f9a72f5c9dc22254bc608bac70a6284536d
bruno13palhano <bruno13palhano@gmail.com>
2023-09-01 12:04:26 -0300

    Add composed char and more options menu with range of days

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/insights/StockOrdersSalesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/insights/StockOrdersSalesScreen.kt
@@ -1,11 +1,16 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.insights
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,24 +20,99 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
+import com.bruno13palhano.shopdanimanagement.ui.components.rememberMarker
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.DateChartEntry
+import com.bruno13palhano.shopdanimanagement.ui.screens.insights.viewmodel.StockOrdersSalesViewModel
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.column.columnChart
+import com.patrykandpatrick.vico.compose.chart.edges.rememberFadingEdges
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollSpec
+import com.patrykandpatrick.vico.compose.component.shapeComponent
+import com.patrykandpatrick.vico.compose.component.textComponent
+import com.patrykandpatrick.vico.compose.dimensions.dimensionsOf
+import com.patrykandpatrick.vico.compose.m3.style.m3ChartStyle
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.chart.composed.plus
+import com.patrykandpatrick.vico.core.chart.line.LineChart
+import com.patrykandpatrick.vico.core.component.shape.Shapes
+import com.patrykandpatrick.vico.core.entry.ChartEntryModel
+import com.patrykandpatrick.vico.core.entry.composed.ComposedChartEntryModelProducer
+import com.patrykandpatrick.vico.core.scroll.InitialScroll
 
 @Composable
 fun StockOrdersSalesScreen(
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    viewModel: StockOrdersSalesViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.setStockOrdersSalesRange(7)
+    }
+    val stockVsOrdersEntries by viewModel.allSales.collectAsStateWithLifecycle()
+    val menuOptions = arrayOf(
+        stringResource(id = R.string.last_7_days_label),
+        stringResource(id = R.string.last_21_days_label),
+        stringResource(id = R.string.last_31_days_label)
+    )
+    val sevenDaysTitle = stringResource(id = R.string.last_7_days_label)
+    val twentyOneDaysTitle = stringResource(id = R.string.last_21_days_label)
+    val thirtyOneDaysTitle = stringResource(id = R.string.last_31_days_label)
+    var chartTitle by remember { mutableStateOf(sevenDaysTitle) }
 
+    StockOrdersSalesContent(
+        chartTitle = chartTitle,
+        chartEntry = stockVsOrdersEntries,
+        menuOptions = menuOptions,
+        onMenuItemClick = { index ->
+            when (index) {
+                0 -> {
+                    viewModel.setStockOrdersSalesRange(7)
+                    chartTitle = sevenDaysTitle
+                }
+                1 -> {
+                    viewModel.setStockOrdersSalesRange(21)
+                    chartTitle = twentyOneDaysTitle
+                }
+                2 -> {
+                    viewModel.setStockOrdersSalesRange(31)
+                    chartTitle = thirtyOneDaysTitle
+                }
+            }
+        },
+        navigateUp = navigateUp
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StockOrdersSalesContent(
+    chartTitle: String,
+    chartEntry: ComposedChartEntryModelProducer<ChartEntryModel>,
+    menuOptions: Array<String>,
+    onMenuItemClick: (index: Int) -> Unit,
     navigateUp: () -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -44,12 +124,79 @@ fun StockOrdersSalesContent(
                             contentDescription = stringResource(id = R.string.up_button_label)
                         )
                     }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.more_options_label)
+                            )
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MoreOptionsMenu(
+                                    items = menuOptions,
+                                    expanded = expanded,
+                                    onDismissRequest = { expanded = it },
+                                    onClick = onMenuItemClick
+                                )
+                            }
+                        }
+                    }
                 }
             )
         }
     ) {
+        val axisValuesFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+            try {
+                (chartValues.chartEntryModel.entries.first()
+                    .getOrNull(value.toInt()) as? DateChartEntry)
+                    ?.date.orEmpty()
+            } catch (ignored: Exception) { "0" }
+        }
         Column(modifier = Modifier.padding(it)) {
+            ProvideChartStyle(
+                chartStyle = m3ChartStyle(
+                    entityColors = listOf(
+                        MaterialTheme.colorScheme.primary,
+                        MaterialTheme.colorScheme.secondary
+                    )
+                )
+            ) {
+                val columnChart = columnChart()
+                val lineChart = lineChart(lines = listOf(LineChart.LineSpec(lineColor = Color.BLUE)))
+                val composedChart = remember(columnChart, lineChart) { columnChart + lineChart }
 
+                Chart(
+                    modifier = Modifier.fillMaxSize(),
+                    chart = composedChart,
+                    runInitialAnimation = true,
+                    chartModelProducer = chartEntry,
+                    marker = rememberMarker(),
+                    fadingEdges = rememberFadingEdges(),
+                    startAxis = startAxis(),
+                    bottomAxis = if (chartEntry.getModel().entries.isEmpty()) {
+                        bottomAxis()
+                    } else {
+                        bottomAxis(
+                            guideline = null,
+                            valueFormatter = axisValuesFormatter,
+                            titleComponent = textComponent(
+                                color = MaterialTheme.colorScheme.onBackground,
+                                background = shapeComponent(Shapes.pillShape, MaterialTheme.colorScheme.primaryContainer),
+                                padding = dimensionsOf(horizontal = 8.dp, vertical = 2.dp),
+                                margins = dimensionsOf(top = 8.dp, start = 8.dp, end = 8.dp, bottom = 16.dp),
+                                typeface = Typeface.MONOSPACE
+                            ),
+                            title = chartTitle
+                        )
+                    },
+                    chartScrollSpec = rememberChartScrollSpec(initialScroll = InitialScroll.End)
+                )
+            }
         }
     }
 }
@@ -64,6 +211,10 @@ fun StockOrdersSalesDynamicPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             StockOrdersSalesContent(
+                chartTitle = stringResource(id = R.string.last_7_days_label),
+                chartEntry = ComposedChartEntryModelProducer(),
+                menuOptions = emptyArray(),
+                onMenuItemClick = {},
                 navigateUp = {}
             )
         }
@@ -82,6 +233,10 @@ fun StockOrdersSalesPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             StockOrdersSalesContent(
+                chartTitle = stringResource(id = R.string.last_21_days_label),
+                chartEntry = ComposedChartEntryModelProducer(),
+                menuOptions = emptyArray(),
+                onMenuItemClick = {},
                 navigateUp = {}
             )
         }
