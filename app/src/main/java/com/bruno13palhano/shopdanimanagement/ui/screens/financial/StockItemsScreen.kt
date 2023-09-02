7979c539ee3a502f003527593a0308a09b0f04a8
bruno13palhano <bruno13palhano@gmail.com>
2023-09-01 23:30:50 -0300

    Add more options menu

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/StockItemsScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/StockItemsScreen.kt
@@ -1,11 +1,14 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.financial
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,10 +18,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
@@ -31,8 +40,12 @@ fun StockItemsScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StockItemContent(
+    menuOptions: Array<String>,
+    onMenuItemClick: (index: Int) -> Unit,
     navigateUp: () -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -43,6 +56,28 @@ fun StockItemContent(
                             imageVector = Icons.Filled.ArrowBack,
                             contentDescription = stringResource(id = R.string.up_button_label)
                         )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = true } ) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.more_options_label)
+                            )
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MoreOptionsMenu(
+                                    items = menuOptions,
+                                    expanded = expanded,
+                                    onDismissRequest = { expanded = it },
+                                    onClick = onMenuItemClick
+                                )
+                            }
+                        }
                     }
                 }
             )
@@ -64,6 +99,8 @@ fun StockItemDynamicPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             StockItemContent(
+                menuOptions = emptyArray(),
+                onMenuItemClick = {},
                 navigateUp = {}
             )
         }
@@ -82,6 +119,8 @@ fun StockItemPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             StockItemContent(
+                menuOptions = emptyArray(),
+                onMenuItemClick = {},
                 navigateUp = {}
             )
         }
