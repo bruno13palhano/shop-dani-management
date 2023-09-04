01af893faa3d639c68fadff7b26af9f2c90f63d6
bruno13palhano <bruno13palhano@gmail.com>
2023-09-04 11:20:31 -0300

    Add chart

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/CustomerInfoScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/CustomerInfoScreen.kt
@@ -1,11 +1,14 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.customers
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
@@ -30,6 +33,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,12 +43,37 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.rememberAsyncImagePainter
 import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.components.clearFocusOnKeyboardDismiss
+import com.bruno13palhano.shopdanimanagement.ui.components.rememberMarker
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.DateChartEntry
 import com.bruno13palhano.shopdanimanagement.ui.screens.customers.viewmodel.CustomerInfoViewModel
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.edges.rememberFadingEdges
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.chart.scroll.rememberChartScrollSpec
+import com.patrykandpatrick.vico.compose.component.shapeComponent
+import com.patrykandpatrick.vico.compose.component.textComponent
+import com.patrykandpatrick.vico.compose.dimensions.dimensionsOf
+import com.patrykandpatrick.vico.compose.legend.verticalLegend
+import com.patrykandpatrick.vico.compose.legend.verticalLegendItem
+import com.patrykandpatrick.vico.compose.m3.style.m3ChartStyle
+import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
+import com.patrykandpatrick.vico.compose.style.currentChartStyle
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.component.shape.ShapeComponent
+import com.patrykandpatrick.vico.core.component.shape.Shapes
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.legend.VerticalLegend
+import com.patrykandpatrick.vico.core.scroll.InitialScroll
 
 @Composable
 fun CustomerInfoScreen(
@@ -52,7 +82,20 @@ fun CustomerInfoScreen(
     navigateUp: () -> Unit,
     viewModel: CustomerInfoViewModel = hiltViewModel()
 ) {
+    val entry by viewModel.entry.collectAsStateWithLifecycle()
 
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getCustomerPurchases(customerId)
+    }
+
+    CustomerInfoContent(
+        name = "",
+        address = "",
+        photo = "",
+        entry = entry,
+        onEditIconClick = onEditIconClick,
+        navigateUp = navigateUp
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -61,6 +104,7 @@ fun CustomerInfoContent(
     name: String,
     address: String,
     photo: String,
+    entry: ChartEntryModelProducer,
     onEditIconClick: () -> Unit,
     navigateUp: () -> Unit
 ) {
@@ -183,6 +227,75 @@ fun CustomerInfoContent(
                     )
                 }
             }
+            ElevatedCard(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 8.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.owing_tag, "120.99")
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.amount_of_all_purchases_tag, "1449.90")
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.last_purchase_tag, "77.99")
+                )
+            }
+            val axisValuesFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+                try {
+                    (chartValues.chartEntryModel.entries.first()
+                        .getOrNull(value.toInt()) as? DateChartEntry)
+                        ?.date.orEmpty()
+                } catch (ignored: Exception) { "0" }
+            }
+            ProvideChartStyle(
+                chartStyle = m3ChartStyle(entityColors = listOf(MaterialTheme.colorScheme.tertiary))
+            ) {
+                Chart(
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                        .fillMaxWidth()
+                        .height(264.dp),
+                    chart = lineChart(),
+                    runInitialAnimation = true,
+                    chartModelProducer = entry,
+                    marker = rememberMarker(),
+                    fadingEdges = rememberFadingEdges(),
+                    startAxis = startAxis(
+                        titleComponent = textComponent(
+                            color = MaterialTheme.colorScheme.onBackground,
+                            background = shapeComponent(Shapes.pillShape, MaterialTheme.colorScheme.primaryContainer),
+                            padding = dimensionsOf(horizontal = 8.dp, vertical = 2.dp),
+                            margins = dimensionsOf(end = 8.dp),
+                            typeface = Typeface.MONOSPACE
+                        ),
+                        title = "Amount of purchases"
+                    ),
+                    bottomAxis = if (entry.getModel().entries.isEmpty()) {
+                        bottomAxis()
+                    } else {
+                        bottomAxis(
+                            guideline = null,
+                            valueFormatter = axisValuesFormatter,
+                            titleComponent = textComponent(
+                                color = MaterialTheme.colorScheme.onBackground,
+                                background = shapeComponent(Shapes.pillShape, MaterialTheme.colorScheme.primaryContainer),
+                                padding = dimensionsOf(horizontal = 8.dp, vertical = 2.dp),
+                                margins = dimensionsOf(top = 8.dp, start = 8.dp, end = 8.dp),
+                                typeface = Typeface.MONOSPACE
+                            ),
+                            title = "Last purchases"
+                        )
+                    },
+                    chartScrollSpec = rememberChartScrollSpec(initialScroll = InitialScroll.End)
+                )
+            }
         }
     }
 }
@@ -200,6 +313,7 @@ fun CustomerInfoDynamicPreview() {
                 name = "",
                 address = "",
                 photo = "",
+                entry = ChartEntryModelProducer(),
                 onEditIconClick = {},
                 navigateUp = {}
             )
@@ -222,6 +336,7 @@ fun CustomerInfoPreview() {
                 name = "",
                 address = "",
                 photo = "",
+                entry = ChartEntryModelProducer(),
                 onEditIconClick = {},
                 navigateUp = {}
             )
