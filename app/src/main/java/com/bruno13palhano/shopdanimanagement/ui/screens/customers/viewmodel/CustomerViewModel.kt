9c2849599f2c9bb7fe33db009cb543f83b7c2066
bruno13palhano <bruno13palhano@gmail.com>
2023-09-03 22:16:44 -0300

    Add CustomerViewModel

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomerViewModel.kt
@@ -0,0 +1,12 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.customers.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CustomerViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
