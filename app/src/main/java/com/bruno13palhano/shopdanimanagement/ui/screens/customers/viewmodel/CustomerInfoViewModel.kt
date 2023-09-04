c10cacfc074bb76488e0030dec1963f891940133
bruno13palhano <bruno13palhano@gmail.com>
2023-09-04 12:29:00 -0300

    Get Customer info

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomerInfoViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomerInfoViewModel.kt
@@ -2,14 +2,18 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.customers.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.CustomerData
 import com.bruno13palhano.core.data.SaleData
+import com.bruno13palhano.core.data.di.DefaultCustomerRepository
 import com.bruno13palhano.core.data.di.DefaultSaleRepository
+import com.bruno13palhano.core.model.Customer
 import com.bruno13palhano.core.model.Sale
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.DateChartEntry
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -22,17 +26,60 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CustomerInfoViewModel @Inject constructor(
+    @DefaultCustomerRepository private val customerRepository: CustomerData<Customer>,
     @DefaultSaleRepository private val saleRepository: SaleData<Sale>
 ) : ViewModel() {
     private val currentDay = LocalDate.now()
 
-    private val _customerPurchases = MutableStateFlow(ChartEntryModelProducer())
-    val entry = _customerPurchases
+    private val _customerPurchases = MutableStateFlow(emptyList<Sale>())
+    private val _customer = MutableStateFlow(Customer(0L, "", "", "", "", ""))
+
+    val customerInfo = combine(_customerPurchases, _customer) { purchases, customer ->
+        var owingValue = 0F
+        var purchasesValue = 0F
+        var lastPurchaseValue = 0F
+        purchases.filter { !it.isPaidByCustomer }.map { owingValue += it.salePrice }
+        purchases.map { purchasesValue += it.salePrice }
+        purchases.map { sale ->
+            if (sale.salePrice != 0F) lastPurchaseValue = sale.salePrice
+        }
+
+        CustomerInfo(
+            name = customer.name,
+            address = customer.address,
+            photo = customer.photo,
+            owingValue = owingValue.toString(),
+            purchasesValue = purchasesValue.toString(),
+            lastPurchaseValue = lastPurchaseValue.toString()
+        )
+    }
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = CustomerInfo()
+        )
+
+    private val _entry = MutableStateFlow(ChartEntryModelProducer())
+    val entry = _entry
         .stateIn(
             scope = viewModelScope,
             started = WhileSubscribed(5_000),
             initialValue = ChartEntryModelProducer()
         )
+
+    fun getCustomerInfo(customerId: Long) {
+        viewModelScope.launch {
+            saleRepository.getByCustomerId(customerId).collect {
+                _customerPurchases.value = it
+            }
+        }
+
+        viewModelScope.launch {
+            customerRepository.getById(customerId).collect {
+                _customer.value = it
+            }
+        }
+    }
 
     fun getCustomerPurchases(customerId: Long) {
         val days = Array(31) { 0 }
@@ -51,7 +98,7 @@ class CustomerInfoViewModel @Inject constructor(
                     )
                 }
                 .collect {
-                    _customerPurchases.value = it
+                    _entry.value = it
                 }
         }
     }
@@ -75,4 +122,13 @@ class CustomerInfoViewModel @Inject constructor(
             )
         }
     }
+
+    data class CustomerInfo(
+        val name: String = "",
+        val address: String = "",
+        val photo: String = "",
+        val owingValue: String = "",
+        val purchasesValue: String = "",
+        val lastPurchaseValue: String = "",
+    )
 }
