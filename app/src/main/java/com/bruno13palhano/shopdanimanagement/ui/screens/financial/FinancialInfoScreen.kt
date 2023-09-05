193be917adf32016aa2aef706fbf4fcaf6eac4b0
bruno13palhano <bruno13palhano@gmail.com>
2023-09-05 11:06:43 -0300

    Gets financial info from viewModel

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/FinancialInfoScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/FinancialInfoScreen.kt
@@ -3,9 +3,11 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.financial
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -15,17 +17,30 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel.FinancialInfoViewModel
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun FinancialInfoScreen(
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    viewModel: FinancialInfoViewModel = hiltViewModel()
 ) {
+    val financialInfo by viewModel.financial.collectAsStateWithLifecycle()
+
     FinancialInfoContent(
+        allSales = financialInfo.allSales,
+        stockSales = financialInfo.stockSales,
+        ordersSales = financialInfo.ordersSales,
+        profit = financialInfo.profit,
+        shopping = financialInfo.shopping,
         navigateUp = navigateUp
     )
 }
@@ -33,6 +48,11 @@ fun FinancialInfoScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FinancialInfoContent(
+    allSales: Float,
+    stockSales: Float,
+    ordersSales: Float,
+    profit: Float,
+    shopping: Float,
     navigateUp: () -> Unit
 ) {
     Scaffold(
@@ -51,7 +71,38 @@ fun FinancialInfoContent(
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
-
+            ElevatedCard(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.all_sales_tag, allSales)
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.stock_sales_tag, stockSales)
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.orders_sales_tag, ordersSales)
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.profit_tag, profit)
+                )
+                Text(
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .fillMaxWidth(),
+                    text = stringResource(id = R.string.shopping_tag, shopping)
+                )
+            }
         }
     }
 }
@@ -66,6 +117,11 @@ fun FinancialInfoDynamicPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             FinancialInfoContent(
+                allSales = 5234.99F,
+                stockSales = 2543.44F,
+                ordersSales = 1456.55F,
+                profit = 3000.99F,
+                shopping = 1234.99F,
                 navigateUp = {}
             )
         }
@@ -84,6 +140,11 @@ fun FinancialInfoPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             FinancialInfoContent(
+                allSales = 5234.99F,
+                stockSales = 2543.44F,
+                ordersSales = 1456.55F,
+                profit = 3000.99F,
+                shopping = 1234.99F,
                 navigateUp = {}
             )
         }
