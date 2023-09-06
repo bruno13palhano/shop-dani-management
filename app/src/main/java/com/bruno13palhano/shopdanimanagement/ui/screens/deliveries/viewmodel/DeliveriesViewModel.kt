600e78665dd5ebf0cc7cb7eb4c1414a1649f844d
bruno13palhano <bruno13palhano@gmail.com>
2023-09-05 22:46:01 -0300

    Add DeliveriesViewModel

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/viewmodel/DeliveriesViewModel.kt
@@ -0,0 +1,12 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.deliveries.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DeliveriesViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
