0f1c7a0d9dd5fb531e5495450e33b8dd1f0b58a6
bruno13palhano <bruno13palhano@gmail.com>
2023-09-05 22:44:56 -0300

    Add Up button icon

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
@@ -4,7 +4,11 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,7 +25,9 @@ import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 fun DeliveriesScreen(
     navigateUp: () -> Unit
 ) {
-
+    DeliveriesContent(
+        navigateUp = navigateUp
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -32,7 +38,15 @@ fun DeliveriesContent(
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.deliveries_label)) }
+                title = { Text(text = stringResource(id = R.string.deliveries_label)) },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
             )
         }
     ) {
