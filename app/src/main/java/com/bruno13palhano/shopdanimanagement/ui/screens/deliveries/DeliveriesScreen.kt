79b72d0c61c96d21f278a4ecd3c5c87b6b74161d
bruno13palhano <bruno13palhano@gmail.com>
2023-09-08 01:32:46 -0300

    Extract deliveries previews to a file

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
@@ -1,25 +1,19 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.deliveries
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import com.bruno13palhano.shopdanimanagement.R
-import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun DeliveriesScreen(
@@ -52,40 +46,6 @@ fun DeliveriesContent(
     ) {
         Column(modifier = Modifier.padding(it)) {
 
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun DeliveriesDynamicPreview() {
-    ShopDaniManagementTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            DeliveriesContent(
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun DeliveriesPreview() {
-    ShopDaniManagementTheme(
-        dynamicColor = false
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            DeliveriesContent(
-                navigateUp = {}
-            )
         }
     }
 }
---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/DeliveriesPreview.kt
@@ -0,0 +1,45 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.previews
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bruno13palhano.shopdanimanagement.ui.screens.deliveries.DeliveriesContent
+import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun DeliveriesDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            DeliveriesContent(
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun DeliveriesPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            DeliveriesContent(
+                navigateUp = {}
+            )
+        }
+    }
+}
