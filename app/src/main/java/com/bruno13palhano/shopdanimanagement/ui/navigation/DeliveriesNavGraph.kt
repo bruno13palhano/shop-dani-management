c6deb4eacd164e56fd26d1e6ac08ffcb2f1034e1
bruno13palhano <bruno13palhano@gmail.com>
2023-09-08 23:02:01 -0300

    Add new routes to deliveriesNavGraph

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/DeliveriesNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/DeliveriesNavGraph.kt
@@ -16,15 +16,31 @@ fun NavGraphBuilder.deliveriesNAvGraph(
         startDestination = DeliveriesDestinations.DELIVERIES_MAIN_ROUTE,
         route = HomeDestinations.HOME_DELIVERIES_ROUTE
     ) {
-        showBottomMenu(true)
         composable(route = DeliveriesDestinations.DELIVERIES_MAIN_ROUTE) {
+            showBottomMenu(true)
             DeliveriesScreen(
+                onItemClick = { deliveryId ->
+                    navController.navigate(route = "$DeliveriesDestinations.DELIVERIES_EDIT_DELIVERY_ROUTE$deliveryId")
+                },
+                onAddButtonClick = {
+                    navController.navigate(route = DeliveriesDestinations.DELIVERIES_NEW_DELIVERY_ROUTE)
+                },
                 navigateUp = { navController.navigateUp() }
             )
+        }
+        composable(route = DeliveriesDestinations.DELIVERIES_NEW_DELIVERY_ROUTE) {
+            showBottomMenu(true)
+        }
+        composable(route = DeliveriesDestinations.DELIVERIES_EDIT_DELIVERY_WITH_ID_ROUTE) {
+            showBottomMenu(true)
+
         }
     }
 }
 
 object DeliveriesDestinations {
     const val DELIVERIES_MAIN_ROUTE = "deliveries_main_route"
+    const val DELIVERIES_NEW_DELIVERY_ROUTE = "deliveries_new_delivery_route"
+    const val DELIVERIES_EDIT_DELIVERY_ROUTE = "deliveries_edit_delivery_route"
+    const val DELIVERIES_EDIT_DELIVERY_WITH_ID_ROUTE = "$DELIVERIES_EDIT_DELIVERY_ROUTE{$ITEM_ID}"
 }
