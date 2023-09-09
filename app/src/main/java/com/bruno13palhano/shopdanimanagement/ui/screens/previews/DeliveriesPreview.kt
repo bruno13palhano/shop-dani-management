c90fb8ccf70c74750d7d22356249312d9b1b718c
bruno13palhano <bruno13palhano@gmail.com>
2023-09-08 21:51:16 -0300

    Add a list with mock deliveries to preview

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/DeliveriesPreview.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/DeliveriesPreview.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import com.bruno13palhano.shopdanimanagement.ui.screens.deliveries.DeliveriesContent
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
@@ -20,6 +21,8 @@ fun DeliveriesDynamicPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             DeliveriesContent(
+                deliveries = deliveries,
+                onItemClick = {},
                 navigateUp = {}
             )
         }
@@ -38,8 +41,20 @@ fun DeliveriesPreview() {
             color = MaterialTheme.colorScheme.background
         ) {
             DeliveriesContent(
+                deliveries = deliveries,
+                onItemClick = {},
                 navigateUp = {}
             )
         }
     }
 }
+
+private val deliveries = listOf(
+    CommonItem(1L, "", "Bruno Barbosa", "Essencial", "Rua 15 de novembro"),
+    CommonItem(2L, "", "Josué Barbosa", "Essencial", "Rua 15 de novembro"),
+    CommonItem(3L, "", "Daniela Barbosa", "Homem", "Rua 15 de novembro"),
+    CommonItem(4L, "", "Brenda Barbosa", "Essencial", "Rua 15 de novembro"),
+    CommonItem(5L, "", "Helena Barbosa", "Una", "Rua 15 de novembro"),
+    CommonItem(6L, "", "Socorro Barbosa", "Luna", "Rua 15 de novembro"),
+    CommonItem(7L, "", "Fernando Barbosa", "Kaiak", "Rua 15 de novembro"),
+ )
