620b26b3bb23d7074fa319e96c02efcdfeb7c0bd
bruno13palhano <bruno13palhano@gmail.com>
2023-09-08 21:43:50 -0300

    Gets deliveries from viewModel

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
@@ -1,7 +1,8 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.deliveries
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,15 +12,27 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.CommonItemList
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
+import com.bruno13palhano.shopdanimanagement.ui.screens.deliveries.viewmodel.DeliveriesViewModel
 
 @Composable
 fun DeliveriesScreen(
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    onItemClick: (id: Long) -> Unit,
+    viewModel: DeliveriesViewModel = hiltViewModel()
 ) {
+    val deliveries by viewModel.deliveries.collectAsStateWithLifecycle()
     DeliveriesContent(
+        deliveries = deliveries,
+        onItemClick = onItemClick,
         navigateUp = navigateUp
     )
 }
@@ -27,6 +40,8 @@ fun DeliveriesScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DeliveriesContent(
+    deliveries: List<CommonItem>,
+    onItemClick: (id: Long) -> Unit,
     navigateUp: () -> Unit
 ) {
     Scaffold(
@@ -44,8 +59,16 @@ fun DeliveriesContent(
             )
         }
     ) {
-        Column(modifier = Modifier.padding(it)) {
-
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(items = deliveries, key = { delivery -> delivery.id }) { delivery ->
+                CommonItemList(
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                    title = delivery.title,
+                    subtitle = delivery.subtitle,
+                    description = delivery.description,
+                    onClick = { onItemClick(delivery.id) }
+                )
+            }
         }
     }
 }
