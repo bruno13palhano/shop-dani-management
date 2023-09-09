cdcc90d364fe03f89db4b762e3d9d660fef9f41c
bruno13palhano <bruno13palhano@gmail.com>
2023-09-08 21:43:27 -0300

    Gets deliveries

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/viewmodel/DeliveriesViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/viewmodel/DeliveriesViewModel.kt
@@ -1,12 +1,36 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.deliveries.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.DeliveryData
+import com.bruno13palhano.core.data.di.DefaultDeliveryRepository
+import com.bruno13palhano.core.model.Delivery
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class DeliveriesViewModel @Inject constructor(
-
+    @DefaultDeliveryRepository private val deliveryRepository: DeliveryData<Delivery>
 ) : ViewModel() {
-
+    val deliveries = deliveryRepository.getAll()
+        .map {
+            it.map { delivery ->
+                CommonItem(
+                    id = delivery.id,
+                    photo = "",
+                    title = delivery.customerName,
+                    subtitle = delivery.productName,
+                    description = delivery.address
+                )
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
 }
