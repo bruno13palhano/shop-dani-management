36cb6177a1a0ed6715cece95bcda5da95c947ac3
bruno13palhano <bruno13palhano@gmail.com>
2023-09-09 22:03:34 -0300

    Add Delivery content

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveryScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveryScreen.kt
@@ -1,23 +1,74 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.deliveries
 
+import android.icu.text.DecimalFormat
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.EditCalendar
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.LocationCity
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.PriceCheck
+import androidx.compose.material.icons.filled.Title
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.clearFocusOnKeyboardDismiss
+import com.bruno13palhano.shopdanimanagement.ui.screens.deliveries.viewmodel.DeliveryViewModel
+import java.util.Locale
 
 @Composable
 fun DeliveryScreen(
-    screenTitle: String,
-    isEditable: Boolean,
-    navigateUp: () -> Unit
+    deliveryId: Long,
+    navigateUp: () -> Unit,
+    viewModel: DeliveryViewModel = hiltViewModel()
 ) {
     DeliveryContent(
-        screenTitle = screenTitle,
-        isEditable = isEditable,
+        name = "",
+        email = "",
+        address = "",
+        phoneNumber = "",
+        productName = "",
+        price = "",
+        shippingDate = "",
+        deliveryDate = "",
+        delivered = false,
+        onNameChange = {},
+        onEmailChange = {},
+        onAddressChange = {},
+        onPhoneNumberChange = {},
+        onProductNameChange = {},
+        onDeliveredChange = {},
+        onPriceChange = {},
+        onShippingDateClick = {},
+        onDeliveryDateClick = {},
+        onDoneButtonClick = {},
         navigateUp = navigateUp
     )
 }
@@ -25,19 +76,339 @@ fun DeliveryScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DeliveryContent(
-    screenTitle: String,
-    isEditable: Boolean,
-    navigateUp: () -> Unit
+    name: String,
+    email: String,
+    address: String,
+    phoneNumber: String,
+    productName: String,
+    price: String,
+    shippingDate: String,
+    deliveryDate: String,
+    delivered: Boolean,
+    onNameChange: (name: String) -> Unit,
+    onEmailChange: (email: String) -> Unit,
+    onAddressChange: (address: String) -> Unit,
+    onPhoneNumberChange: (phoneNumber: String) -> Unit,
+    onProductNameChange: (productName: String) -> Unit,
+    onDeliveredChange: (delivered: Boolean) -> Unit,
+    onPriceChange: (price: String) -> Unit,
+    onShippingDateClick: () -> Unit,
+    onDeliveryDateClick: () -> Unit,
+    onDoneButtonClick: () -> Unit,
+    navigateUp: () -> Unit,
 ) {
+    val decimalFormat = DecimalFormat.getInstance(Locale.getDefault()) as DecimalFormat
+    val decimalSeparator = decimalFormat.decimalFormatSymbols.decimalSeparator
+    val pattern = remember { Regex("^\\d*\\$decimalSeparator?\\d*\$") }
+    val patternInt = remember { Regex("^\\d*") }
+
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = screenTitle) }
+                title = { Text(text = stringResource(id = R.string.delivery_label),) },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onDoneButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = stringResource(id = R.string.done_label)
+                )
+            }
         }
     ) {
         Column(modifier = Modifier.padding(it)) {
-
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = name,
+                onValueChange = onNameChange,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = stringResource(id = R.string.name_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.name_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_name_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = email,
+                onValueChange = onEmailChange,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Email,
+                        contentDescription = stringResource(id = R.string.email_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.email_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_email_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = address,
+                onValueChange = onAddressChange,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.LocationCity,
+                        contentDescription = stringResource(id = R.string.address_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.address_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_address_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = phoneNumber,
+                onValueChange = { phoneNumberValue ->
+                    if (phoneNumberValue.isEmpty() || phoneNumberValue.matches(patternInt)) {
+                        onPhoneNumberChange(phoneNumberValue)
+                    }
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Phone,
+                        contentDescription = stringResource(id = R.string.phone_number_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Number
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.phone_number_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_phone_number_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = productName,
+                onValueChange = onProductNameChange,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Title,
+                        contentDescription = stringResource(id = R.string.product_name_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.product_name_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_product_name_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = price,
+                onValueChange = { priceValue ->
+                    if (priceValue.isEmpty() || priceValue.matches(pattern)) {
+                        onPriceChange(priceValue)
+                    }
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.PriceCheck,
+                        contentDescription = stringResource(id = R.string.sale_price_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.sale_price_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_sale_price_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss()
+                    .onFocusChanged { focusState ->
+                        if (focusState.hasFocus) {
+                            onShippingDateClick()
+                        }
+                    },
+                value = shippingDate,
+                onValueChange = {},
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.CalendarMonth,
+                        contentDescription = stringResource(id = R.string.shipping_date_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Decimal
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                readOnly = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.shipping_date_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_shipping_date_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss()
+                    .onFocusChanged { focusState ->
+                        if (focusState.hasFocus) {
+                            onDeliveryDateClick()
+                        }
+                    },
+                value = deliveryDate,
+                onValueChange = {},
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.EditCalendar,
+                        contentDescription = stringResource(id = R.string.delivery_date_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                readOnly = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.delivery_date_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_delivery_date_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = delivered,
+                    onCheckedChange = onDeliveredChange
+                )
+                Text(text = stringResource(id = R.string.delivered_label))
+            }
         }
     }
 }
