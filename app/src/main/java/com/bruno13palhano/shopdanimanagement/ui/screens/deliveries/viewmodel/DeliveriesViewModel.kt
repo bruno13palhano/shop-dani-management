9d9aeac71ff6616a033d1f95516c4beb0c7f6636
bruno13palhano <bruno13palhano@gmail.com>
2023-09-11 20:29:20 -0300

    Gets deliveries conditionally

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/viewmodel/DeliveriesViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/viewmodel/DeliveriesViewModel.kt
@@ -7,16 +7,19 @@ import com.bruno13palhano.core.data.di.DefaultDeliveryRepository
 import com.bruno13palhano.core.model.Delivery
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DeliveriesViewModel @Inject constructor(
     @DefaultDeliveryRepository private val deliveryRepository: DeliveryData<Delivery>
 ) : ViewModel() {
-    val deliveries = deliveryRepository.getAll()
+    private var _deliveries = MutableStateFlow(emptyList<Delivery>())
+    val deliveries = _deliveries
         .map {
             it.map { delivery ->
                 CommonItem(
@@ -33,4 +36,20 @@ class DeliveriesViewModel @Inject constructor(
             started = WhileSubscribed(5_000),
             initialValue = emptyList()
         )
+
+    fun getAllDeliveries() {
+        viewModelScope.launch {
+            deliveryRepository.getAll().collect {
+                _deliveries.value = it
+            }
+        }
+    }
+
+    fun getDeliveries(delivered: Boolean) {
+        viewModelScope.launch {
+            deliveryRepository.getDeliveries(delivered).collect {
+                _deliveries.value = it
+            }
+        }
+    }
 }
