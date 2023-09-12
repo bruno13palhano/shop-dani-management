81f4e6adfc082f612e07c695d0b25b38d13f7ce4
bruno13palhano <bruno13palhano@gmail.com>
2023-09-12 14:41:16 -0300

    Extract Shopping previews to a file

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/Shopping.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/Shopping.kt
@@ -1,11 +1,9 @@
 package com.bruno13palhano.shopdanimanagement.ui.components
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.icu.text.DecimalFormat
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -28,12 +26,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -47,11 +43,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.bruno13palhano.shopdanimanagement.R
-import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 import java.util.Locale
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -277,70 +271,6 @@ fun ShoppingContent(
                     onCheckedChange = onIsPaidChange
                 )
             }
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun ShoppingDynamicPreview() {
-    ShopDaniManagementTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            ShoppingContent(
-                screenTitle = stringResource(id = R.string.new_item_label),
-                snackbarHostState = remember { SnackbarHostState() },
-                name = "",
-                purchasePrice = "",
-                quantity = "",
-                isPaid = true,
-                photo = "",
-                date = "",
-                onNameChange = {},
-                onPurchasePriceChange = {},
-                onQuantityChange = {},
-                onIsPaidChange = {},
-                onDateClick = {},
-                onOutsideClick = {},
-                onDoneClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun ShoppingPreview() {
-    ShopDaniManagementTheme(
-        dynamicColor = false
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            ShoppingContent(
-                screenTitle = stringResource(id = R.string.new_item_label),
-                snackbarHostState = remember { SnackbarHostState() },
-                name = "",
-                purchasePrice = "",
-                quantity = "",
-                isPaid = true,
-                photo = "",
-                date = "",
-                onNameChange = {},
-                onPurchasePriceChange = {},
-                onQuantityChange = {},
-                onIsPaidChange = {},
-                onDateClick = {},
-                onOutsideClick = {},
-                onDoneClick = {},
-                navigateUp = {}
-            )
         }
     }
 }
---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/components/ShoppingPreview.kt
@@ -0,0 +1,79 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.previews.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.ShoppingContent
+import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun ShoppingDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ShoppingContent(
+                screenTitle = stringResource(id = R.string.new_item_label),
+                snackbarHostState = remember { SnackbarHostState() },
+                name = "",
+                purchasePrice = "",
+                quantity = "",
+                isPaid = true,
+                photo = "",
+                date = "",
+                onNameChange = {},
+                onPurchasePriceChange = {},
+                onQuantityChange = {},
+                onIsPaidChange = {},
+                onDateClick = {},
+                onOutsideClick = {},
+                onDoneClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun ShoppingPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ShoppingContent(
+                screenTitle = stringResource(id = R.string.new_item_label),
+                snackbarHostState = remember { SnackbarHostState() },
+                name = "",
+                purchasePrice = "",
+                quantity = "",
+                isPaid = true,
+                photo = "",
+                date = "",
+                onNameChange = {},
+                onPurchasePriceChange = {},
+                onQuantityChange = {},
+                onIsPaidChange = {},
+                onDateClick = {},
+                onOutsideClick = {},
+                onDoneClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
