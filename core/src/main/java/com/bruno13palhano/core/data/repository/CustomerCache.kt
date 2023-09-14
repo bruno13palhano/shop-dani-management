fd53d07ee25cd90f25e0e4fe68b7d3782bb8a1e2
bruno13palhano <bruno13palhano@gmail.com>
2023-09-14 19:54:30 -0300

    Implement CustomerCache methods

--- core/src/main/java/com/bruno13palhano/core/data/repository/CustomerCache.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/CustomerCache.kt
@@ -1,39 +1,76 @@
 package com.bruno13palhano.core.data.repository
 
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import app.cash.sqldelight.coroutines.mapToOne
 import cache.CustomerTableQueries
 import com.bruno13palhano.core.data.CustomerData
 import com.bruno13palhano.core.model.Customer
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class CustomerCache @Inject constructor(
-    private val customerCache: CustomerTableQueries
+    private val customerQueries: CustomerTableQueries
 ) : CustomerData<Customer> {
     override suspend fun insert(model: Customer): Long {
-        TODO("Not yet implemented")
+        customerQueries.insert(
+            name = model.name,
+            photo = model.photo,
+            email = model.email,
+            address = model.address,
+            phoneNumber = model.phoneNumber
+        )
+        return 0L
     }
 
     override suspend fun update(model: Customer) {
-        TODO("Not yet implemented")
+        customerQueries.update(
+            id = model.id,
+            name = model.name,
+            photo = model.photo,
+            email = model.email,
+            address = model.address,
+            phoneNumber = model.phoneNumber
+        )
     }
 
     override suspend fun delete(model: Customer) {
-        TODO("Not yet implemented")
+        customerQueries.delete(model.id)
     }
 
     override suspend fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+        customerQueries.delete(id)
     }
 
     override fun getAll(): Flow<List<Customer>> {
-        TODO("Not yet implemented")
+        return customerQueries.getAll(::mapCustomer)
+            .asFlow().mapToList(Dispatchers.IO)
     }
 
     override fun getById(id: Long): Flow<Customer> {
-        TODO("Not yet implemented")
+        return customerQueries.getById(id, mapper = ::mapCustomer)
+            .asFlow().mapToOne(Dispatchers.IO)
     }
 
     override fun getLast(): Flow<Customer> {
-        TODO("Not yet implemented")
+        return customerQueries.getLast(mapper = ::mapCustomer)
+            .asFlow().mapToOne(Dispatchers.IO)
     }
+
+    private fun mapCustomer(
+        id: Long,
+        name: String,
+        photo: String,
+        email: String,
+        address: String,
+        phoneNumber: String
+    ) = Customer(
+        id = id,
+        name = name,
+        photo = photo,
+        email = email,
+        address = address,
+        phoneNumber = phoneNumber
+    )
 }
