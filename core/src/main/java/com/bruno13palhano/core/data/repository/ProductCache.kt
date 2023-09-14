a02d6ecc400361c39910ef7990173d84545632e4
bruno13palhano <bruno13palhano@gmail.com>
2023-09-13 21:39:06 -0300

    Add ProductCache

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/ProductCache.kt
@@ -0,0 +1,96 @@
+package com.bruno13palhano.core.data.repository
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import app.cash.sqldelight.coroutines.mapToOne
+import cache.ShopDatabaseQueries
+import com.bruno13palhano.core.data.ProductData
+import com.bruno13palhano.core.model.Product
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+internal class ProductCache @Inject constructor(
+    private val productQueries: ShopDatabaseQueries
+) : ProductData<Product> {
+
+    override suspend fun insert(model: Product): Long {
+        productQueries.insert(
+            name = model.name,
+            code = model.code,
+            description = model.description,
+            photo = model.photo,
+            date = model.date,
+            categories = model.categories.toString(),
+            company = model.company
+        )
+        return 0L
+    }
+
+    override suspend fun update(model: Product) {
+        productQueries.update(
+            name = model.name,
+            code = model.code,
+            description = model.description,
+            photo = model.photo,
+            date = model.date,
+            categories = model.categories.toString(),
+            company = model.company,
+            id = model.id
+        )
+    }
+
+    override suspend fun delete(model: Product) {
+        productQueries.delete(model.id)
+    }
+
+    override fun search(value: String): Flow<List<Product>> {
+        return productQueries.search(value, value, value,value, mapper = ::mapProduct)
+            .asFlow().mapToList(Dispatchers.IO)
+    }
+
+    override fun getByCategory(category: String): Flow<List<Product>> {
+        return productQueries.getByCategory(category, mapper = ::mapProduct)
+            .asFlow().mapToList(Dispatchers.IO)
+    }
+
+    override suspend fun deleteById(id: Long) {
+        productQueries.delete(id)
+    }
+
+    override fun getAll(): Flow<List<Product>> {
+        return productQueries.getAll(::mapProduct).asFlow().mapToList(Dispatchers.IO)
+    }
+
+    override fun getById(id: Long): Flow<Product> {
+        return productQueries.getById(id, mapper = ::mapProduct)
+            .asFlow().mapToOne(Dispatchers.IO)
+    }
+
+    override fun getLast(): Flow<Product> {
+        return productQueries.getLast(mapper = ::mapProduct)
+            .asFlow().mapToOne(Dispatchers.IO)
+    }
+
+    private fun mapProduct(
+        id: Long,
+        name: String,
+        code: String,
+        description: String,
+        photo: String,
+        date: Long,
+        categories: String,
+        company: String
+    ): Product {
+        return Product(
+            id = id,
+            name = name,
+            code = code,
+            description = description,
+            photo = photo,
+            date = date,
+            categories = listOf(categories),
+            company = company
+        )
+    }
+}
