4fdde0460bf36932999da26d54702be09a6c4c28
bruno13palhano <bruno13palhano@gmail.com>
2023-09-13 23:40:55 -0300

    Add an adapter to sqldelight database to handle list of strings

--- core/src/main/java/com/bruno13palhano/core/data/database/DatabaseFactory.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/DatabaseFactory.kt
@@ -1,9 +1,31 @@
 package com.bruno13palhano.core.data.database
 
+import app.cash.sqldelight.ColumnAdapter
+import cache.Product
+import cache.SaleTable
 import com.bruno13palhano.cache.ShopDatabase
 
 internal class DatabaseFactory(private val driverFactory: DriverFactory) {
     fun createDriver(): ShopDatabase {
-        return ShopDatabase(driver = driverFactory.createDriver())
+        return ShopDatabase(
+            driver = driverFactory.createDriver(),
+            SaleTableAdapter = SaleTable.Adapter(
+                categoriesAdapter = listOfStringAdapter
+            ),
+            productAdapter = Product.Adapter(
+                categoriesAdapter = listOfStringAdapter
+            )
+        )
     }
+}
+
+private val listOfStringAdapter = object : ColumnAdapter<List<String>, String> {
+    override fun decode(databaseValue: String) =
+        if (databaseValue.isEmpty()) {
+            listOf()
+        } else {
+            databaseValue.split(",")
+        }
+
+    override fun encode(value: List<String>) = value.joinToString(separator = ",")
 }
