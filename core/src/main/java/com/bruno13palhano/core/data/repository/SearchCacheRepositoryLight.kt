722a504f3fc8f611082ebfe849e892b346c3715f
bruno13palhano <bruno13palhano@gmail.com>
2023-09-15 23:19:59 -0300

    Add SearchCacheRepositoryLight

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/SearchCacheRepositoryLight.kt
@@ -0,0 +1,38 @@
+package com.bruno13palhano.core.data.repository
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import cache.SearchCacheTableQueries
+import com.bruno13palhano.core.data.SearchCacheData
+import com.bruno13palhano.core.data.di.Dispatcher
+import com.bruno13palhano.core.data.di.ShopDaniManagementDispatchers.IO
+import com.bruno13palhano.core.model.SearchCache
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchCacheRepositoryLight @Inject constructor(
+    private val searchQueries: SearchCacheTableQueries,
+    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher
+) : SearchCacheData<SearchCache> {
+    override suspend fun insert(model: SearchCache): Long {
+        searchQueries.insert(model.search)
+        return 0L
+    }
+
+    override suspend fun deleteById(search: String) {
+        searchQueries.delete(search)
+    }
+
+    override fun getAll(): Flow<List<SearchCache>> {
+        return searchQueries.getAll(::mapSearchCache)
+            .asFlow().mapToList(ioDispatcher)
+    }
+
+    private fun mapSearchCache(
+        id: String,
+        search: String
+    ) = SearchCache(
+        search = search
+    )
+}
