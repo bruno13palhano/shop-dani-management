3df48caca3ef19f2323326f6e17e1301af010b43
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 19:21:49 -0300

    Interface to decouple SaleData implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/sale/InternalSaleData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.sale
+
+import com.bruno13palhano.core.data.SaleData
+import com.bruno13palhano.core.model.Sale
+
+internal interface InternalSaleData : SaleData<Sale>
