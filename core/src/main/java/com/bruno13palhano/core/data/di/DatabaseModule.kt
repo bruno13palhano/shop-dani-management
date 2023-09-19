83affcc331680c22635d3f485e7562293335aba7
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 01:09:24 -0300

    Pass slqdelight tables provider to DatabaseModule

--- core/src/main/java/com/bruno13palhano/core/data/di/DatabaseModule.kt
+++ core/src/main/java/com/bruno13palhano/core/data/di/DatabaseModule.kt
@@ -2,8 +2,20 @@ package com.bruno13palhano.core.data.di
 
 import android.content.Context
 import androidx.room.Room
+import cache.CategoryTableQueries
+import cache.CustomerTableQueries
+import cache.DeliveryTableQueries
+import cache.ProductCategoriesTableQueries
+import cache.SaleTableQueries
+import cache.SearchCacheTableQueries
+import cache.ShopDatabaseQueries
+import cache.ShoppingTableQueries
+import cache.StockOrderTableQueries
+import com.bruno13palhano.cache.ShopDatabase
 import com.bruno13palhano.core.data.database.AppDatabase
 import com.bruno13palhano.core.data.database.CategoryConverter
+import com.bruno13palhano.core.data.database.DatabaseFactory
+import com.bruno13palhano.core.data.database.DriverFactory
 import com.bruno13palhano.core.data.database.dao.CategoryDao
 import com.bruno13palhano.core.data.database.dao.CustomerDao
 import com.bruno13palhano.core.data.database.dao.DeliveryDao
@@ -75,4 +87,64 @@ internal object DatabaseModule {
             .fallbackToDestructiveMigration()
             .build()
     }
+
+    @Provides
+    @Singleton
+    fun provideShopDatabaseFactoryDriver(
+        @ApplicationContext context: Context
+    ): ShopDatabase {
+        return DatabaseFactory(
+            driverFactory = DriverFactory(context = context)
+        ).createDriver()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShop(database: ShopDatabase): ShopDatabaseQueries = database.shopDatabaseQueries
+
+    @Provides
+    @Singleton
+    fun provideSaleTable(database: ShopDatabase): SaleTableQueries = database.saleTableQueries
+
+    @Provides
+    @Singleton
+    fun provideCustomerTable(
+        database: ShopDatabase
+    ): CustomerTableQueries = database.customerTableQueries
+
+    @Provides
+    @Singleton
+    fun provideStockOrderTable(
+        database: ShopDatabase
+    ): StockOrderTableQueries = database.stockOrderTableQueries
+
+    @Provides
+    @Singleton
+    fun provideCategoryTable(
+        database: ShopDatabase
+    ): CategoryTableQueries = database.categoryTableQueries
+
+    @Provides
+    @Singleton
+    fun provideShoppingTable(
+        database: ShopDatabase
+    ): ShoppingTableQueries = database.shoppingTableQueries
+
+    @Provides
+    @Singleton
+    fun provideSearchCacheTable(
+        database: ShopDatabase
+    ): SearchCacheTableQueries = database.searchCacheTableQueries
+
+    @Provides
+    @Singleton
+    fun provideDeliveryTable(
+        database: ShopDatabase
+    ): DeliveryTableQueries = database.deliveryTableQueries
+
+    @Provides
+    @Singleton
+    fun provideProductCategoriesTable(
+        database: ShopDatabase
+    ): ProductCategoriesTableQueries = database.productCategoriesTableQueries
 }
