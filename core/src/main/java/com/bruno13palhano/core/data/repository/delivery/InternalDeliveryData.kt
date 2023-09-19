01aca3bea185c2bc69c52ebc6681881e4f77ab2f
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 15:30:24 -0300

    Interface to decouple DeliveryData implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/delivery/InternalDeliveryData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.delivery
+
+import com.bruno13palhano.core.data.DeliveryData
+import com.bruno13palhano.core.model.Delivery
+
+interface InternalDeliveryData : DeliveryData<Delivery>
