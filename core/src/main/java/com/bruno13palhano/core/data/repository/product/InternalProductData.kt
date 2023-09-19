ce33e800685b25b3192b01ebaf5af934f699fb92
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 17:12:49 -0300

    Interface to decouple ProductData implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/product/InternalProductData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.product
+
+import com.bruno13palhano.core.data.ProductData
+import com.bruno13palhano.core.model.Product
+
+internal interface InternalProductData : ProductData<Product>
