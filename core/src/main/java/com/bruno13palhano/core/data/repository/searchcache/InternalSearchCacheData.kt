fd9db6261195a9cc6bb4870777a248917f786ff1
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 20:48:27 -0300

    Interface to decouple SearchCacheData implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/searchcache/InternalSearchCacheData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.searchcache
+
+import com.bruno13palhano.core.data.SearchCacheData
+import com.bruno13palhano.core.model.SearchCache
+
+internal interface InternalSearchCacheData : SearchCacheData<SearchCache>
