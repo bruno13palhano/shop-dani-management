eb84ebb7a890a31ebf9199451064ced9242c8620
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 14:59:24 -0300

    Interface to decouple specific implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/customer/InternalCustomerData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.customer
+
+import com.bruno13palhano.core.data.CustomerData
+import com.bruno13palhano.core.model.Customer
+
+interface InternalCustomerData : CustomerData<Customer>
