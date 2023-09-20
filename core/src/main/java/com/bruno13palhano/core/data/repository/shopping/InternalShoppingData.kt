69f0fe887c1c26ac8d4e81a3e8a2ff7a07c97b16
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 22:15:25 -0300

    Interface to decouple ShoppingData implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/shopping/InternalShoppingData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.shopping
+
+import com.bruno13palhano.core.data.ShoppingData
+import com.bruno13palhano.core.model.Shopping
+
+internal interface InternalShoppingData : ShoppingData<Shopping>
