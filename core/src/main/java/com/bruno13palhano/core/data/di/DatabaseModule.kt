ef767823cc7aa8fd890521f0a2c2a5f064abe3c2
bruno13palhano <bruno13palhano@gmail.com>
2023-09-20 12:15:19 -0300

    Remove methods for Room's inejction

--- core/src/main/java/com/bruno13palhano/core/data/di/DatabaseModule.kt
+++ core/src/main/java/com/bruno13palhano/core/data/di/DatabaseModule.kt
@@ -1,7 +1,6 @@
 package com.bruno13palhano.core.data.di
 
 import android.content.Context
-import androidx.room.Room
 import cache.CategoryTableQueries
 import cache.CustomerTableQueries
 import cache.DeliveryTableQueries
@@ -12,18 +11,8 @@ import cache.ShopDatabaseQueries
 import cache.ShoppingTableQueries
 import cache.StockOrderTableQueries
 import com.bruno13palhano.cache.ShopDatabase
-import com.bruno13palhano.core.data.database.AppDatabase
-import com.bruno13palhano.core.data.database.CategoryConverter
 import com.bruno13palhano.core.data.database.DatabaseFactory
 import com.bruno13palhano.core.data.database.DriverFactory
-import com.bruno13palhano.core.data.database.dao.CategoryDao
-import com.bruno13palhano.core.data.database.dao.CustomerDao
-import com.bruno13palhano.core.data.database.dao.DeliveryDao
-import com.bruno13palhano.core.data.database.dao.ProductDao
-import com.bruno13palhano.core.data.database.dao.SaleDao
-import com.bruno13palhano.core.data.database.dao.SearchCacheDao
-import com.bruno13palhano.core.data.database.dao.ShoppingDao
-import com.bruno13palhano.core.data.database.dao.StockOrderDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -34,59 +23,6 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 internal object DatabaseModule {
-
-    @Provides
-    fun providesProductDao(database: AppDatabase): ProductDao {
-        return database.productDao
-    }
-
-    @Provides
-    fun providesCategoryDao(database: AppDatabase): CategoryDao {
-        return database.categoryDao
-    }
-
-    @Provides
-    fun providesSearchCacheDao(database: AppDatabase): SearchCacheDao {
-        return database.searchCacheDao
-    }
-
-    @Provides
-    fun providesSaleDao(database: AppDatabase): SaleDao {
-        return database.saleDao
-    }
-
-    @Provides
-    fun providesShoppingDao(database: AppDatabase): ShoppingDao {
-        return database.shoppingDao
-    }
-
-    @Provides
-    fun providesStockOrderDao(database: AppDatabase): StockOrderDao {
-        return database.stockOrderDao
-    }
-
-    @Provides
-    fun providesCustomerDao(database: AppDatabase): CustomerDao {
-        return database.customerDao
-    }
-
-    @Provides
-    fun providesDeliveryDao(database: AppDatabase): DeliveryDao {
-        return database.deliveryDao
-    }
-
-    @Provides
-    @Singleton
-    fun provideDatabase(@ApplicationContext appContext: Context): AppDatabase {
-        return Room.databaseBuilder(
-            appContext,
-            AppDatabase::class.java,
-            "shop_dani_management_database"
-        )
-            .addTypeConverter(CategoryConverter())
-            .fallbackToDestructiveMigration()
-            .build()
-    }
 
     @Provides
     @Singleton
--- core/src/main/java/com/bruno13palhano/core/data/di/InternalDataModule.kt
+++ core/src/main/java/com/bruno13palhano/core/data/di/InternalDataModule.kt
@@ -9,21 +9,13 @@ import com.bruno13palhano.core.data.SearchCacheData
 import com.bruno13palhano.core.data.ShoppingData
 import com.bruno13palhano.core.data.StockOrderData
 import com.bruno13palhano.core.data.repository.category.CategoryLight
-import com.bruno13palhano.core.data.repository.category.CategoryRoom
 import com.bruno13palhano.core.data.repository.customer.CustomerLight
-import com.bruno13palhano.core.data.repository.customer.CustomerRoom
 import com.bruno13palhano.core.data.repository.delivery.DeliveryLight
-import com.bruno13palhano.core.data.repository.delivery.DeliveryRoom
 import com.bruno13palhano.core.data.repository.product.ProductLight
-import com.bruno13palhano.core.data.repository.product.ProductRoom
 import com.bruno13palhano.core.data.repository.sale.SaleLight
-import com.bruno13palhano.core.data.repository.sale.SaleRoom
 import com.bruno13palhano.core.data.repository.searchcache.SearchCacheLight
-import com.bruno13palhano.core.data.repository.searchcache.SearchCacheRoom
 import com.bruno13palhano.core.data.repository.shopping.ShoppingLight
-import com.bruno13palhano.core.data.repository.shopping.ShoppingRoom
 import com.bruno13palhano.core.data.repository.stockorder.StockOrderLight
-import com.bruno13palhano.core.data.repository.stockorder.StockOrderRoom
 import com.bruno13palhano.core.model.Category
 import com.bruno13palhano.core.model.Customer
 import com.bruno13palhano.core.model.Delivery
@@ -40,49 +32,25 @@ import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Qualifier
-internal annotation class InternalCategoryRoom
-
-@Qualifier
 internal annotation class InternalCategoryLight
-
-@Qualifier
-internal annotation class InternalCustomerRoom
 
 @Qualifier
 internal annotation class InternalCustomerLight
 
 @Qualifier
-internal annotation class InternalDeliveryRoom
-
-@Qualifier
 internal annotation class InternalDeliveryLight
-
-@Qualifier
-internal annotation class InternalProductRoom
 
 @Qualifier
 internal annotation class InternalProductLight
 
 @Qualifier
-internal annotation class InternalSaleRoom
-
-@Qualifier
 internal annotation class InternalSaleLight
-
-@Qualifier
-internal annotation class InternalSearchCacheRoom
 
 @Qualifier
 internal annotation class InternalSearchCacheLight
 
 @Qualifier
-internal annotation class InternalShoppingRoom
-
-@Qualifier
 internal annotation class InternalShoppingLight
-
-@Qualifier
-internal annotation class InternalStockOrderRoom
 
 @Qualifier
 internal annotation class InternalStockOrderLight
@@ -91,62 +59,30 @@ internal annotation class InternalStockOrderLight
 @Module
 internal abstract class InternalDataModule {
 
-    @InternalCategoryRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalCategoryRoom(repository: CategoryRoom): CategoryData<Category>
-
     @InternalCategoryLight
     @Singleton
     @Binds
     abstract fun bindInternalCategoryLight(repository: CategoryLight): CategoryData<Category>
-
-    @InternalCustomerRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalCustomerRoom(repository: CustomerRoom): CustomerData<Customer>
 
     @InternalCustomerLight
     @Singleton
     @Binds
     abstract fun bindInternalCustomerLight(repository: CustomerLight): CustomerData<Customer>
 
-    @InternalDeliveryRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalDeliveryRoom(repository: DeliveryRoom): DeliveryData<Delivery>
-
     @InternalDeliveryLight
     @Singleton
     @Binds
     abstract fun bindInternalDeliveryLight(repository: DeliveryLight): DeliveryData<Delivery>
-
-    @InternalProductRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalProductRoom(repository: ProductRoom): ProductData<Product>
 
     @InternalProductLight
     @Singleton
     @Binds
     abstract fun bindInternalProductLight(repository: ProductLight): ProductData<Product>
 
-    @InternalSaleRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalSaleRoom(repository: SaleRoom): SaleData<Sale>
-
     @InternalSaleLight
     @Singleton
     @Binds
     abstract fun bindInternalSaleLight(repository: SaleLight): SaleData<Sale>
-
-    @InternalSearchCacheRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalSearchCacheRoom(
-        repository: SearchCacheRoom
-    ): SearchCacheData<SearchCache>
 
     @InternalSearchCacheLight
     @Singleton
@@ -155,20 +91,10 @@ internal abstract class InternalDataModule {
         repository: SearchCacheLight
     ): SearchCacheData<SearchCache>
 
-    @InternalShoppingRoom
-    @Singleton
-    @Binds
-    abstract fun bindInternalShoppingRoom(repository: ShoppingRoom): ShoppingData<Shopping>
-
     @InternalShoppingLight
     @Singleton
     @Binds
     abstract fun bindInternalShoppingLight(repository: ShoppingLight): ShoppingData<Shopping>
-
-    @InternalStockOrderRoom
-    @Singleton
-    @Binds
-    abstract fun bindStockOrderRepository(repository: StockOrderRoom): StockOrderData<StockOrder>
 
     @InternalStockOrderLight
     @Singleton
