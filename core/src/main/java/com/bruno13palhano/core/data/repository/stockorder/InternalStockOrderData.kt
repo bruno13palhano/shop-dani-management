ed48d58ea62087449ac0983ee1462ec060dd458a
bruno13palhano <bruno13palhano@gmail.com>
2023-09-19 23:09:14 -0300

    Interface to decouple StockOrderData implementations from database access wrappers

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/stockorder/InternalStockOrderData.kt
@@ -0,0 +1,6 @@
+package com.bruno13palhano.core.data.repository.stockorder
+
+import com.bruno13palhano.core.data.StockOrderData
+import com.bruno13palhano.core.model.StockOrder
+
+internal interface InternalStockOrderData : StockOrderData<StockOrder>
