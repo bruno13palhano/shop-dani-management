87b0c91de4edc33da543c9045f3a271adbcde15e
bruno13palhano <bruno13palhano@gmail.com>
2023-09-22 21:01:29 -0300

    Add insertItems method

--- core/src/main/java/com/bruno13palhano/core/data/SaleData.kt
+++ core/src/main/java/com/bruno13palhano/core/data/SaleData.kt
@@ -1,8 +1,12 @@
 package com.bruno13palhano.core.data
 
+import com.bruno13palhano.core.model.Delivery
+import com.bruno13palhano.core.model.Sale
+import com.bruno13palhano.core.model.StockOrder
 import kotlinx.coroutines.flow.Flow
 
 interface SaleData<T> : DataOperations<T> {
+    suspend fun insertItems(sale: Sale, stockOrder: StockOrder, delivery: Delivery)
     fun getByCustomerId(customerId: Long): Flow<List<T>>
     fun getLastSales(offset: Int, limit: Int): Flow<List<T>>
     fun getAllStockSales(offset: Int, limit: Int): Flow<List<T>>
