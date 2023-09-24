e4184ad99c4e13e51e70a2a570f94cc2eb40ee7a
bruno13palhano <bruno13palhano@gmail.com>
2023-09-23 21:47:45 -0300

    Change stringAdapter to categoryAdapter

--- core/src/main/java/com/bruno13palhano/core/data/database/DatabaseFactory.kt
+++ core/src/main/java/com/bruno13palhano/core/data/database/DatabaseFactory.kt
@@ -3,25 +3,31 @@ package com.bruno13palhano.core.data.database
 import app.cash.sqldelight.ColumnAdapter
 import cache.ProductCategoriesTable
 import com.bruno13palhano.cache.ShopDatabase
+import com.bruno13palhano.core.model.Category
 
 internal class DatabaseFactory(private val driverFactory: DriverFactory) {
     fun createDriver(): ShopDatabase {
         return ShopDatabase(
             driver = driverFactory.createDriver(),
             ProductCategoriesTableAdapter = ProductCategoriesTable.Adapter(
-                categoriesAdapter = listOfStringAdapter
+                categoriesAdapter = listOfCategoryAdapter
             )
         )
     }
 }
 
-private val listOfStringAdapter = object : ColumnAdapter<List<String>, String> {
-    override fun decode(databaseValue: String) =
+private val listOfCategoryAdapter = object : ColumnAdapter<List<Category>, String> {
+    override fun decode(databaseValue: String): List<Category> =
         if (databaseValue.isEmpty()) {
-            listOf()
+            emptyList()
         } else {
-            databaseValue.split(",")
+            databaseValue.split(",").map {
+                val params = it.split(":")
+                Category(id = params[0].toLong(), name = params[1])
+            }
         }
 
-    override fun encode(value: List<String>) = value.joinToString(separator = ",")
+    override fun encode(value: List<Category>) = value.joinToString(",") {
+            "${it.id}:${it.name}"
+        }
 }
