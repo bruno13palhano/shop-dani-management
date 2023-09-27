4a41282f3698cdd945ba957060d49e5ff367f47b
bruno13palhano <bruno13palhano@gmail.com>
2023-09-27 17:05:54 -0300

    Remove ShoppingTable

--- core/src/main/java/com/bruno13palhano/core/data/ShoppingData.kt
+++
@@ -1,7 +0,0 @@
-package com.bruno13palhano.core.data
-
-import kotlinx.coroutines.flow.Flow
-
-interface ShoppingData<T> : DataOperations<T> {
-    fun getItemsLimited(offset: Int, limit: Int): Flow<List<T>>
-}
