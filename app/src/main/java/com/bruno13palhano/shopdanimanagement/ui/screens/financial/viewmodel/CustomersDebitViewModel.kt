0abb8352ed688af5bcf12f20f730ce6115b5f40e
bruno13palhano <bruno13palhano@gmail.com>
2023-09-28 16:05:28 -0300

    Gets customers debit

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/CustomersDebitViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/CustomersDebitViewModel.kt
@@ -1,12 +1,37 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.SaleData
+import com.bruno13palhano.core.data.di.SaleRep
+import com.bruno13palhano.core.model.Sale
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class CustomersDebitViewModel @Inject constructor(
-
+    @SaleRep private val saleRepository: SaleData<Sale>
 ) : ViewModel() {
-
+    val debits = saleRepository.getAll()
+        .map { it.filter { sale -> !sale.isPaidByCustomer } }
+        .map {
+            it.map { sale ->
+                CommonItem(
+                    id = sale.id,
+                    photo = sale.photo,
+                    title = sale.customerName,
+                    subtitle = sale.salePrice.toString(),
+                    description = sale.dateOfPayment.toString()
+                )
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = emptyList()
+        )
 }
