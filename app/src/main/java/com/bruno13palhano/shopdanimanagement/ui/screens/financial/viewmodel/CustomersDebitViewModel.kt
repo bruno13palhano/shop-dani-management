a4846a1a9af3f908dd2c0cc6057298b497216eed
bruno13palhano <bruno13palhano@gmail.com>
2023-09-28 12:46:28 -0300

    Add a ViewModel for CustomersDebitScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/CustomersDebitViewModel.kt
@@ -0,0 +1,12 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CustomersDebitViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
