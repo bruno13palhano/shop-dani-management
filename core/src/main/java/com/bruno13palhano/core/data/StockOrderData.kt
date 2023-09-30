6e12113c2f299483b1d01c77af7bd4507cfb830a
bruno13palhano <bruno13palhano@gmail.com>
2023-09-30 14:40:22 -0300

    Add a method to gets items with quantity > 0

--- core/src/main/java/com/bruno13palhano/core/data/StockOrderData.kt
+++ core/src/main/java/com/bruno13palhano/core/data/StockOrderData.kt
@@ -9,6 +9,7 @@ interface StockOrderData<T> : DataOperations<T> {
     fun search(value: String, isOrderedByCustomer: Boolean): Flow<List<T>>
     fun getByCategory(category: String, isOrderedByCustomer: Boolean): Flow<List<T>>
     suspend fun updateStockOrderQuantity(id: Long, quantity: Int)
+    fun getStockOrderItems(isOrderedByCustomer: Boolean): Flow<List<T>>
     fun getDebitStock(): Flow<List<T>>
     fun getOutOfStock(): Flow<List<T>>
 }
