37179253eef7c8d53a384fb47d00487aee280fcb
bruno13palhano <bruno13palhano@gmail.com>
2023-10-02 15:35:12 -0300

    Add more options menu

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/SalesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/sales/SalesScreen.kt
@@ -1,12 +1,17 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.sales
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -15,7 +20,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -23,6 +33,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.components.HorizontalItemList
+import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import com.bruno13palhano.shopdanimanagement.ui.screens.sales.viewmodel.SalesViewModel
 
@@ -33,11 +44,36 @@ fun SalesScreen(
     navigateUp: () -> Unit,
     viewModel: SalesViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getSales()
+    }
+
     val saleList by viewModel.saleList.collectAsStateWithLifecycle()
+    val menuItems = arrayOf(
+        stringResource(id = R.string.ordered_by_name_label),
+        stringResource(id = R.string.ordered_by_price_label)
+    )
+
+    var orderedByName by remember { mutableStateOf(false) }
+    var orderedByPrice by remember { mutableStateOf(false) }
 
     SalesContent(
         saleList = saleList,
+        menuItems = menuItems,
         onItemClick = onItemClick,
+        onMoreOptionsItemClick = { index ->
+            when (index) {
+                0 -> {
+                    viewModel.getSalesByCustomerName(isOrderedAsc = orderedByName)
+                    orderedByName = !orderedByName
+                }
+                1 -> {
+                    viewModel.getSalesBySalePrice(isOrderedAsc = orderedByPrice)
+                    orderedByPrice = !orderedByPrice
+                }
+                else -> { viewModel.getSales() }
+            }
+        },
         onAddButtonClick = onAddButtonClick,
         navigateUp = navigateUp
     )
@@ -47,10 +83,14 @@ fun SalesScreen(
 @Composable
 fun SalesContent(
     saleList: List<CommonItem>,
+    menuItems: Array<String>,
     onItemClick: (id: Long) -> Unit,
+    onMoreOptionsItemClick: (index: Int) -> Unit,
     onAddButtonClick: () -> Unit,
     navigateUp: () -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -61,6 +101,30 @@ fun SalesContent(
                             imageVector = Icons.Filled.ArrowBack,
                             contentDescription = stringResource(id = R.string.up_button_label)
                         )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.drawer_menu_label)
+                            )
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MoreOptionsMenu(
+                                    items = menuItems,
+                                    expanded = expanded,
+                                    onDismissRequest = { expandedValue ->
+                                        expanded = expandedValue
+                                    },
+                                    onClick = onMoreOptionsItemClick
+                                )
+                            }
+                        }
                     }
                 }
             )
