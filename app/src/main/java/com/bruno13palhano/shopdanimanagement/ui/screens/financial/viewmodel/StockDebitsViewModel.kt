7d3302223fbfee95977ce2f7a1a471ddedc95c9e
bruno13palhano <bruno13palhano@gmail.com>
2023-10-02 11:15:17 -0300

    Add methods to ordered debit stock by name and price

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/StockDebitsViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/StockDebitsViewModel.kt
@@ -7,16 +7,19 @@ import com.bruno13palhano.core.data.di.StockOrderRep
 import com.bruno13palhano.core.model.StockOrder
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.Stock
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StockDebitsViewModel @Inject constructor(
     @StockOrderRep private val stockRepository: StockOrderData<StockOrder>
 ) : ViewModel() {
-    val debitItems = stockRepository.getDebitStock()
+    private val _debitItems = MutableStateFlow(emptyList<StockOrder>())
+    val debitItems = _debitItems
         .map {
             it.map { stockItem ->
                 Stock(
@@ -33,4 +36,28 @@ class StockDebitsViewModel @Inject constructor(
             started = WhileSubscribed(5_000),
             initialValue = emptyList()
         )
+
+    fun getDebitStock() {
+        viewModelScope.launch {
+            stockRepository.getDebitStock().collect {
+                _debitItems.value = it
+            }
+        }
+    }
+
+    fun getStockByPrice(isOrderedAsc: Boolean) {
+        viewModelScope.launch {
+            stockRepository.getDebitStockByPrice(isOrderedAsc = isOrderedAsc).collect {
+                _debitItems.value = it
+            }
+        }
+    }
+
+    fun getStockByName(isOrderedAsc: Boolean) {
+        viewModelScope.launch {
+            stockRepository.getDebitStockByName(isOrderedAsc = isOrderedAsc).collect {
+                _debitItems.value = it
+            }
+        }
+    }
 }
