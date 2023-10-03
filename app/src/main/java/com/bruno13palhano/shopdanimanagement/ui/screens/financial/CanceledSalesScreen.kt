07abd1b48242b3ce318c6cd7261dd54033e9d1a4
bruno13palhano <bruno13palhano@gmail.com>
2023-10-03 13:56:01 -0300

    Add a Screen to show canceled sales

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/CanceledSalesScreen.kt
@@ -0,0 +1,17 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.financial
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CanceledSalesScreen(
+    navigateUp: () -> Unit
+) {
+
+}
+
+@Composable
+fun CanceledSalesContent(
+    navigateUp: () -> Unit
+) {
+
+}
