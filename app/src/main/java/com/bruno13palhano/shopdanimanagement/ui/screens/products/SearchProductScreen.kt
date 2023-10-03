44956a99ba27d381d25687dc3921709b571ff4b6
bruno13palhano <bruno13palhano@gmail.com>
2023-10-03 07:28:02 -0300

    Implement SearchProductContent

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/products/SearchProductScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/products/SearchProductScreen.kt
@@ -1,15 +1,43 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.products
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.bruno13palhano.shopdanimanagement.ui.components.SearchContent
+import com.bruno13palhano.core.model.SearchCache
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.HorizontalItemList
+import com.bruno13palhano.shopdanimanagement.ui.components.SimpleItemList
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import com.bruno13palhano.shopdanimanagement.ui.screens.products.viewmodel.SearchProductsViewModel
 
 @Composable
 fun SearchProductScreen(
+    isEditable: Boolean,
+    categoryId: Long,
     onItemClick: (id: Long) -> Unit,
     navigateUp: () -> Unit,
     viewModel: SearchProductsViewModel = hiltViewModel()
@@ -17,17 +45,107 @@ fun SearchProductScreen(
     LaunchedEffect(key1 = Unit) {
         viewModel.getSearchCache()
     }
-    val stockProducts by viewModel.stockProducts.collectAsStateWithLifecycle()
+
+    val products by viewModel.products.collectAsStateWithLifecycle()
     val searchCacheList by viewModel.searchCache.collectAsStateWithLifecycle()
 
-    SearchContent(
-        stockProducts = stockProducts,
+    SearchProductContent(
+        products = products,
         searchCacheList = searchCacheList,
         onSearchClick = { search ->
-            viewModel.search(search)
-            viewModel.insertSearch(search)
+            if (isEditable) {
+                viewModel.searchPerCategory(search = search, categoryId = categoryId)
+            } else {
+                viewModel.search(search = search)
+            }
+            viewModel.insertSearch(search = search)
         },
         onItemClick = onItemClick,
         navigateUp = navigateUp
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchProductContent(
+    products: List<CommonItem>,
+    searchCacheList: List<SearchCache>,
+    onSearchClick: (search: String) -> Unit,
+    onItemClick: (id: Long) -> Unit,
+    navigateUp: () -> Unit
+) {
+    var search by rememberSaveable { mutableStateOf("") }
+    var active by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(topBar = { TopAppBar( title = {} ) }) {
+        SearchBar(
+            modifier = Modifier
+                .fillMaxWidth(),
+            query = search,
+            onQueryChange = { searchValue -> search = searchValue },
+            onSearch = { searchValue ->
+                active = false
+                onSearchClick(searchValue)
+            },
+            active = active,
+            onActiveChange = { isActive ->
+                active = isActive
+            },
+            leadingIcon = {
+                IconButton(onClick = { if (active) active = false else navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.up_button_label)
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {
+                    active = false
+                    onSearchClick(search)
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = stringResource(id = R.string.search_label)
+                    )
+                }
+            },
+            placeholder = { Text(text = stringResource(id = R.string.search_products_label)) }
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                reverseLayout = true
+            ) {
+                items(
+                    items = searchCacheList,
+                    key = { searchCache -> searchCache.search }
+                ) { searchCache ->
+                    SimpleItemList(
+                        modifier = Modifier.padding(1.dp),
+                        itemName = searchCache.search,
+                        imageVector = Icons.Filled.Close,
+                        onClick = {
+                            active = false
+                            onSearchClick(searchCache.search)
+                        }
+                    )
+                }
+            }
+        }
+        LazyColumn(
+            modifier = Modifier.padding(it),
+            contentPadding = PaddingValues(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            items(items = products, key = { product -> product.id } ) { product ->
+                HorizontalItemList(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    title = product.title,
+                    subtitle = product.subtitle,
+                    description = product.description,
+                    photo = product.photo,
+                    onClick = { onItemClick(product.id) }
+                )
+            }
+        }
+    }
 }
