dd82fcab64930f055f2c60275647ffce567a273d
bruno13palhano <bruno13palhano@gmail.com>
2023-10-04 19:43:40 -0300

    Add a repository for Catalog

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/catalog/CatalogRepository.kt
@@ -0,0 +1,39 @@
+package com.bruno13palhano.core.data.repository.catalog
+
+import com.bruno13palhano.core.data.CatalogData
+import com.bruno13palhano.core.data.di.InternalCatalogLight
+import com.bruno13palhano.core.model.Catalog
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CatalogRepository @Inject constructor(
+    @InternalCatalogLight private val catalogData: CatalogData<Catalog>
+) : CatalogData<Catalog> {
+    override suspend fun insert(model: Catalog): Long {
+        return catalogData.insert(model = model)
+    }
+
+    override suspend fun update(model: Catalog) {
+        catalogData.update(model = model)
+    }
+
+    override suspend fun delete(model: Catalog) {
+        catalogData.delete(model = model)
+    }
+
+    override suspend fun deleteById(id: Long) {
+        catalogData.deleteById(id = id)
+    }
+
+    override fun getAll(): Flow<List<Catalog>> {
+        return catalogData.getAll()
+    }
+
+    override fun getById(id: Long): Flow<Catalog> {
+        return catalogData.getById(id = id)
+    }
+
+    override fun getLast(): Flow<Catalog> {
+        return catalogData.getLast()
+    }
+}
