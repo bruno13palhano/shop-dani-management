6e8e32d1bebe384ae351b85afc4d5142ad01ef6a
bruno13palhano <bruno13palhano@gmail.com>
2023-10-04 19:57:54 -0300

    Add a screen to show catalog

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/catalog/CatalogScreen.kt
@@ -0,0 +1,19 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.catalog
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun CatalogScreen(
+    navigateUp: () -> Unit,
+    viewModel: CatalogViewModel = hiltViewModel()
+) {
+
+}
+
+@Composable
+fun CatalogContent(
+    navigateUp: () -> Unit
+) {
+
+}
