f6706f512403a028c3fd2e3e2e49377cb3a42e48
bruno13palhano <bruno13palhano@gmail.com>
2023-10-04 07:56:15 -0300

    Extract StockDebits previews to StockOrderListPreview file

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/StockDebtisScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/StockDebtisScreen.kt
@@ -1,6 +1,5 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.financial
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,9 +14,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -29,7 +26,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -38,7 +34,6 @@ import com.bruno13palhano.shopdanimanagement.ui.components.HorizontalItemList
 import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.Stock
 import com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel.StockDebitsViewModel
-import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun StockDebitsScreen(
@@ -146,60 +141,3 @@ fun StockDebitsContent(
         }
     }
 }
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-private fun StockDebitsDynamicPreview() {
-    ShopDaniManagementTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            StockDebitsContent(
-                stockItems = items,
-                menuItems = arrayOf(),
-                onItemClick = {},
-                onMoreOptionsItemClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-private fun StockDebitsPreview() {
-    ShopDaniManagementTheme(
-        dynamicColor = false
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            StockDebitsContent(
-                stockItems = items,
-                menuItems = arrayOf(),
-                onItemClick = {},
-                onMoreOptionsItemClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-private val items = listOf(
-    Stock(id= 1L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 120.45F, quantity = 12),
-    Stock(id= 2L, name = "Kaiak", photo = byteArrayOf(), purchasePrice = 40.33F, quantity = 2),
-    Stock(id= 3L, name = "Homem", photo = byteArrayOf(), purchasePrice = 99.99F, quantity = 7),
-    Stock(id= 4L, name = "Luna", photo = byteArrayOf(), purchasePrice = 12.39F, quantity = 2),
-    Stock(id= 5L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 56.78F, quantity = 1),
-    Stock(id= 6L, name = "Una", photo = byteArrayOf(), purchasePrice = 12.12F, quantity = 2),
-    Stock(id= 7L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 120.45F, quantity = 12),
-    Stock(id= 8L, name = "Kaiak", photo = byteArrayOf(), purchasePrice = 40.33F, quantity = 2),
-    Stock(id= 9L, name = "Homem", photo = byteArrayOf(), purchasePrice = 99.99F, quantity = 7),
-    Stock(id= 10L, name = "Luna", photo = byteArrayOf(), purchasePrice = 12.39F, quantity = 2),
-    Stock(id= 11L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 56.78F, quantity = 1),
-    Stock(id= 12L, name = "Una", photo = byteArrayOf(), purchasePrice = 12.12F, quantity = 2)
-)
--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/components/StockOrederListPreview.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/components/StockOrederListPreview.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.Stock
 import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.components.StockOrderListContent
+import com.bruno13palhano.shopdanimanagement.ui.screens.financial.StockDebitsContent
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -63,6 +64,48 @@ fun StockOrderListItemPreview() {
     }
 }
 
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+private fun StockDebitsDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            StockDebitsContent(
+                stockItems = items,
+                menuItems = arrayOf(),
+                onItemClick = {},
+                onMoreOptionsItemClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+private fun StockDebitsPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            StockDebitsContent(
+                stockItems = items,
+                menuItems = arrayOf(),
+                onItemClick = {},
+                onMoreOptionsItemClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
 private val items = listOf(
     Stock(id= 1L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 120.45F, quantity = 12),
     Stock(id= 2L, name = "Kaiak", photo = byteArrayOf(), purchasePrice = 40.33F, quantity = 2),
@@ -70,4 +113,10 @@ private val items = listOf(
     Stock(id= 4L, name = "Luna", photo = byteArrayOf(), purchasePrice = 12.39F, quantity = 2),
     Stock(id= 5L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 56.78F, quantity = 1),
     Stock(id= 6L, name = "Una", photo = byteArrayOf(), purchasePrice = 12.12F, quantity = 2),
+    Stock(id= 7L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 120.45F, quantity = 12),
+    Stock(id= 8L, name = "Kaiak", photo = byteArrayOf(), purchasePrice = 40.33F, quantity = 2),
+    Stock(id= 9L, name = "Homem", photo = byteArrayOf(), purchasePrice = 99.99F, quantity = 7),
+    Stock(id= 10L, name = "Luna", photo = byteArrayOf(), purchasePrice = 12.39F, quantity = 2),
+    Stock(id= 11L, name = "Essencial", photo = byteArrayOf(), purchasePrice = 56.78F, quantity = 1),
+    Stock(id= 12L, name = "Una", photo = byteArrayOf(), purchasePrice = 12.12F, quantity = 2)
 )
