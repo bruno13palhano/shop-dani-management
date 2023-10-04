9ed3108b79e04f7718e611499a31813e20455f1f
bruno13palhano <bruno13palhano@gmail.com>
2023-10-04 07:24:05 -0300

    Extract CustomersDebits previews to CustomersPreview file

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/CustomersDebitScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/CustomersDebitScreen.kt
@@ -1,6 +1,5 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.financial
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,9 +14,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -29,7 +26,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -38,7 +34,6 @@ import com.bruno13palhano.shopdanimanagement.ui.components.CommonItemList
 import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel.CustomersDebitViewModel
-import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun CustomersDebitScreen(
@@ -145,55 +140,3 @@ fun CustomersDebitContent(
         }
     }
 }
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun CustomersDebitDynamicPreview() {
-    ShopDaniManagementTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            CustomersDebitContent(
-                debits = debits,
-                menuItems = arrayOf(),
-                onItemClick = {},
-                onMoreOptionsItemClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun CustomersDebitPreview() {
-    ShopDaniManagementTheme(
-        dynamicColor = false
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            CustomersDebitContent(
-                debits = debits,
-                menuItems = arrayOf(),
-                onItemClick = {},
-                onMoreOptionsItemClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-private val debits = listOf(
-    CommonItem(id = 1L, photo = byteArrayOf(), title = "Bruno", subtitle = "R$12.50", description = "Feb 4, 2023"),
-    CommonItem(id = 2L, photo = byteArrayOf(), title = "Daniela", subtitle = "R$170.90", description = "Feb 7, 2023"),
-    CommonItem(id = 3L, photo = byteArrayOf(), title = "Josué", subtitle = "R$165.99", description = "Feb 8, 2023"),
-    CommonItem(id = 4L, photo = byteArrayOf(), title = "Helena", subtitle = "R$9.90", description = "Feb 11, 2023"),
-    CommonItem(id = 5L, photo = byteArrayOf(), title = "Fernando", subtitle = "R$160.50", description = "Feb 12, 2023"),
-    CommonItem(id = 6L, photo = byteArrayOf(), title = "Socorro", subtitle = "R$122.50", description = "Feb 14, 2023"),
-    CommonItem(id = 7L, photo = byteArrayOf(), title = "Brenda", subtitle = "R$282.99", description = "Feb 14, 2023"),
-)
--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/CustomersPreview.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/CustomersPreview.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import com.bruno13palhano.shopdanimanagement.ui.screens.customers.CustomerInfoContent
 import com.bruno13palhano.shopdanimanagement.ui.screens.customers.CustomersContent
+import com.bruno13palhano.shopdanimanagement.ui.screens.financial.CustomersDebitContent
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 
@@ -60,15 +61,15 @@ fun CustomerPreview() {
 }
 
 private val customerList = listOf(
-    CommonItem(1L, byteArrayOf(), "Bruno", "Rua 15 de novembro", description = ""),
-    CommonItem(2L, byteArrayOf(), "Brenda",  "13 de maio", description = ""),
-    CommonItem(3L, byteArrayOf(), "Daniela", "Rua do serrote", description = ""),
-    CommonItem(4L, byteArrayOf(), "Josué", "Rua 15 de novembro", description = ""),
-    CommonItem(5L, byteArrayOf(), "Helena", "Rua 13 de maio", description = ""),
-    CommonItem(6L, byteArrayOf(),"Socorro","Rua do serrote", description = ""),
-    CommonItem(7L, byteArrayOf(),"Fernando","Rua do serrote", description = ""),
-    CommonItem(8L, byteArrayOf(),"Henrique","Carão", description = ""),
-    CommonItem(9L, byteArrayOf(), "Bruno","Rua 15 de novembro", description = ""),
+    CommonItem(1L, byteArrayOf(), "Bruno", "Rua 15 de novembro", ""),
+    CommonItem(2L, byteArrayOf(), "Brenda",  "13 de maio", ""),
+    CommonItem(3L, byteArrayOf(), "Daniela", "Rua do serrote", ""),
+    CommonItem(4L, byteArrayOf(), "Josué", "Rua 15 de novembro", ""),
+    CommonItem(5L, byteArrayOf(), "Helena", "Rua 13 de maio", ""),
+    CommonItem(6L, byteArrayOf(),"Socorro","Rua do serrote", ""),
+    CommonItem(7L, byteArrayOf(),"Fernando","Rua do serrote", ""),
+    CommonItem(8L, byteArrayOf(),"Henrique","Carão", ""),
+    CommonItem(9L, byteArrayOf(), "Bruno","Rua 15 de novembro", ""),
 )
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -122,3 +123,55 @@ fun CustomerInfoPreview() {
         }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun CustomersDebitDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CustomersDebitContent(
+                debits = debits,
+                menuItems = arrayOf(),
+                onItemClick = {},
+                onMoreOptionsItemClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun CustomersDebitPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CustomersDebitContent(
+                debits = debits,
+                menuItems = arrayOf(),
+                onItemClick = {},
+                onMoreOptionsItemClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+private val debits = listOf(
+    CommonItem(1L, byteArrayOf(), "Bruno", "R$12.50", "Feb 4, 2023"),
+    CommonItem(2L, byteArrayOf(), "Daniela", "R$170.90", "Feb 7, 2023"),
+    CommonItem(3L, byteArrayOf(), "Josué", "R$165.99", "Feb 8, 2023"),
+    CommonItem(4L, byteArrayOf(), "Helena", "R$9.90", "Feb 11, 2023"),
+    CommonItem(5L, byteArrayOf(), "Fernando", "R$160.50", "Feb 12, 2023"),
+    CommonItem(6L, byteArrayOf(), "Socorro","R$122.50", "Feb 14, 2023"),
+    CommonItem(7L, byteArrayOf(), "Brenda", "R$282.99", "Feb 14, 2023"),
+)
