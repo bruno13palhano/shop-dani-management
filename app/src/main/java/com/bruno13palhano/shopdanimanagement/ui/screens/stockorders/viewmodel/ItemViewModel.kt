ca41457d5bd17f728bf9525034bf6fa1c6811d72
bruno13palhano <bruno13palhano@gmail.com>
2023-10-04 11:54:14 -0300

    Refactor ItemViewModel

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stockorders/viewmodel/ItemViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/stockorders/viewmodel/ItemViewModel.kt
@@ -7,24 +7,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bruno13palhano.core.data.CategoryData
 import com.bruno13palhano.core.data.ProductData
 import com.bruno13palhano.core.data.StockOrderData
-import com.bruno13palhano.core.data.di.CategoryRep
 import com.bruno13palhano.core.data.di.ProductRep
 import com.bruno13palhano.core.data.di.StockOrderRep
 import com.bruno13palhano.core.model.Category
-import com.bruno13palhano.core.model.Company
 import com.bruno13palhano.core.model.Product
 import com.bruno13palhano.core.model.StockOrder
-import com.bruno13palhano.shopdanimanagement.ui.components.CategoryCheck
-import com.bruno13palhano.shopdanimanagement.ui.components.CompanyCheck
 import com.bruno13palhano.shopdanimanagement.ui.screens.currentDate
 import com.bruno13palhano.shopdanimanagement.ui.screens.dateFormat
 import com.bruno13palhano.shopdanimanagement.ui.screens.stringToFloat
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -32,13 +26,8 @@ import javax.inject.Inject
 @HiltViewModel
 class ItemViewModel @Inject constructor(
     @ProductRep private val productRepository: ProductData<Product>,
-    @CategoryRep private val categoryRepository: CategoryData<Category>,
     @StockOrderRep private val stockRepository: StockOrderData<StockOrder>
 ) : ViewModel() {
-    private val companiesCheck = listOf(
-        CompanyCheck(Company.AVON, true),
-        CompanyCheck(Company.NATURA, false)
-    )
     private var productId by mutableLongStateOf(0L)
     var name by mutableStateOf("")
         private set
@@ -62,9 +51,6 @@ class ItemViewModel @Inject constructor(
         private set
     var company by mutableStateOf("")
         private set
-    private var categories by mutableStateOf(listOf<Category>())
-    private var allCategories by mutableStateOf((listOf<CategoryCheck>()))
-    private var allCompanies by mutableStateOf(companiesCheck)
     var isPaid by mutableStateOf(false)
         private set
 
@@ -103,61 +89,21 @@ class ItemViewModel @Inject constructor(
         this.isPaid = isPaid
     }
 
-    private fun updateCategories(categories: List<CategoryCheck>) {
-        val catList = mutableListOf<Category>()
-        categories
-            .filter { it.isChecked }
-            .map { catList.add(Category(it.id, it.category)) }
-        this.categories = catList
-        category = this.categories.joinToString(", ") { category ->
-            category.name
-        }
-    }
-
-    private fun setCategoriesChecked(allCategories: List<Category>) {
-        this.allCategories.forEach { categoryCheck ->
-            allCategories.forEach {
-                if (categoryCheck.id == it.id) {
-                    categoryCheck.isChecked = true
-                }
-            }
-        }
-        updateCategories(this.allCategories)
-    }
-
-    private fun setCompanyChecked(company: String) {
-        companiesCheck.forEach { companyCheck ->
-            if (companyCheck.name.company == company) {
-                companyCheck.isChecked = true
-            }
-        }
-        allCompanies = companiesCheck
-    }
-
-    init {
-        viewModelScope.launch {
-            categoryRepository.getAll()
-                .map {
-                    it.map { category -> CategoryCheck(category.id, category.name, false) }
-                }.collect {
-                    allCategories = it
-                }
-        }
-    }
-
     fun getProduct(id: Long) {
         viewModelScope.launch {
             productRepository.getById(id).collect {
                 name = it.name
                 photo = it.photo
                 updateDate(it.date)
-                categories = it.categories
                 company = it.company
-                setCategoriesChecked(categories)
-                setCompanyChecked(it.company)
+                category = setCategories(it.categories)
+                company = it.company
             }
         }
     }
+
+    private fun setCategories(categories: List<Category>) =
+        categories.joinToString(", ") { category -> category.name }
 
     fun insertItems(productId: Long, isOrderedByCustomer: Boolean) {
         viewModelScope.launch {
@@ -178,8 +124,8 @@ class ItemViewModel @Inject constructor(
                 company = it.company
                 updateDate(it.date)
                 updateValidity(it.validity)
-                setCategoriesChecked(it.categories)
-                setCompanyChecked(it.company)
+                category = setCategories(it.categories)
+                company = it.company
                 purchasePrice = it.purchasePrice.toString()
                 salePrice = it.salePrice.toString()
                 isPaid = it.isPaid
@@ -207,7 +153,7 @@ class ItemViewModel @Inject constructor(
         quantity = quantity.toInt(),
         date = dateInMillis,
         validity = validityInMillis,
-        categories = categories,
+        categories = emptyList(),
         company = company,
         purchasePrice = stringToFloat(purchasePrice),
         salePrice = stringToFloat(salePrice),
@@ -223,7 +169,7 @@ class ItemViewModel @Inject constructor(
         quantity = quantity.toInt(),
         date = dateInMillis,
         validity = validityInMillis,
-        categories = categories,
+        categories = emptyList(),
         company = company,
         purchasePrice = stringToFloat(purchasePrice),
         salePrice = stringToFloat(salePrice),
