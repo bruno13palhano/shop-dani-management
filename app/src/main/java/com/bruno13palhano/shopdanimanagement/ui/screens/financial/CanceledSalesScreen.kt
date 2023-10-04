b386f34ffd9aea44f523602017d30118c29fee35
bruno13palhano <bruno13palhano@gmail.com>
2023-10-04 06:59:47 -0300

    Extract CanceledSalesPreview to SalesPreview file

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/CanceledSalesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/CanceledSalesScreen.kt
@@ -1,6 +1,5 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.financial
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,9 +14,7 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -29,7 +26,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -38,7 +34,6 @@ import com.bruno13palhano.shopdanimanagement.ui.components.HorizontalItemList
 import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
 import com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel.CanceledSalesViewModel
-import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
 @Composable
 fun CanceledSalesScreen(
@@ -151,53 +146,3 @@ fun CanceledSalesContent(
         }
     }
 }
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun CanceledSalesDynamicPreview() {
-    ShopDaniManagementTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            CanceledSalesContent(
-                canceledList = canceledList,
-                menuItems = arrayOf(),
-                onMoreOptionsItemClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun CanceledSalesPreview() {
-    ShopDaniManagementTheme(
-        dynamicColor = false
-    ) {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            CanceledSalesContent(
-                canceledList = canceledList,
-                menuItems = arrayOf(),
-                onMoreOptionsItemClick = {},
-                navigateUp = {}
-            )
-        }
-    }
-}
-
-private val canceledList = listOf(
-    CommonItem(id = 1, photo = byteArrayOf(), title = "Homem", subtitle = "R$77.90", description = "Bruno Barbosa"),
-    CommonItem(id = 2, photo = byteArrayOf(), title = "Essencial", subtitle = "R$157.99", description = "Daniela"),
-    CommonItem(id = 3, photo = byteArrayOf(), title = "Kaiak", subtitle = "R$88.90", description = "Brenda"),
-    CommonItem(id = 4, photo = byteArrayOf(), title = "Luna", subtitle = "R$67.90", description = "Fernando"),
-    CommonItem(id = 5, photo = byteArrayOf(), title = "Una", subtitle = "R$99.99", description = "Socorro"),
-    CommonItem(id = 6, photo = byteArrayOf(), title = "Essencial", subtitle = "R$157.99", description = "Josué"),
-    CommonItem(id = 7, photo = byteArrayOf(), title = "Homem", subtitle = "R$77.90", description = "Helena"),
-)
--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/SalesPreview.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/previews/SalesPreview.kt
@@ -7,6 +7,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
+import com.bruno13palhano.shopdanimanagement.ui.screens.financial.CanceledSalesContent
 import com.bruno13palhano.shopdanimanagement.ui.screens.sales.SalesOptionsScreen
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
 
@@ -27,3 +29,53 @@ fun SalesOptionsDynamicPreview() {
         }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun CanceledSalesDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CanceledSalesContent(
+                canceledList = canceledList,
+                menuItems = arrayOf(),
+                onMoreOptionsItemClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun CanceledSalesPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CanceledSalesContent(
+                canceledList = canceledList,
+                menuItems = arrayOf(),
+                onMoreOptionsItemClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+private val canceledList = listOf(
+    CommonItem(id = 1, photo = byteArrayOf(), title = "Homem", subtitle = "R$77.90", description = "Bruno Barbosa"),
+    CommonItem(id = 2, photo = byteArrayOf(), title = "Essencial", subtitle = "R$157.99", description = "Daniela"),
+    CommonItem(id = 3, photo = byteArrayOf(), title = "Kaiak", subtitle = "R$88.90", description = "Brenda"),
+    CommonItem(id = 4, photo = byteArrayOf(), title = "Luna", subtitle = "R$67.90", description = "Fernando"),
+    CommonItem(id = 5, photo = byteArrayOf(), title = "Una", subtitle = "R$99.99", description = "Socorro"),
+    CommonItem(id = 6, photo = byteArrayOf(), title = "Essencial", subtitle = "R$157.99", description = "Josué"),
+    CommonItem(id = 7, photo = byteArrayOf(), title = "Homem", subtitle = "R$77.90", description = "Helena"),
+)
