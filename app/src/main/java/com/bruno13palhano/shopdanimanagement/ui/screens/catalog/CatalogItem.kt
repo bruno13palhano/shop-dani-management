7799c10bcf6949afc9325e1b20cff4ff3620f401
bruno13palhano <bruno13palhano@gmail.com>
2023-10-05 08:35:42 -0300

    Add a screen to show catalog item

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/catalog/CatalogItem.kt
@@ -0,0 +1,18 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.catalog
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CatalogItemScreen(
+    catalogId: Long,
+    navigateUp: () -> Unit
+) {
+
+}
+
+@Composable
+fun CatalogItemContent(
+
+) {
+
+}
