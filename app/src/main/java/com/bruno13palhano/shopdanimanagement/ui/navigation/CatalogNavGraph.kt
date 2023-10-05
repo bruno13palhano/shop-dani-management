88f7c2ef4849ae6303b938633f99eed80e308ea0
bruno13palhano <bruno13palhano@gmail.com>
2023-10-05 19:35:20 -0300

    Add a navGraph for catalog destinations

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/CatalogNavGraph.kt
@@ -0,0 +1,58 @@
+package com.bruno13palhano.shopdanimanagement.ui.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.navArgument
+import com.bruno13palhano.shopdanimanagement.ui.screens.catalog.CatalogItemScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.catalog.CatalogScreen
+
+private const val ITEM_ID = "item_id"
+private const val ITEM_EDITABLE = "item_editable"
+
+fun NavGraphBuilder.catalogNavGraph(
+    navController: NavController,
+    showBottomMenu: (show: Boolean) -> Unit
+) {
+    navigation(
+        startDestination = CatalogDestination.CATALOG_MAIN_ROUTE,
+        route = HomeDestinations.HOME_CATALOG_ROUTE
+    ) {
+        composable(route = CatalogDestination.CATALOG_MAIN_ROUTE) {
+            CatalogScreen(
+                onItemClick = { id ->
+                    navController.navigate(
+                        route = "${CatalogDestination.CATALOG_ITEM_ROUTE}${id}${true}"
+                    )
+                },
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+        composable(
+            route = CatalogDestination.CATALOG_ITEM_WITH_ID_ROUTE,
+            arguments = listOf(
+                navArgument(ITEM_ID) { type = NavType.LongType },
+                navArgument(ITEM_EDITABLE) { type = NavType.BoolType }
+            )
+        ) { backStackEntry ->
+            showBottomMenu(true)
+            val id = backStackEntry.arguments?.getLong(ITEM_ID)
+            val editable = backStackEntry.arguments?.getBoolean(ITEM_EDITABLE)
+            if (id != null && editable != null) {
+                CatalogItemScreen(
+                    productId = if (editable) 0L else id,
+                    catalogId = if (editable) id else 0L,
+                    navigateUp = { navController.navigateUp() }
+                )
+            }
+        }
+    }
+}
+
+object CatalogDestination {
+    const val CATALOG_MAIN_ROUTE = "catalog_main_route"
+    const val CATALOG_ITEM_ROUTE = "catalog_item_route"
+    const val CATALOG_ITEM_WITH_ID_ROUTE = "$CATALOG_ITEM_ROUTE{$ITEM_ID}{$ITEM_EDITABLE}"
+}
