581a5ae8ac8ad6d20f404f1eb0fd3b77405a9fa9
bruno13palhano <bruno13palhano@gmail.com>
2023-10-05 14:09:49 -0300

    Add CatalogItem content

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/catalog/CatalogItem.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/catalog/CatalogItem.kt
@@ -1,18 +1,444 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.catalog
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.icu.text.DecimalFormat
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Description
+import androidx.compose.material.icons.filled.Discount
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material.icons.filled.Label
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Paid
+import androidx.compose.material.icons.filled.Title
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
+import com.bruno13palhano.shopdanimanagement.ui.components.clearFocusOnKeyboardDismiss
+import com.bruno13palhano.shopdanimanagement.ui.screens.catalog.viewmodel.CatalogItemViewModel
+import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
+import java.util.Locale
 
 @Composable
 fun CatalogItemScreen(
+    productId: Long,
     catalogId: Long,
-    navigateUp: () -> Unit
+    navigateUp: () -> Unit,
+    viewModel: CatalogItemViewModel = hiltViewModel()
 ) {
+    val editable = catalogId != 0L
+    LaunchedEffect(key1 = Unit) {
+        if (editable) {
+            viewModel.getProduct(id = productId)
+        } else {
+            viewModel.getCatalogItem(id = catalogId)
+        }
+    }
 
+    val menuOptions = arrayOf(
+        stringResource(id = R.string.delete_label),
+    )
+
+    CatalogItemContent(
+        editable = editable,
+        name = viewModel.name,
+        photo = viewModel.photo,
+        title = viewModel.title,
+        description = viewModel.description,
+        discount = viewModel.discount,
+        price = viewModel.price,
+        menuOptions = menuOptions,
+        onTitleChange = viewModel::updateTitle,
+        onDescriptionChange = viewModel::updateDescription,
+        onDiscountChange = viewModel::updateDiscount,
+        onPriceChange = viewModel::updatePrice,
+        onMenuOptionsItemClick = { index ->
+            when (index) {
+                0 -> {
+                    viewModel.delete()
+                    navigateUp()
+                }
+            }
+        },
+        onDoneButtonClick = {
+            if (catalogId == 0L) viewModel.insert()
+            else viewModel.update()
+            navigateUp()
+        },
+        navigateUp = navigateUp
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CatalogItemContent(
-
+    editable: Boolean,
+    name: String,
+    photo: ByteArray,
+    title: String,
+    description: String,
+    discount: String,
+    price: String,
+    menuOptions: Array<String>,
+    onTitleChange: (title: String) -> Unit,
+    onDescriptionChange: (description: String) -> Unit,
+    onDiscountChange: (discount: String) -> Unit,
+    onPriceChange: (price: String) -> Unit,
+    onMenuOptionsItemClick: (index: Int) -> Unit,
+    onDoneButtonClick: () -> Unit,
+    navigateUp: () -> Unit
 ) {
+    var expanded by remember { mutableStateOf(false) }
+    val decimalFormat = DecimalFormat.getInstance(Locale.getDefault()) as DecimalFormat
+    val decimalSeparator = decimalFormat.decimalFormatSymbols.decimalSeparator
+    val pattern = remember { Regex("^\\d*\\$decimalSeparator?\\d*\$") }
+    val patternInt = remember { Regex("^\\d*") }
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.product_label)) },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                },
+                actions = {
+                    if (editable) {
+                        IconButton(onClick = { expanded = true }) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.more_options_label)
+                            )
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MoreOptionsMenu(
+                                    items = menuOptions,
+                                    expanded = expanded,
+                                    onDismissRequest = { expandedValue ->
+                                        expanded = expandedValue
+                                    },
+                                    onClick = onMenuOptionsItemClick
+                                )
+                            }
+                        }
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = onDoneButtonClick) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_label)
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxHeight()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ElevatedCard(
+                modifier = Modifier
+                    .padding(16.dp),
+                shape = RoundedCornerShape(5)
+            ) {
+                if (photo.isEmpty()) {
+                    Image(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(5)),
+                        imageVector = Icons.Filled.Image,
+                        contentDescription = stringResource(id = R.string.customer_photo_label),
+                        contentScale = ContentScale.Crop
+                    )
+                } else {
+                    Image(
+                        modifier = Modifier
+                            .size(200.dp)
+                            .padding(8.dp)
+                            .clip(RoundedCornerShape(5)),
+                        painter = rememberAsyncImagePainter(model = photo),
+                        contentDescription = stringResource(id = R.string.customer_photo_label),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = name,
+                onValueChange = {},
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Title,
+                        contentDescription = stringResource(id = R.string.product_name_label)
+                    )
+                },
+                singleLine = true,
+                readOnly = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.product_name_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = title,
+                onValueChange = onTitleChange,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Label,
+                        contentDescription = stringResource(id = R.string.title_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.title_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_title_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = discount,
+                onValueChange = { discountValue ->
+                    if (discountValue.isEmpty() || discountValue.matches(patternInt)) {
+                        onDiscountChange(discountValue)
+                    }
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Discount,
+                        contentDescription = stringResource(id = R.string.discount_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Number
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.discount_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_discount_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = price,
+                onValueChange = { priceValue ->
+                    if (priceValue.isEmpty() || priceValue.matches(pattern)) {
+                        onPriceChange(priceValue)
+                    }
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Paid,
+                        contentDescription = stringResource(id = R.string.purchase_price_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Decimal
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.price_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_price_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+                    .fillMaxWidth()
+                    .clearFocusOnKeyboardDismiss(),
+                value = description,
+                onValueChange = onDescriptionChange,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Description,
+                        contentDescription = stringResource(id = R.string.description_label)
+                    )
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    defaultKeyboardAction(ImeAction.Done)
+                }),
+                singleLine = true,
+                label = {
+                    Text(
+                        text = stringResource(id = R.string.description_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                },
+                placeholder = {
+                    Text(
+                        text = stringResource(id = R.string.enter_description_label),
+                        fontStyle = FontStyle.Italic
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun CatalogItemDynamicPreview() {
+    ShopDaniManagementTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CatalogItemContent(
+                editable = false,
+                name = "",
+                photo = byteArrayOf(),
+                title = "",
+                description = "",
+                discount = "",
+                price = "",
+                menuOptions = arrayOf(),
+                onTitleChange = {},
+                onDescriptionChange = {},
+                onDiscountChange = {},
+                onPriceChange = {},
+                onMenuOptionsItemClick = {},
+                onDoneButtonClick = {},
+                navigateUp = {}
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Preview(uiMode = UI_MODE_NIGHT_YES, showSystemUi = true)
+@Composable
+fun CatalogItemPreview() {
+    ShopDaniManagementTheme(
+        dynamicColor = false
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            CatalogItemContent(
+                editable = true,
+                name = "",
+                photo = byteArrayOf(),
+                title = "",
+                description = "",
+                discount = "",
+                price = "",
+                menuOptions = arrayOf(),
+                onTitleChange = {},
+                onDescriptionChange = {},
+                onDiscountChange = {},
+                onPriceChange = {},
+                onMenuOptionsItemClick = {},
+                onDoneButtonClick = {},
+                navigateUp = {}
+            )
+        }
+    }
 }
