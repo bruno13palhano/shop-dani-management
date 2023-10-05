ed5a6ecd1142e38bc751da7adf6bda343f038e18
bruno13palhano <bruno13palhano@gmail.com>
2023-10-05 07:33:23 -0300

    Add methods to get catalog items ordered by name and price

--- core/src/main/java/com/bruno13palhano/core/data/CatalogData.kt
+++ core/src/main/java/com/bruno13palhano/core/data/CatalogData.kt
@@ -1,3 +1,8 @@
 package com.bruno13palhano.core.data
 
-interface CatalogData<T> : DataOperations<T>
+import kotlinx.coroutines.flow.Flow
+
+interface CatalogData<T> : DataOperations<T> {
+    fun getOrderedByName(isOrderedAsc: Boolean): Flow<List<T>>
+    fun getOrderedByPrice(isOrderedAsc: Boolean): Flow<List<T>>
+}
