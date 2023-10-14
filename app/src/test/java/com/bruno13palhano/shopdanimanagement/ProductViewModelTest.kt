77087fc810e4ada6eea19d9e760e5d23e63f1bb2
bruno13palhano <bruno13palhano@gmail.com>
2023-10-14 17:21:57 -0300

    Refactor

--- app/src/test/java/com/bruno13palhano/shopdanimanagement/ProductViewModelTest.kt
+++ app/src/test/java/com/bruno13palhano/shopdanimanagement/ProductViewModelTest.kt
@@ -1,6 +1,5 @@
 package com.bruno13palhano.shopdanimanagement
 
-import app.cash.turbine.turbineScope
 import com.bruno13palhano.core.data.CategoryData
 import com.bruno13palhano.core.data.ProductData
 import com.bruno13palhano.core.model.Category
@@ -41,43 +40,25 @@ class ProductViewModelTest {
     @get: Rule
     val standardDispatcherRule = StandardDispatcherRule()
 
-    private lateinit var product: Product
+    private var name = "Essencial"
+    private var code = "2"
+    private var description = "Perfume masculino"
+    private var photo = byteArrayOf()
+    private var date: Long = 0L
+    private var categories = listOf(
+        Category(id = 1L, name = "Perfumes"),
+        Category(id = 2L, name = "Soaps"),
+        Category(id = 3L, name = "Others")
+    )
+    private var company = "Natura"
 
     @Before
     fun setup() {
         sut = ProductViewModel(productRepository, categoryRepository)
-
-        product = Product(
-            id = 1L,
-            name = "Homem",
-            code = "1",
-            description = "tests",
-            photo = byteArrayOf(),
-            date = 100000L,
-            categories = listOf(Category(id = 1L, name = "Perfumes")),
-            company = "Avon"
-        )
     }
-
-    @Test
-    fun updateName_shouldChangeTheProductName() = runTest {
-        val nameResult = "Bruno"
-
-        turbineScope {
-            sut.updateName("Bruno")
-            assertEquals(nameResult, sut.name)
-        }
-    }
-
 
     @Test
     fun getAllCategories_shouldSetAllCategoriesProperty() = runTest {
-        val categories = listOf(
-            Category(id = 1L, name = "Perfumes"),
-            Category(id = 2L, name = "Soaps"),
-            Category(id = 3L, name = "Others")
-        )
-
         val categoriesCheck = listOf(
             CategoryCheck(id = 1L, category = "Perfumes", isChecked = false),
             CategoryCheck(id = 2L, category = "Soaps", isChecked = false),
@@ -94,44 +75,55 @@ class ProductViewModelTest {
 
     @Test
     fun getById_shouldSetProductProperties_ifProductExists() = runTest {
-        `when`(productRepository.getById(id = 1L)).thenReturn(flow { emit(product) } )
+        `when`(productRepository.getById(id = 1L))
+            .thenReturn(
+                flow {
+                    emit(
+                        Product(
+                            id = 1L,
+                            name = name,
+                            code = code,
+                            description = description,
+                            photo = photo,
+                            date = date,
+                            categories = categories,
+                            company = company
+                        )
+                    )
+                }
+            )
         sut.getProduct(id = 1L)
 
         advanceUntilIdle()
-        productPropertiesTest()
+        propertiesTest()
     }
 
     @Test
     fun updateName_shouldChangeNameProperty() {
-        val name = "Essencial"
         sut.updateName(name)
         assertEquals(name, sut.name)
     }
 
     @Test
     fun updateCode_shouldChangeCodeProperty() {
-        val code = "3"
         sut.updateCode(code)
         assertEquals(code, sut.code)
     }
 
     @Test
     fun updateDescription_shouldChangeDescriptionProperty() {
-        val description = "Perfume masculino"
         sut.updateDescription(description)
         assertEquals(description, sut.description)
     }
 
     @Test
     fun updatePhoto_shouldChangePhotoProperty() {
-        val photo = byteArrayOf()
         sut.updatePhoto(photo)
         assertEquals(photo, sut.photo)
     }
 
     @Test
     fun updateDate_shouldChangeDateProperty() {
-        val date = 0L
         sut.updateDate(date)
         assertEquals(date, sut.date)
     }
@@ -141,115 +133,60 @@ class ProductViewModelTest {
         val category = listOf(
             CategoryCheck(id = 1L, category = "Perfumes", isChecked = false),
             CategoryCheck(id = 2L, category = "Soaps", isChecked = false),
-            CategoryCheck(id = 3L, category = "Others", isChecked = false)
+            CategoryCheck(id = 3L, category = "Others", isChecked = true)
         )
         sut.updateCategories(category)
-        assertEquals(category.joinToString(", ") { it.category }, sut.category)
+        assertEquals(category[2].category, sut.category)
     }
 
     @Test
     fun updateCompany_shouldChangeCompanyProperty() {
-        val company = "Natura"
         sut.updateCompany(company)
         assertEquals(company, sut.company)
     }
 
     @Test
     fun insert_shouldSetProductProperties() = runTest {
-        val name = "Essencial"
-        val code = "2"
-        val description = "Perfume masculino"
-        val photo = byteArrayOf()
-        val date = 200000L
-        val company = "Natura"
-
-        `when`(productRepository.insert(any()))
-            .then {
-                sut.updateName(name)
-                sut.updateCode(code)
-                sut.updateDescription(description)
-                sut.updatePhoto(photo)
-                sut.updateDate(date)
-                sut.updateCompany(company)
-            }
-
+        `when`(productRepository.insert(any())).then { setProperties() }
         sut.insertProduct()
 
         advanceUntilIdle()
-
-        assertEquals(name, sut.name)
-        assertEquals(code, sut.code)
-        assertEquals(description, sut.description)
-        assertEquals(photo, sut.photo)
-        assertEquals(date, sut.date)
-        assertEquals(company, sut.company)
+        propertiesTest()
     }
 
     @Test
     fun update_shouldChangeProductProperties() = runTest {
-        val name = "Essencial"
-        val code = "2"
-        val description = "Perfume masculino"
-        val photo = byteArrayOf()
-        val date = 200000L
-        val company = "Natura"
-
-        `when`(productRepository.update(any())).then {
-            sut.updateName(name)
-            sut.updateCode(code)
-            sut.updateDescription(description)
-            sut.updatePhoto(photo)
-            sut.updateDate(date)
-            sut.updateCompany(company)
-        }
-
+        `when`(productRepository.update(any())).then { setProperties() }
         sut.updateProduct(id = 1L)
 
         advanceUntilIdle()
-        assertEquals(name, sut.name)
-        assertEquals(code, sut.code)
-        assertEquals(description, sut.description)
-        assertEquals(photo, sut.photo)
-        assertEquals(date, sut.date)
-        assertEquals(company, sut.company)
+        propertiesTest()
     }
 
     @Test
     fun delete_shouldSetProductProperties() = runTest {
-        val name = "Essencial"
-        val code = "2"
-        val description = "Perfume masculino"
-        val photo = byteArrayOf()
-        val date = 200000L
-        val company = "Natura"
-
-        `when`(productRepository.deleteById(id = 1L)).then {
-            sut.updateName(name)
-            sut.updateCode(code)
-            sut.updateDescription(description)
-            sut.updatePhoto(photo)
-            sut.updateDate(date)
-            sut.updateCompany(company)
-        }
-
+        `when`(productRepository.deleteById(id = 1L)).then { setProperties() }
         sut.deleteProduct(id = 1L)
 
         advanceUntilIdle()
+        propertiesTest()
+    }
+
+    private fun setProperties() {
+        sut.updateName(name)
+        sut.updateCode(code)
+        sut.updateDescription(description)
+        sut.updatePhoto(photo)
+        sut.updateDate(date)
+        sut.updateCompany(company)
+    }
+
+    private fun propertiesTest() {
         assertEquals(name, sut.name)
         assertEquals(code, sut.code)
         assertEquals(description, sut.description)
         assertEquals(photo, sut.photo)
         assertEquals(date, sut.date)
         assertEquals(company, sut.company)
-    }
-
-    private fun productPropertiesTest() {
-        assertEquals(product.name, sut.name)
-        assertEquals(product.code, sut.code)
-        assertEquals(product.description, sut.description)
-        assertEquals(product.photo, sut.photo)
-        assertEquals(product.date, sut.date)
-        assertEquals(product.categories.joinToString(", ") { it.name }, sut.category)
-        assertEquals(product.company, sut.company)
     }
 }
