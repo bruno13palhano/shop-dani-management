f18457f17591c39f3cb56600b1b551af22496dbb
bruno13palhano <bruno13palhano@gmail.com>
2023-10-24 20:04:14 -0300

    Add a test to verify the value of lastSalesEntry in the LastSalesViewModel

--- app/src/test/java/com/bruno13palhano/shopdanimanagement/insights/LastSalesViewModelTest.kt
+++ app/src/test/java/com/bruno13palhano/shopdanimanagement/insights/LastSalesViewModelTest.kt
@@ -3,12 +3,18 @@ package com.bruno13palhano.shopdanimanagement.insights
 import com.bruno13palhano.core.data.SaleData
 import com.bruno13palhano.core.model.Sale
 import com.bruno13palhano.shopdanimanagement.StandardDispatcherRule
+import com.bruno13palhano.shopdanimanagement.makeRandomSale
 import com.bruno13palhano.shopdanimanagement.repository.TestSaleRepository
 import com.bruno13palhano.shopdanimanagement.ui.screens.insights.viewmodel.LastSalesViewModel
+import com.bruno13palhano.shopdanimanagement.ui.screens.setChartEntries
+import com.bruno13palhano.shopdanimanagement.ui.screens.setQuantity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -21,6 +27,8 @@ import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
@@ -32,8 +40,29 @@ class LastSalesViewModelTest {
     @get: Rule
     val standardDispatcherRule = StandardDispatcherRule()
 
+    private val currentDay = LocalDate.now()
     private lateinit var saleRepository: SaleData<Sale>
     private lateinit var sut: LastSalesViewModel
+
+    private val sales = listOf(
+        makeRandomSale(
+            id = 1L,
+            dateOfSale = currentDay.minusDays(2).atStartOfDay()
+                .toInstant(ZoneOffset.UTC).toEpochMilli(),
+            canceled = false
+        ),
+        makeRandomSale(
+            id = 2L,
+            dateOfSale = currentDay.minusDays(1).atStartOfDay()
+                .toInstant(ZoneOffset.UTC).toEpochMilli(),
+            canceled = false
+        ),
+        makeRandomSale(
+            id = 3L,
+            dateOfSale = currentDay.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli(),
+            canceled = false
+        )
+    )
 
     @Before
     fun setup() {
@@ -52,5 +81,31 @@ class LastSalesViewModelTest {
         advanceUntilIdle()
 
         verify(saleRepository).getLastSales(any(), any())
+    }
+
+    @Test
+    fun setLastSalesEntryByRange_shouldSetLastSalesEntryProperty() = runTest {
+        insertSales()
+
+        val collectJob = launch { sut.lastSalesEntry.collect() }
+
+        sut.setLastSalesEntryByRange(rangeOfDays = 7)
+        advanceUntilIdle()
+
+        assertEquals(mapToEntries(sales), sut.lastSalesEntry.value)
+
+        collectJob.cancel()
+    }
+
+    private suspend fun insertSales() = sales.forEach { saleRepository.insert(it) }
+
+    private fun mapToEntries(sales: List<Sale>): List<Pair<String, Float>> {
+        val entries = mutableListOf<Pair<String, Float>>()
+        val days = Array(7) { 0 }
+
+        sales.map { sale -> setQuantity(days, sale.dateOfSale, sale.quantity) }
+        setChartEntries(entries, days)
+
+        return entries
     }
 }
