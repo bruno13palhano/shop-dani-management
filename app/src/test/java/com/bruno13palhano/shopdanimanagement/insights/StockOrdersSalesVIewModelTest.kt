f11321cbbff2d9c81cc97d29b444c920faf27b46
bruno13palhano <bruno13palhano@gmail.com>
2023-10-24 20:53:13 -0300

    Add a test to verify the value of allSalles in the StockOrdersSaslesViewModel

--- app/src/test/java/com/bruno13palhano/shopdanimanagement/insights/StockOrdersSalesVIewModelTest.kt
+++ app/src/test/java/com/bruno13palhano/shopdanimanagement/insights/StockOrdersSalesVIewModelTest.kt
@@ -3,12 +3,19 @@ package com.bruno13palhano.shopdanimanagement.insights
 import com.bruno13palhano.core.data.SaleData
 import com.bruno13palhano.core.model.Sale
 import com.bruno13palhano.shopdanimanagement.StandardDispatcherRule
+import com.bruno13palhano.shopdanimanagement.makeRandomSale
+import com.bruno13palhano.shopdanimanagement.makeRandomStockOrder
 import com.bruno13palhano.shopdanimanagement.repository.TestSaleRepository
 import com.bruno13palhano.shopdanimanagement.ui.screens.insights.viewmodel.StockOrdersSalesViewModel
+import com.bruno13palhano.shopdanimanagement.ui.screens.setChartEntries
+import com.bruno13palhano.shopdanimanagement.ui.screens.setQuantity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -21,6 +28,8 @@ import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
@@ -32,8 +41,29 @@ class StockOrdersSalesVIewModelTest {
     @get: Rule
     val standardDispatcherRule = StandardDispatcherRule()
 
+    private val currentDay = LocalDate.now()
     private lateinit var saleRepository: SaleData<Sale>
     private lateinit var sut: StockOrdersSalesViewModel
+
+    private val sales = listOf(
+        makeRandomSale(
+            id = 1L,
+            dateOfSale = currentDay.minusDays(2).atStartOfDay()
+                .toInstant(ZoneOffset.UTC).toEpochMilli(),
+            canceled = false
+        ),
+        makeRandomSale(
+            id = 2L,
+            dateOfSale = currentDay.minusDays(1).atStartOfDay()
+                .toInstant(ZoneOffset.UTC).toEpochMilli(),
+            canceled = false
+        ),
+        makeRandomSale(
+            id = 3L,
+            dateOfSale = currentDay.atStartOfDay().toInstant(ZoneOffset.UTC).toEpochMilli(),
+            canceled = false
+        )
+    )
 
     @Before
     fun setup() {
@@ -52,5 +82,40 @@ class StockOrdersSalesVIewModelTest {
         advanceUntilIdle()
 
         verify(saleRepository).getLastSales(any(), any())
+    }
+
+    @Test
+    fun setStockOrderSalesRange_shouldSetAllSalesProperty() = runTest {
+        insertSales()
+
+        val collectJob = launch { sut.allSales.collect() }
+
+        sut.setStockOrdersSalesRange(rangeOfDays = 7)
+        advanceUntilIdle()
+
+        assertEquals(mapToSalesEntries(sales), sut.allSales.value)
+
+        collectJob.cancel()
+    }
+
+    private suspend fun insertSales() = sales.forEach { saleRepository.insert(it) }
+
+    private fun mapToSalesEntries(sales: List<Sale>): StockOrdersSalesViewModel.ItemsSalesEntries {
+        val stockEntries = mutableListOf<Pair<String, Float>>()
+        val ordersEntries = mutableListOf<Pair<String, Float>>()
+        val days = Array(7) { 0 }
+
+        sales.filter { sale -> !sale.isOrderedByCustomer }
+            .map { sale -> setQuantity(days, sale.dateOfSale, sale.quantity) }
+        setChartEntries(stockEntries, days)
+
+        sales.filter { sale -> sale.isOrderedByCustomer }
+            .map { sale -> setQuantity(days, sale.dateOfSale, sale.quantity) }
+        setChartEntries(ordersEntries, days)
+
+        return StockOrdersSalesViewModel.ItemsSalesEntries(
+            stockEntries = stockEntries,
+            ordersEntries = ordersEntries
+        )
     }
 }
