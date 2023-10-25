56469162f749b502a3d00fbcef159b3f9e0776d3
bruno13palhano <bruno13palhano@gmail.com>
2023-10-24 21:22:17 -0300

    Refactor

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomerInfoViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomerInfoViewModel.kt
@@ -8,6 +8,8 @@ import com.bruno13palhano.core.data.di.CustomerRep
 import com.bruno13palhano.core.data.di.SaleRep
 import com.bruno13palhano.core.model.Customer
 import com.bruno13palhano.core.model.Sale
+import com.bruno13palhano.shopdanimanagement.ui.screens.setChartEntries
+import com.bruno13palhano.shopdanimanagement.ui.screens.setQuantity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
@@ -15,11 +17,6 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,8 +24,6 @@ class CustomerInfoViewModel @Inject constructor(
     @CustomerRep private val customerRepository: CustomerData<Customer>,
     @SaleRep private val saleRepository: SaleData<Sale>
 ) : ViewModel() {
-    private val currentDay = LocalDate.now()
-
     private val _customerPurchases = MutableStateFlow(emptyList<Sale>())
     private val _customer = MutableStateFlow(Customer(0L, "", byteArrayOf(), "", "", ""))
 
@@ -92,26 +87,6 @@ class CustomerInfoViewModel @Inject constructor(
                 .collect {
                     _entry.value = it
                 }
-        }
-    }
-
-    private fun setQuantity(days: Array<Int>, date: Long, quantity: Int) {
-        for (i in days.indices) {
-            if (LocalDateTime.ofInstant(Instant.ofEpochMilli(date), ZoneId.of("UTC")).toLocalDate()
-                == currentDay.minusDays(i.toLong())) {
-                days[i] += quantity
-            }
-        }
-    }
-
-    private fun setChartEntries(chart: MutableList<Pair<String, Float>>, days: Array<Int>) {
-        for (i in days.size-1 downTo 0) {
-            chart.add(
-                Pair(
-                    DateTimeFormatter.ofPattern("dd/MM").format(currentDay.minusDays(i.toLong())),
-                    days[i].toFloat()
-                )
-            )
         }
     }
 
