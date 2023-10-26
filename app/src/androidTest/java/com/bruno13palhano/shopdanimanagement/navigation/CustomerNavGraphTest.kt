cf6d08e84089e159cf83e60fae7c2d6d0bc4b4cc
bruno13palhano <bruno13palhano@gmail.com>
2023-10-26 19:53:31 -0300

    Add test for customersNavGraph

---
+++ app/src/androidTest/java/com/bruno13palhano/shopdanimanagement/navigation/CustomerNavGraphTest.kt
@@ -0,0 +1,142 @@
+package com.bruno13palhano.shopdanimanagement.navigation
+
+import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onLast
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.espresso.Espresso
+import com.bruno13palhano.shopdanimanagement.MainActivity
+import com.bruno13palhano.shopdanimanagement.ui.navigation.CustomersDestinations
+import com.bruno13palhano.shopdanimanagement.ui.navigation.MainNavGraph
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+private const val ITEM_ID = "item_id"
+private const val EDITABLE = "item_editable"
+
+@HiltAndroidTest
+class CustomerNavGraphTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+
+        composeTestRule.activity.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            MainNavGraph(
+                navController = navController,
+            ) {}
+            navController.navigate(CustomersDestinations.MAIN_CUSTOMERS_ROUTE)
+        }
+    }
+
+    @Test
+    fun verifyCustomerNavGraphStartDestination() {
+        composeTestRule.onNodeWithText("Customers")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun onItemClick_fromCustomersScreen_shouldNavigateToCustomerInfoScreen() {
+        val expected = "${CustomersDestinations.CUSTOMERS_CUSTOMER_INFO_ROUTE}/{$ITEM_ID}"
+
+        composeTestRule.onNodeWithContentDescription("List of customers")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onSearchClick_fromCustomersScreen_shouldNavigateToSearchCustomerScreen() {
+        val expected = CustomersDestinations.CUSTOMERS_SEARCH_ROUTE
+
+        composeTestRule.onNodeWithContentDescription("Search").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onAddButtonClick_fromCustomersScreen_shouldNavigateToCustomerScreen() {
+        val expected = "${CustomersDestinations.CUSTOMERS_CUSTOMER_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeTestRule.onNodeWithContentDescription("Add").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onItemClick_FromSearchCustomersScreen_shouldNavigateToCustomerScreen() {
+        val expected = "${CustomersDestinations.CUSTOMERS_CUSTOMER_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeTestRule.activity.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            MainNavGraph(
+                navController = navController,
+            ) {}
+            navController.navigate(CustomersDestinations.CUSTOMERS_SEARCH_ROUTE)
+        }
+
+        composeTestRule.onNodeWithContentDescription("Search bar")
+            .performClick()
+
+        composeTestRule.onNodeWithContentDescription("List of search")
+            .onChildren()
+            .onLast()
+            .performClick()
+        Espresso.closeSoftKeyboard()
+
+        composeTestRule.onNodeWithContentDescription("List of customers")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onEditIconClick_FromCustomerInfoScreen_shouldNavigateToCustomerScreen() {
+        val expected = "${CustomersDestinations.CUSTOMERS_CUSTOMER_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeTestRule.onNodeWithContentDescription("List of customers")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeTestRule.onNodeWithContentDescription("Edit").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+}
