711cb6054a67ff92a3b2317b6bde5d3a5f5bfffc
bruno13palhano <bruno13palhano@gmail.com>
2023-10-27 19:54:54 -0300

    Add semantics for LazyColumn in the StockDebitsScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/StockDebtisScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/StockDebtisScreen.kt
@@ -26,6 +26,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -127,7 +129,9 @@ fun StockDebitsContent(
         }
     ) {
         LazyColumn(
-            modifier = Modifier.padding(it),
+            modifier = Modifier
+                .semantics { contentDescription = "List of items" }
+                .padding(it),
             contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp)
         ) {
             items(items = stockItems, key = { stockItem -> stockItem.id }) { stockItem ->
