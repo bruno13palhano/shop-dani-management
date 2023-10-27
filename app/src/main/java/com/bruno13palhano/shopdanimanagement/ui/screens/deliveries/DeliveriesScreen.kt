284febcc92219783d7fcf61972eb91b2a298bd67
bruno13palhano <bruno13palhano@gmail.com>
2023-10-27 13:32:59 -0300

    Add semantics to lazyColumn

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/deliveries/DeliveriesScreen.kt
@@ -26,6 +26,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -122,7 +124,9 @@ fun DeliveriesContent(
         }
     ) {
         LazyColumn(
-            modifier = Modifier.padding(it),
+            modifier = Modifier
+                .semantics { contentDescription = "List of deliveries" }
+                .padding(it),
             contentPadding = PaddingValues(vertical = 4.dp)
         ) {
             items(items = deliveries, key = { delivery -> delivery.id }) { delivery ->
