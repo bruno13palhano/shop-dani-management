137b97e6aad62287a2e8ba073729c2368072e537
bruno13palhano <bruno13palhano@gmail.com>
2023-10-26 22:07:05 -0300

    Add test for CatalogNavGraph

---
+++ app/src/androidTest/java/com/bruno13palhano/shopdanimanagement/navigation/CatalogNavGraphTest.kt
@@ -0,0 +1,96 @@
+package com.bruno13palhano.shopdanimanagement.navigation
+
+import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.bruno13palhano.shopdanimanagement.MainActivity
+import com.bruno13palhano.shopdanimanagement.ui.navigation.CatalogDestination
+import com.bruno13palhano.shopdanimanagement.ui.navigation.HomeDestinations
+import com.bruno13palhano.shopdanimanagement.ui.navigation.MainNavGraph
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+private const val ITEM_ID = "item_id"
+private const val EDITABLE = "editable"
+
+@HiltAndroidTest
+class CatalogNavGraphTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+
+        composeTestRule.activity.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            MainNavGraph(navController = navController) { }
+            navController.navigate(CatalogDestination.CATALOG_MAIN_ROUTE)
+        }
+    }
+
+    @Test
+    fun verifyCatalogNavGraphStartDestination() {
+        composeTestRule.onNodeWithText("Catalog").assertIsDisplayed()
+    }
+
+    @Test
+    fun onNavigateUp_fromCatalogScreen_shouldNavigateToHome() {
+        val expected = HomeDestinations.HOME_MAIN_ROUTE
+
+        composeTestRule.onNodeWithContentDescription("Up button").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onItemClick_fromCatalogScreen_shouldNavigateToCatalogItemScreen() {
+        val expected = "${CatalogDestination.CATALOG_ITEM_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeTestRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onNavigateUp_fromCatalogItemScreen_shouldNavigateToCatalogScreen() {
+        val expected = CatalogDestination.CATALOG_MAIN_ROUTE
+
+        composeTestRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeTestRule.onNodeWithContentDescription("Up button").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+}
