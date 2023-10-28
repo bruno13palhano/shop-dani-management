c0baa1e1852049cb4dd711526ffe2ab35ca26f50
bruno13palhano <bruno13palhano@gmail.com>
2023-10-27 21:50:20 -0300

    Rename

--- app/src/androidTest/java/com/bruno13palhano/shopdanimanagement/navigation/HomeNavGraphTest.kt
+++ app/src/androidTest/java/com/bruno13palhano/shopdanimanagement/navigation/HomeNavGraphTest.kt
@@ -28,7 +28,7 @@ import org.junit.Rule
 import org.junit.Test
 
 @HiltAndroidTest
-class MainNavGraphTest {
+class HomeNavGraphTest {
 
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
