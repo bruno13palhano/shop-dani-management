4177d9fde60d23ef14226cc2d0ce08a43580ff18
bruno13palhano <bruno13palhano@gmail.com>
2023-10-28 21:03:09 -0300

    Add unit test for ProductsNavGraph

---
+++ app/src/androidTest/java/com/bruno13palhano/shopdanimanagement/navigation/ProductsNavGraphTest.kt
@@ -0,0 +1,260 @@
+package com.bruno13palhano.shopdanimanagement.navigation
+
+import androidx.activity.compose.setContent
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onLast
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.espresso.Espresso
+import com.bruno13palhano.shopdanimanagement.MainActivity
+import com.bruno13palhano.shopdanimanagement.ui.navigation.MainNavGraph
+import com.bruno13palhano.shopdanimanagement.ui.navigation.ProductsDestinations
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+private const val ITEM_ID = "item_Id"
+private const val EDITABLE = "item_editable"
+
+@HiltAndroidTest
+class ProductsNavGraphTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+
+        composeRule.activity.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            MainNavGraph(navController = navController) { }
+            navController.navigate(ProductsDestinations.MAIN_PRODUCTS_ROUTE)
+        }
+    }
+
+    @Test
+    fun verifyProductsNavGraphStartDestination() {
+        val expected = ProductsDestinations.MAIN_PRODUCTS_ROUTE
+
+        composeRule.onNodeWithText("Product Categories").assertIsDisplayed()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onItemClick_fromProductCategoriesScreen_shouldNavigateToProductListScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_LIST_ROUTE}/{$ITEM_ID}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onItemClick_fromProductListScreen_shouldNavigateToProductScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_PRODUCT_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onSearchClick_fromProductListScreen_shouldNavigateToSearchProductScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_SEARCH_ROUTE}/{$ITEM_ID}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("Search").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onAddButtonClick_fromProductListScreen_shouldNavigateToProductScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_PRODUCT_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("Add").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onNavigateUp_fromProductListScreen_shouldNavigateToProductCategoriesScreen() {
+        val expected = ProductsDestinations.MAIN_PRODUCTS_ROUTE
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("Up button").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onItemClick_fromSearchProductScreen_shouldNavigateToProductScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_PRODUCT_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+        composeRule.onNodeWithContentDescription("Search").performClick()
+        composeRule.onNodeWithText("Search for products").performClick()
+        Espresso.closeSoftKeyboard()
+
+        composeRule.onNodeWithContentDescription("List of search")
+            .onChildren()
+            .onLast()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onNavigateUp_fromSearchProductScreen_shouldNavigateToProductListScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_LIST_ROUTE}/{$ITEM_ID}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+        composeRule.onNodeWithContentDescription("Search").performClick()
+
+        composeRule.onNodeWithContentDescription("Up button").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onAddToCatalogClick_fromProductScreen_shouldNavigateToCatalogItemScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_CATALOG_ITEM_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("More options").performClick()
+        composeRule.onNodeWithContentDescription("List of options")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onNavigateUp_fromProductScreen_shouldNavigateToProductListScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_LIST_ROUTE}/{$ITEM_ID}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("Up button").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+
+    @Test
+    fun onNavigateUp_fromCatalogItemScreen_shouldNavigateToProductScreen() {
+        val expected = "${ProductsDestinations.PRODUCTS_PRODUCT_ROUTE}/{$ITEM_ID}/{$EDITABLE}"
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("List of items")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("More options").performClick()
+        composeRule.onNodeWithContentDescription("List of options")
+            .onChildren()
+            .onFirst()
+            .performClick()
+
+        composeRule.onNodeWithContentDescription("Up button").performClick()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+
+        assertEquals(expected, route)
+    }
+}
