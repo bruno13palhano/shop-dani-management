cc57b82568e2e9e15edc5aebd8707f8c03085c42
bruno13palhano <bruno13palhano@gmail.com>
2023-10-28 21:00:03 -0300

    Add semantics for MoreOptionsMenu

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/Menus.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/Menus.kt
@@ -49,6 +49,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -203,6 +205,7 @@ fun MoreOptionsMenu(
     onClick: (index: Int) -> Unit
 ) {
     DropdownMenu(
+        modifier = Modifier.semantics { contentDescription = "List of options" },
         expanded = expanded,
         onDismissRequest = { onDismissRequest(false) }
     ) {
