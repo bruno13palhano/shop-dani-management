15a7104f645a92f454c2f0afac0f50fed8d52cda
bruno13palhano <bruno13palhano@gmail.com>
2023-10-31 00:13:12 -0300

    Create SpalshScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.bruno13palhano.shopdanimanagement.ui.components.BottomMenu
 import com.bruno13palhano.shopdanimanagement.ui.theme.ShopDaniManagementTheme
@@ -31,6 +32,7 @@ import kotlinx.coroutines.launch
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             ShopDaniManagementTheme {
                 val navController = rememberNavController()
