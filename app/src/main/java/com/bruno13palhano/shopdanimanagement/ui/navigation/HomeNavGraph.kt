214a791e32e11ddc5698d9c5a538a9897cfb32ec
bruno13palhano <bruno13palhano@gmail.com>
2023-10-31 21:22:03 -0300

    Add SaleScreen destination

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/HomeNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/HomeNavGraph.kt
@@ -1,10 +1,18 @@
 package com.bruno13palhano.shopdanimanagement.ui.navigation
 
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import androidx.navigation.navigation
+import com.bruno13palhano.shopdanimanagement.R
 import com.bruno13palhano.shopdanimanagement.ui.screens.home.HomeScreen
+import com.bruno13palhano.shopdanimanagement.ui.screens.sales.SaleScreen
+
+private const val ITEM_ID = "item_Id"
+private const val IS_ORDERED = "is_ordered"
 
 fun NavGraphBuilder.homeNavGraph(
     navController: NavController,
@@ -21,9 +29,38 @@ fun NavGraphBuilder.homeNavGraph(
                 onOptionsItemClick = { route ->
                     navController.navigate(route = route)
                 },
+                onSalesItemClick = { id, isOrderedByCustomer ->
+                    navController.navigate(
+                        route = "${HomeDestinations.HOME_SALE_ROUTE}/$id/$isOrderedByCustomer"
+                    )
+                },
                 onMenuClick = onIconMenuClick,
             )
         }
+
+        composable(
+            route = "${HomeDestinations.HOME_SALE_ROUTE}/{$ITEM_ID}/{$IS_ORDERED}",
+            arguments = listOf(
+                navArgument(ITEM_ID) { type = NavType.LongType },
+                navArgument(IS_ORDERED) { type = NavType.BoolType }
+            )
+        ) { backStackEntry ->
+            showBottomMenu(true)
+            val saleId = backStackEntry.arguments?.getLong(ITEM_ID)
+            val isOrdered = backStackEntry.arguments?.getBoolean(IS_ORDERED)
+
+            if (saleId != null && isOrdered != null) {
+                SaleScreen(
+                    isEdit = true,
+                    screenTitle = stringResource(id = R.string.edit_sale_label),
+                    isOrderedByCustomer = isOrdered,
+                    stockOrderId = 0L,
+                    saleId = saleId,
+                    navigateUp = { navController.navigateUp() }
+                )
+            }
+        }
+
         salesNavGraph(
             navController = navController,
             showBottomMenu = showBottomMenu
@@ -54,4 +91,5 @@ object HomeDestinations {
     const val HOME_ORDERS_ROUTE = "home_orders_route"
     const val HOME_DELIVERIES_ROUTE = "home_deliveries_route"
     const val HOME_CATALOG_ROUTE = "home_catalog_route"
+    const val HOME_SALE_ROUTE = "home_sale_route"
 }
