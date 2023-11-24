56e49e4ba563ad9faea75df58307d942b9e45acc
bruno13palhano <bruno13palhano@gmail.com>
2023-11-24 18:07:21 -0300

    Add insert, update and delete methods to CategoryNetwork interface

--- core/src/main/java/com/bruno13palhano/core/network/category/CategoryNetwork.kt
+++ core/src/main/java/com/bruno13palhano/core/network/category/CategoryNetwork.kt
@@ -4,4 +4,7 @@ import com.bruno13palhano.core.model.Category
 
 internal interface CategoryNetwork {
     suspend fun getAll(): List<Category>
+    suspend fun insertCategory(category: Category)
+    suspend fun updateCategory(category: Category)
+    suspend fun deleteCategory(id: Long)
 }
