1d21d1ed762d16edcd004c6e0cfdaf7a5a0e2b22
bruno13palhano <bruno13palhano@gmail.com>
2023-11-24 18:42:54 -0300

    Add a bind to inject ProductNetwork

--- core/src/main/java/com/bruno13palhano/core/network/di/NetworkModule.kt
+++ core/src/main/java/com/bruno13palhano/core/network/di/NetworkModule.kt
@@ -2,6 +2,8 @@ package com.bruno13palhano.core.network.di
 
 import com.bruno13palhano.core.network.category.CategoryNetwork
 import com.bruno13palhano.core.network.category.CategoryNetworkRetrofit
+import com.bruno13palhano.core.network.product.ProductNetwork
+import com.bruno13palhano.core.network.product.ProductNetworkRetrofit
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,6 +14,9 @@ import javax.inject.Singleton
 @Qualifier
 internal annotation class CategoryNet
 
+@Qualifier
+internal annotation class ProductNet
+
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class NetworkModule {
@@ -20,4 +25,9 @@ internal abstract class NetworkModule {
     @Singleton
     @Binds
     abstract fun bindCategoryNetwork(categoryNetwork: CategoryNetworkRetrofit): CategoryNetwork
+
+    @ProductNet
+    @Singleton
+    @Binds
+    abstract fun bindProductNetwork(productNetwork: ProductNetworkRetrofit): ProductNetwork
 }
