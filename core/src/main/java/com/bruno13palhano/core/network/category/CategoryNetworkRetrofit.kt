8743d062562e17f3864918ef25278dd2973f0914
bruno13palhano <bruno13palhano@gmail.com>
2023-11-24 18:36:17 -0300

    Implement insert, update and delete methods

--- core/src/main/java/com/bruno13palhano/core/network/category/CategoryNetworkRetrofit.kt
+++ core/src/main/java/com/bruno13palhano/core/network/category/CategoryNetworkRetrofit.kt
@@ -3,6 +3,7 @@ package com.bruno13palhano.core.network.category
 import com.bruno13palhano.core.model.Category
 import com.bruno13palhano.core.network.Service
 import com.bruno13palhano.core.network.model.asExternal
+import com.bruno13palhano.core.network.model.asNetwork
 import javax.inject.Inject
 
 internal class CategoryNetworkRetrofit @Inject constructor(
@@ -11,6 +12,33 @@ internal class CategoryNetworkRetrofit @Inject constructor(
     override suspend fun getAll(): List<Category> {
         return try {
             apiService.getAllCategories().map { it.asExternal() }
-        } catch (ignored: Exception) { emptyList() }
+        } catch (ignored: Exception) {
+            ignored.printStackTrace()
+            emptyList()
+        }
+    }
+
+    override suspend fun insertCategory(category: Category) {
+        try {
+            apiService.insertCategory(category.asNetwork())
+        } catch (ignored: Exception) {
+            ignored.printStackTrace()
+        }
+    }
+
+    override suspend fun updateCategory(category: Category) {
+        try {
+            apiService.updateCategory(category.asNetwork())
+        } catch (ignored: Exception) {
+            ignored.printStackTrace()
+        }
+    }
+
+    override suspend fun deleteCategory(id: Long) {
+        try {
+            apiService.deleteCategory(id)
+        } catch (ignored: Exception) {
+            ignored.printStackTrace()
+        }
     }
 }
