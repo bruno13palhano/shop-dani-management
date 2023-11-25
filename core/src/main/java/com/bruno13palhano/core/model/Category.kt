c532b95708f5600cd30a14b939197ba50c9742bf
bruno13palhano <bruno13palhano@gmail.com>
2023-11-25 20:42:45 -0300

    Add a property for timestamp

--- core/src/main/java/com/bruno13palhano/core/model/Category.kt
+++ core/src/main/java/com/bruno13palhano/core/model/Category.kt
@@ -1,5 +1,7 @@
 package com.bruno13palhano.core.model
 
+import java.time.OffsetDateTime
+
 /**
  * Products categories.
  *
@@ -7,5 +9,6 @@ package com.bruno13palhano.core.model
  */
 data class Category(
     override val id: Long,
-    val category: String
+    val category: String,
+    val timestamp: OffsetDateTime
 ) : Model(id = id)
--- core/src/main/java/com/bruno13palhano/core/network/model/CategoryNet.kt
+++ core/src/main/java/com/bruno13palhano/core/network/model/CategoryNet.kt
@@ -2,18 +2,22 @@ package com.bruno13palhano.core.network.model
 
 import com.bruno13palhano.core.model.Category
 import com.squareup.moshi.Json
+import java.time.OffsetDateTime
 
 data class CategoryNet(
     @Json(name = "id") val id: Long,
-    @Json(name = "category") val category: String
+    @Json(name = "category") val category: String,
+    @Json(name = "timestamp") val timestamp: String
 )
 
 internal fun CategoryNet.asExternal() = Category(
     id = id,
-    category = category
+    category = category,
+    timestamp = OffsetDateTime.parse(timestamp)
 )
 
 internal fun Category.asNetwork() = CategoryNet(
     id = id,
-    category = category
+    category = category,
+    timestamp = timestamp.toString()
 )
