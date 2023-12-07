46131a5771d834d63b18f17edfc43165f7bba9ed
bruno13palhano <bruno13palhano@gmail.com>
2023-12-07 09:44:16 -0300

    Change default timestamp

--- core/src/main/java/com/bruno13palhano/core/model/DataVersion.kt
+++ core/src/main/java/com/bruno13palhano/core/model/DataVersion.kt
@@ -5,5 +5,5 @@ import java.time.OffsetDateTime
 data class DataVersion(
     override val id: Long = 0L,
     val name: String = "",
-    override val timestamp: OffsetDateTime = OffsetDateTime.now()
+    override val timestamp: OffsetDateTime = OffsetDateTime.parse("1974-01-01T00:00:00Z")
 ) : Model(id = id, timestamp = timestamp)
