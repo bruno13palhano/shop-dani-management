bb1ad6aeec79ab6cbced640db04131d6de88ba35
bruno13palhano <bruno13palhano@gmail.com>
2023-12-09 15:07:14 -0300

    Add a timestamp param to deleteById method

--- core/src/main/java/com/bruno13palhano/core/data/RepositoryOperations.kt
+++ core/src/main/java/com/bruno13palhano/core/data/RepositoryOperations.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryOperations<T> {
     suspend fun insert(model: T): Long
     suspend fun update(model: T)
-    suspend fun deleteById(id: Long)
+    suspend fun deleteById(id: Long, timestamp: String)
     fun getAll(): Flow<List<T>>
     fun getById(id: Long): Flow<T>
     fun getLast(): Flow<T>
