0a2d1c19528b12eb54ad68b6f1b5036f5ecb97d2
bruno13palhano <bruno13palhano@gmail.com>
2023-12-11 23:11:41 -0300

    Remove unused classes

--- core/src/main/java/com/bruno13palhano/core/data/repository/version/VersionData.kt
+++
@@ -1,6 +0,0 @@
-package com.bruno13palhano.core.data.repository.version
-
-import com.bruno13palhano.core.data.DataOperations
-import com.bruno13palhano.core.model.DataVersion
-
-interface VersionData : DataOperations<DataVersion>
--- core/src/main/java/com/bruno13palhano/core/data/repository/version/VersionRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.bruno13palhano.core.data.repository.version
-
-import com.bruno13palhano.core.data.RepositoryOperations
-import com.bruno13palhano.core.model.DataVersion
-
-interface VersionRepository : RepositoryOperations<DataVersion>
