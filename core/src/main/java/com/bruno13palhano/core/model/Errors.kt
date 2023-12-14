f975ea44e87b2552e2ff184a6e57f15fa9cc5a12
bruno13palhano <bruno13palhano@gmail.com>
2023-12-14 11:36:59 -0300

    Add a object to wrapper constants for errors

---
+++ core/src/main/java/com/bruno13palhano/core/model/Errors.kt
@@ -0,0 +1,10 @@
+package com.bruno13palhano.core.model
+
+object Errors {
+    const val INSERT_DATABASE_ERROR = 1
+    const val UPDATE_DATABASE_ERROR = 2
+    const val DELETE_DATABASE_ERROR = 3
+    const val INSERT_SERVER_ERROR = 4
+    const val UPDATE_SERVER_ERROR = 5
+    const val DELETE_SERVER_ERROR = 6
+}
