72fa15767ffdfe67ccb909114083fd934842a0f8
bruno13palhano <bruno13palhano@gmail.com>
2023-12-13 21:31:42 -0300

    Remove unused class

--- core/src/main/java/com/bruno13palhano/core/network/model/ProductCategoriesNet.kt
+++
@@ -1,9 +0,0 @@
-package com.bruno13palhano.core.network.model
-
-import com.bruno13palhano.core.model.Category
-import com.squareup.moshi.Json
-
-data class ProductCategoriesNet(
-    @Json(name = "id") val id: Long,
-    @Json(name = "categories") val categories: List<Category>
-)
