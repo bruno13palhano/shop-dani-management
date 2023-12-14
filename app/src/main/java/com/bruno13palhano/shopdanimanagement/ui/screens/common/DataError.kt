1ffefbed22e9be110ab3bf7da8f504932b3d18e8
bruno13palhano <bruno13palhano@gmail.com>
2023-12-14 12:04:39 -0300

    Get code error from Errors

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/DataError.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/common/DataError.kt
@@ -3,15 +3,34 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.common
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import com.bruno13palhano.core.model.Errors
 import com.bruno13palhano.shopdanimanagement.R
 
 sealed class DataError(val error: Int, @StringRes val resourceId: Int) {
-    object InsertDatabase: DataError(1, R.string.insert_database_error)
-    object UpdateDatabase: DataError(2, R.string.update_database_error)
-    object DeleteDatabase: DataError(3, R.string.delete_database_error)
-    object InsertServer: DataError(4, R.string.insert_server_error)
-    object UpdateServer: DataError(5, R.string.update_server_error)
-    object DeleteServer: DataError(6, R.string.delete_server_error)
+    object InsertDatabase: DataError(
+        error = Errors.INSERT_DATABASE_ERROR,
+        resourceId = R.string.insert_database_error
+    )
+    object UpdateDatabase: DataError(
+        error = Errors.UPDATE_DATABASE_ERROR,
+        resourceId = R.string.update_database_error
+    )
+    object DeleteDatabase: DataError(
+        error = Errors.DELETE_DATABASE_ERROR,
+        resourceId = R.string.delete_database_error
+    )
+    object InsertServer: DataError(
+        error = Errors.INSERT_SERVER_ERROR,
+        resourceId = R.string.insert_server_error
+    )
+    object UpdateServer: DataError(
+        error = Errors.UPDATE_SERVER_ERROR,
+        resourceId = R.string.update_server_error
+    )
+    object DeleteServer: DataError(
+        error = Errors.DELETE_SERVER_ERROR,
+        resourceId = R.string.delete_server_error
+    )
 }
 
 @Composable
