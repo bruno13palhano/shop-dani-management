8f621a60466de4c9b6efa47d2bb92c1f99b1463b
bruno13palhano <bruno13palhano@gmail.com>
2023-12-16 11:26:59 -0300

    Refactor FinancialInfoViewModel

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/FinancialInfoViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/FinancialInfoViewModel.kt
@@ -2,13 +2,10 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bruno13palhano.core.data.repository.delivery.DeliveryRepository
 import com.bruno13palhano.core.data.repository.sale.SaleRepository
-import com.bruno13palhano.core.data.di.DeliveryRep
 import com.bruno13palhano.core.data.di.SaleRep
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
@@ -16,31 +13,27 @@ import javax.inject.Inject
 @HiltViewModel
 class FinancialInfoViewModel @Inject constructor(
     @SaleRep private val saleRepository: SaleRepository,
-    @DeliveryRep private val deliveryRepository: DeliveryRepository
 ) : ViewModel() {
-    val financial = combine(
-        saleRepository.getAll(),
-        deliveryRepository.getAll()
-    ) { sale, deliveries ->
+    val financial = saleRepository.getAll().map {
         var allSalesPurchasePrice = 0F
         var allSales = 0F
         var allDeliveriesPrice = 0F
         var stockSales = 0F
         var ordersSales = 0F
 
-        sale.map {
-            allSales += it.salePrice
-            allSalesPurchasePrice += it.purchasePrice
+        it.map { sale ->
+            allSales += sale.salePrice
+            allSalesPurchasePrice += sale.purchasePrice
 
-            if (it.isOrderedByCustomer) {
-                ordersSales += it.salePrice
+            if (sale.isOrderedByCustomer) {
+                ordersSales += sale.salePrice
             } else {
-                stockSales += it.salePrice
+                stockSales += sale.salePrice
             }
         }
 
-        deliveries.map {
-            allDeliveriesPrice += it.deliveryPrice
+        it.map { sale ->
+            allDeliveriesPrice += sale.deliveryPrice
         }
 
         FinancialInfo(
