ba338f33ed00435eab0fba02de226653dc74ddef
bruno13palhano <bruno13palhano@gmail.com>
2023-12-20 13:21:28 -0300

    Add a interface for User repository

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/user/UserRepository.kt
@@ -0,0 +1,10 @@
+package com.bruno13palhano.core.data.repository.user
+
+import com.bruno13palhano.core.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+    suspend fun create(user: User, onError: (error: Int) -> Unit, onSuccess: (id: Long) -> Unit)
+    suspend fun update(user: User, onError: (error: Int) -> Unit, onSuccess: () -> Unit)
+    fun getById(userId: Long, onError: (error: Int) -> Unit, onSuccess: () -> Unit): Flow<User>
+}
