5186fc3fd1872f39132bbdec45462895b67d064a
bruno13palhano <bruno13palhano@gmail.com>
2023-12-20 13:21:04 -0300

    Add a interface for User data

---
+++ core/src/main/java/com/bruno13palhano/core/data/repository/user/UserData.kt
@@ -0,0 +1,10 @@
+package com.bruno13palhano.core.data.repository.user
+
+import com.bruno13palhano.core.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface UserData {
+    suspend fun insert(user: User, onError: (error: Int) -> Unit, onSuccess: (id: Long) -> Unit)
+    suspend fun update(user: User, onError: (error: Int) -> Unit, onSuccess: () -> Unit)
+    fun getById(userId: Long, onError: (error: Int) -> Unit, onSuccess: () -> Unit): Flow<User>
+}
