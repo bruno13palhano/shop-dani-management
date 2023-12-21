11537b0977428f8834a3bf752d28a97f68b87a7b
bruno13palhano <bruno13palhano@gmail.com>
2023-12-20 22:12:29 -0300

    Add user network retrofit implementation

---
+++ core/src/main/java/com/bruno13palhano/core/network/access/impl/UserNetworkRetrofit.kt
@@ -0,0 +1,17 @@
+package com.bruno13palhano.core.network.access.impl
+
+import com.bruno13palhano.core.model.User
+import com.bruno13palhano.core.network.Service
+import com.bruno13palhano.core.network.access.UserNetwork
+import com.bruno13palhano.core.network.model.asNetwork
+import javax.inject.Inject
+
+internal class UserNetworkRetrofit @Inject constructor(
+    private val apiService: Service
+) : UserNetwork{
+    override suspend fun create(user: User) = apiService.createUser(user = user.asNetwork())
+
+    override suspend fun update(user: User) = apiService.updateUser(user = user.asNetwork())
+
+    override suspend fun login(user: User) = apiService.login(user = user.asNetwork())
+}
