ddbc78b069ecd20daa4cc555995c82b0cc0dc02b
bruno13palhano <bruno13palhano@gmail.com>
2023-12-20 23:04:28 -0300

    Add a login method to UserRepository interface

--- core/src/main/java/com/bruno13palhano/core/data/repository/user/UserRepository.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/user/UserRepository.kt
@@ -4,6 +4,7 @@ import com.bruno13palhano.core.model.User
 import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
+    suspend fun login(user: User, onError: (error: Int) -> Unit, onSuccess: () -> Unit)
     suspend fun create(user: User, onError: (error: Int) -> Unit, onSuccess: (id: Long) -> Unit)
     suspend fun update(user: User, onError: (error: Int) -> Unit, onSuccess: () -> Unit)
     fun getById(userId: Long, onError: (error: Int) -> Unit, onSuccess: () -> Unit): Flow<User>
