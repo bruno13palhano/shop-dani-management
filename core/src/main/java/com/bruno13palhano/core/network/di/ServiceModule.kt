d6dd8e21a52c35e5f213cb9b6fc9a739e6762a15
bruno13palhano <bruno13palhano@gmail.com>
2023-12-24 01:46:05 -0300

    Add interceptors

--- core/src/main/java/com/bruno13palhano/core/network/di/ServiceModule.kt
+++ core/src/main/java/com/bruno13palhano/core/network/di/ServiceModule.kt
@@ -1,15 +1,22 @@
 package com.bruno13palhano.core.network.di
 
+import android.content.Context
+import com.bruno13palhano.core.network.AuthenticatorInterceptor
 import com.bruno13palhano.core.network.Service
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
+
+private const val BASE_URL = "http://10.0.0.111:8080/v1/"
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,16 +24,22 @@ internal object ServiceModule {
 
     @Provides
     @Singleton
-    fun provideApiService(): Service {
-        val BASE_URL = "http://10.0.0.111:8080/v1/"
-
+    fun provideApiService(@ApplicationContext context: Context): Service {
         val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
 
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(AuthenticatorInterceptor(context))
+            .addInterceptor(interceptor)
+            .build()
         val retrofit = Retrofit.Builder()
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .baseUrl(BASE_URL)
+            .client(client)
             .build()
 
         val apiService: Service by lazy { retrofit.create(Service::class.java) }
