ca29b4e20ba5b3be197a2d37b8844daefb1ea620
bruno13palhano <bruno13palhano@gmail.com>
2023-12-24 01:46:51 -0300

    Login return ResponseBody

--- core/src/main/java/com/bruno13palhano/core/network/access/UserNetwork.kt
+++ core/src/main/java/com/bruno13palhano/core/network/access/UserNetwork.kt
@@ -1,9 +1,10 @@
 package com.bruno13palhano.core.network.access
 
 import com.bruno13palhano.core.model.User
+import okhttp3.ResponseBody
 
 interface UserNetwork {
     suspend fun create(user: User)
     suspend fun update(user: User)
-    suspend fun login(user: User)
+    suspend fun login(user: User): ResponseBody
 }
