c2cdbadbfc118b7cec063ce45c8046e27fb83160
bruno13palhano <bruno13palhano@gmail.com>
2023-12-25 20:58:56 -0300

    Add a circular progress component

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/CircularProgress.kt
@@ -0,0 +1,25 @@
+package com.bruno13palhano.shopdanimanagement.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CircularProgress() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.padding(16.dp),
+            strokeWidth = 4.dp
+        )
+    }
+}
