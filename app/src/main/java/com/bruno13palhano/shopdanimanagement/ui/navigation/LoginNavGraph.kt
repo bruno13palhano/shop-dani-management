0b53b2721d62f1fabc30fdfd5a26aa9cac8d88d1
bruno13palhano <bruno13palhano@gmail.com>
2023-12-25 15:01:18 -0300

    Add a navGraph for Login

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/LoginNavGraph.kt
@@ -0,0 +1,42 @@
+package com.bruno13palhano.shopdanimanagement.ui.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.bruno13palhano.shopdanimanagement.ui.screens.login.LoginScreen
+
+fun NavGraphBuilder.loginNavGraph(
+    navController: NavController,
+    showBottomMenu: (show: Boolean) -> Unit,
+    onIconMenuClick: () -> Unit
+) {
+    navigation(
+        startDestination = LoginDestinations.LOGIN_MAIN_ROUTE,
+        route = MainDestinations.LOGIN_ROUTE
+    ) {
+        composable(route = LoginDestinations.LOGIN_MAIN_ROUTE) {
+            showBottomMenu(false)
+            LoginScreen(
+                onSuccess = {
+                    navController.navigate(route = HomeDestinations.HOME_MAIN_ROUTE) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            inclusive = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+        composable(route = LoginDestinations.LOGIN_CREATE_ACCOUNT_ROUTE) {
+            showBottomMenu(false)
+        }
+    }
+}
+
+object LoginDestinations {
+    const val LOGIN_MAIN_ROUTE = "login_main_route"
+    const val LOGIN_CREATE_ACCOUNT_ROUTE = "login_create_account_route"
+}
