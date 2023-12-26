e0c3c5394cb2369d02399a7376a5a2e477f9c333
bruno13palhano <bruno13palhano@gmail.com>
2023-12-26 15:30:20 -0300

    Initialize the work only if user is authenticated

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/dependencies/DependenciesApplication.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/dependencies/DependenciesApplication.kt
@@ -3,6 +3,8 @@ package com.bruno13palhano.shopdanimanagement.dependencies
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.bruno13palhano.core.data.di.UserRep
+import com.bruno13palhano.core.data.repository.user.UserRepository
 import com.bruno13palhano.core.sync.Sync
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -12,6 +14,10 @@ class DependenciesApplication : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
+    @Inject
+    @UserRep
+    lateinit var userRepository: UserRepository
+
     override fun getWorkManagerConfiguration() =
         Configuration.Builder()
             .setWorkerFactory(workerFactory)
@@ -19,6 +25,8 @@ class DependenciesApplication : Application(), Configuration.Provider {
 
     override fun onCreate() {
         super.onCreate()
-        Sync.initialize(this)
+
+        if (userRepository.isAuthenticated())
+            Sync.initialize(this)
     }
 }
