a4a7780d1506a52eda231206fbdc2d01dc568a53
bruno13palhano <bruno13palhano@gmail.com>
2023-12-25 21:01:20 -0300

    Add a sealed class to wrapper login state

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/login/LoginState.kt
@@ -0,0 +1,7 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.login
+
+sealed class LoginState {
+    object SignedOut: LoginState()
+    object InProgress: LoginState()
+    object SignedIn: LoginState()
+}
