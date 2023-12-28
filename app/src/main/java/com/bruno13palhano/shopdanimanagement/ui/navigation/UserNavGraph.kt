cf3310480dfe733e6abd8c3a3fbc23d71ac6d350
bruno13palhano <bruno13palhano@gmail.com>
2023-12-28 15:10:58 -0300

    Remove unused import

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/UserNavGraph.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/UserNavGraph.kt
@@ -1,7 +1,6 @@
 package com.bruno13palhano.shopdanimanagement.ui.navigation
 
 import androidx.navigation.NavController
-import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
