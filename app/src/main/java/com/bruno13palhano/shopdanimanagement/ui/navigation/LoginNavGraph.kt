a990548dfd4d367f7b2106c4cd407419e73339de
bruno13palhano <bruno13palhano@gmail.com>
2023-12-28 15:10:06 -0300

    Refactor loginNavGraph

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/LoginNavGraph.kt
@@ -0,0 +1,41 @@
+package com.bruno13palhano.shopdanimanagement.ui.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.bruno13palhano.shopdanimanagement.ui.screens.login.LoginScreen
+
+fun NavGraphBuilder.loginNavGraph(
+    navController: NavController,
+    showBottomMenu: (show: Boolean) -> Unit,
+    gesturesEnabled: (enabled: Boolean) -> Unit
+) {
+    navigation(
+        startDestination = LoginDestinations.LOGIN_MAIN_ROUTE,
+        route = MainDestinations.LOGIN_ROUTE
+    ) {
+        composable(route = LoginDestinations.LOGIN_MAIN_ROUTE) {
+            showBottomMenu(false)
+            gesturesEnabled(false)
+            LoginScreen(
+                onSuccess = {
+                    navController.navigate(route = HomeDestinations.HOME_MAIN_ROUTE) {
+                        popUpTo(0)
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+        composable(route = LoginDestinations.LOGIN_CREATE_ACCOUNT_ROUTE) {
+            showBottomMenu(false)
+            gesturesEnabled(false)
+        }
+    }
+}
+
+object LoginDestinations {
+    const val LOGIN_MAIN_ROUTE = "login_main_route"
+    const val LOGIN_CREATE_ACCOUNT_ROUTE = "login_create_account_route"
+}
