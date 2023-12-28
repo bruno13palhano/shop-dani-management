75deb2d6a20ea0e5f2705f036f01f0a090bb59db
bruno13palhano <bruno13palhano@gmail.com>
2023-12-28 15:02:54 -0300

    Add a screen for user info

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/user/UserScreen.kt
@@ -0,0 +1,104 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.user
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.bruno13palhano.shopdanimanagement.R
+import com.bruno13palhano.shopdanimanagement.ui.components.MoreOptionsMenu
+
+@Composable
+fun UserScreen(
+    onLogout: () -> Unit,
+    navigateUp: () -> Unit
+) {
+    val menuItems = arrayOf(
+        stringResource(id = R.string.logout_label)
+    )
+
+    UserContent(
+        menuItems = menuItems,
+        onMoreOptionsItemClick = { index ->
+            when (index) {
+                0 -> {
+                    onLogout()
+                }
+                else -> {}
+            }
+
+        },
+        navigateUp = navigateUp
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserContent(
+    menuItems: Array<String>,
+    onMoreOptionsItemClick: (index: Int) -> Unit,
+    navigateUp: () -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.user_label)) },
+                navigationIcon = {
+                    IconButton(onClick = navigateUp) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.up_button_label)
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = true }) {
+                        Box(contentAlignment = Alignment.Center) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = stringResource(id = R.string.drawer_menu_label)
+                            )
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                MoreOptionsMenu(
+                                    items = menuItems,
+                                    expanded = expanded,
+                                    onDismissRequest = { expandedValue ->
+                                        expanded = expandedValue
+                                    },
+                                    onClick = onMoreOptionsItemClick
+                                )
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+
+        }
+    }
+}
