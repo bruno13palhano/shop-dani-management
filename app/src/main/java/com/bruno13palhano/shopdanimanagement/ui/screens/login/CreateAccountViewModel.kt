f97671799bd87d8d14dc581e4d37b0eb278db809
bruno13palhano <bruno13palhano@gmail.com>
2023-12-29 20:57:40 -0300

    Add a viewmodel for CreateAccountScreen

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/login/CreateAccountViewModel.kt
@@ -0,0 +1,88 @@
+package com.bruno13palhano.shopdanimanagement.ui.screens.login
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.di.UserRep
+import com.bruno13palhano.core.data.repository.user.UserRepository
+import com.bruno13palhano.core.model.User
+import com.bruno13palhano.shopdanimanagement.ui.screens.getCurrentTimestamp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateAccountViewModel @Inject constructor(
+    @UserRep private val userRepository: UserRepository
+) : ViewModel() {
+    var photo by mutableStateOf(byteArrayOf())
+        private set
+    var username by mutableStateOf("")
+        private set
+    var email by mutableStateOf("")
+        private set
+    var password by mutableStateOf("")
+        private set
+    var repeatPassword by mutableStateOf("")
+        private set
+
+    val isValid = snapshotFlow {
+        username.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()
+                && repeatPassword.isNotEmpty() && (password == repeatPassword)
+    }.stateIn(
+        scope = viewModelScope,
+        started = WhileSubscribed(5_000),
+        initialValue = false
+    )
+
+    fun updatePhoto(photo: ByteArray) {
+        this.photo = photo
+    }
+
+    fun updateUsername(username: String) {
+        this.username = username
+    }
+
+    fun updateEmail(email: String) {
+        this.email = email
+    }
+
+    fun updatePassword(password: String) {
+        this.password = password
+    }
+
+    fun updateRepeatPassword(repeatPassword: String) {
+        this.repeatPassword = repeatPassword
+    }
+
+    fun createAccount(
+        onError: (error: Int) -> Unit,
+        onSuccess: () -> Unit
+    ) {
+        val user = User(
+            id = 0L,
+            username = username,
+            email = email,
+            password = password,
+            photo = photo,
+            role = "",
+            enabled = true,
+            timestamp = getCurrentTimestamp()
+        )
+        viewModelScope.launch {
+            userRepository.create(
+                user = user,
+                onError =  {
+                    onError(it)
+                },
+            ) {
+                onSuccess()
+            }
+        }
+    }
+}
