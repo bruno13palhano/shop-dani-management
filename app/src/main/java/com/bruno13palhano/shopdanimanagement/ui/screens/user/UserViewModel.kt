41c6a62d8221c85cdbed4494551b388cfcdfe8a9
bruno13palhano <bruno13palhano@gmail.com>
2023-12-31 15:53:54 -0300

    Add user properties

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/user/UserViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/user/UserViewModel.kt
@@ -1,5 +1,8 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.user
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.bruno13palhano.core.data.di.UserRep
 import com.bruno13palhano.core.data.repository.user.UserRepository
@@ -10,5 +13,32 @@ import javax.inject.Inject
 class UserViewModel @Inject constructor(
     @UserRep private val userRepository: UserRepository
 ): ViewModel() {
+    var photo by mutableStateOf(byteArrayOf())
+        private set
+    var username by mutableStateOf("")
+        private set
+    var email by mutableStateOf("")
+        private set
+    var role by mutableStateOf("")
+        private set
 
+    fun updatePhoto(photo: ByteArray) {
+        this.photo = photo
+    }
+
+    fun updateUsername(username: String) {
+        this.username = username
+    }
+
+    fun updateEmail(email: String) {
+        this.email = email
+    }
+
+    fun updateRole(role: String) {
+        this.role = role
+    }
+
+    fun updateUser() {
+
+    }
 }
