5a035813997966bf8aeb352ef21e30abdf2d8409
bruno13palhano <bruno13palhano@gmail.com>
2023-12-31 11:20:27 -0300

    Add vertical scroll to LoginScreen

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/login/LoginScreen.kt
@@ -1,11 +1,14 @@
 package com.bruno13palhano.shopdanimanagement.ui.screens.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Key
@@ -147,7 +150,11 @@ fun LoginContent(
             }
         }
     ) {
-        Column(modifier = Modifier.padding(it)) {
+        Column(modifier = Modifier
+            .padding(it)
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState())
+        ) {
             OutlinedTextField(
                 modifier = Modifier
                     .padding(horizontal = 8.dp, vertical = 2.dp)
