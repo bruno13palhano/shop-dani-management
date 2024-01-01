f7cd0de237416c7c9584086ccacfc87d5dd7ead6
bruno13palhano <bruno13palhano@gmail.com>
2024-01-01 02:06:29 -0300

    Get current user

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/user/UserViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/user/UserViewModel.kt
@@ -4,15 +4,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bruno13palhano.core.data.di.UserRep
 import com.bruno13palhano.core.data.repository.user.UserRepository
+import com.bruno13palhano.core.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor(
     @UserRep private val userRepository: UserRepository
 ): ViewModel() {
+    private var userId = 0L
     var photo by mutableStateOf(byteArrayOf())
         private set
     var username by mutableStateOf("")
@@ -36,6 +42,18 @@ class UserViewModel @Inject constructor(
 
     fun updateRole(role: String) {
         this.role = role
+    }
+
+    fun getCurrentUser() {
+        viewModelScope.launch {
+            userRepository.getCurrentUser(onError = {}, onSuccess = {}).collect {
+                userId = it.id
+                photo = it.photo
+                username = it.username
+                email = it.email
+                role = it.role
+            }
+        }
     }
 
     fun updateUser() {
