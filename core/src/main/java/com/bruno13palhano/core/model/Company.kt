26f302c80296bb37a566b9da462614ade39c5299
bruno13palhano <bruno13palhano@gmail.com>
2024-01-10 21:43:53 -0300

    Add new companies for Company

--- core/src/main/java/com/bruno13palhano/core/model/Company.kt
+++ core/src/main/java/com/bruno13palhano/core/model/Company.kt
@@ -3,5 +3,8 @@ package com.bruno13palhano.core.model
 enum class Company(val company: String) {
     AVON("Avon"),
     NATURA("Natura"),
-    BOTICARIO("Boticário")
+    BOTICARIO("O Boticário"),
+    EUDORA("Eudora"),
+    BERENICE("Quem disse Berenice?"),
+    OUI("O.U.I")
 }
