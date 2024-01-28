a26a37473a76232f6e3e1acf2ab4a7bb08c9d132
bruno13palhano <bruno13palhano@gmail.com>
2024-01-27 21:31:05 -0300

    Add QRCodeReader

---
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/components/QRCodeReader.kt
@@ -0,0 +1,143 @@
+package com.bruno13palhano.shopdanimanagement.ui.components
+
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
+import com.bruno13palhano.shopdanimanagement.R
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.util.concurrent.Executors
+
+@androidx.annotation.OptIn(ExperimentalGetImage::class)
+@Composable
+fun QRCodeReader() {
+    val context = LocalContext.current
+    val previewView: PreviewView = remember { PreviewView(context) }
+    val cameraController = remember { LifecycleCameraController(context) }
+    val lifecycleOwner = LocalLifecycleOwner.current
+    cameraController.bindToLifecycle(lifecycleOwner)
+    cameraController.cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    previewView.controller = cameraController
+
+    val executor = remember { Executors.newSingleThreadExecutor() }
+    val textRecognizer = remember {
+        TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    }
+
+    var text by rememberSaveable { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    CameraPermission()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(
+            factory = { previewView },
+            modifier = Modifier.fillMaxSize()
+        )
+
+        if (isLoading) {
+            CircularProgress()
+        } else {
+            IconButton(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp),
+                onClick = {
+                    isLoading = true
+                    cameraController.setImageAnalysisAnalyzer(executor) { imageProxy ->
+                        imageProxy.image?.let { image ->
+                            val img = InputImage.fromMediaImage(
+                                image,
+                                imageProxy.imageInfo.rotationDegrees
+                            )
+
+                            textRecognizer.process(img).addOnCompleteListener { task ->
+                                isLoading = false
+                                text =
+                                    if (!task.isSuccessful) task.exception!!.localizedMessage?.toString()
+                                        .toString()
+                                    else task.result.text
+
+                                cameraController.clearImageAnalysisAnalyzer()
+                                imageProxy.close()
+                            }
+                        }
+                    }
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Camera,
+                    contentDescription = "Camera"
+                )
+            }
+        }
+    }
+
+    if (text.isNotEmpty()) {
+        Dialog(onDismissRequest = { text = "" }) {
+            Card(modifier = Modifier.fillMaxWidth(0.8f)) {
+                Text(
+                    text = text,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 16.dp),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Button(
+                    onClick = { text = "" },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(horizontal = 16.dp, vertical = 16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.done_label))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun CameraPermission() {
+    val cameraPermissionState = rememberPermissionState(
+        android.Manifest.permission.CAMERA
+    )
+
+    if (!cameraPermissionState.status.isGranted) {
+        LaunchedEffect(key1 = Unit) {
+            cameraPermissionState.launchPermissionRequest()
+        }
+    }
+}
