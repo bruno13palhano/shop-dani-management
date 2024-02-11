4a9a67e3d88be1444b4cf1674e7d14ae1c78e467
bruno13palhano <bruno13palhano@gmail.com>
2024-02-11 20:56:29 -0300

    Get FinancialInfo from FinancialUseCase

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/FinancialInfoViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/financial/viewmodel/FinancialInfoViewModel.kt
@@ -2,8 +2,9 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.financial.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bruno13palhano.core.data.repository.sale.SaleRepository
-import com.bruno13palhano.core.data.di.SaleRep
+import com.bruno13palhano.core.data.di.Financial
+import com.bruno13palhano.core.data.domain.FinancialUseCase
+import com.bruno13palhano.core.model.FinancialInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
 import kotlinx.coroutines.flow.map
@@ -12,37 +13,9 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FinancialInfoViewModel @Inject constructor(
-    @SaleRep private val saleRepository: SaleRepository,
+    @Financial private val financialUseCase: FinancialUseCase
 ) : ViewModel() {
-    val financial = saleRepository.getAll().map {
-        var allSalesPurchasePrice = 0F
-        var allSales = 0F
-        var allDeliveriesPrice = 0F
-        var stockSales = 0F
-        var ordersSales = 0F
-
-        it.map { sale ->
-            allSales += sale.salePrice
-            allSalesPurchasePrice += sale.purchasePrice
-
-            if (sale.isOrderedByCustomer) {
-                ordersSales += sale.salePrice
-            } else {
-                stockSales += sale.salePrice
-            }
-        }
-
-        it.map { sale ->
-            allDeliveriesPrice += sale.deliveryPrice
-        }
-
-        FinancialInfo(
-            allSales = allSales,
-            stockSales = stockSales,
-            ordersSales = ordersSales,
-            profit = allSales - (allSalesPurchasePrice + allDeliveriesPrice),
-        )
-    }
+    val financial = financialUseCase()
         .stateIn(
             scope = viewModelScope,
             started = WhileSubscribed(5_000),
@@ -63,13 +36,6 @@ class FinancialInfoViewModel @Inject constructor(
             started = WhileSubscribed(5_000),
             initialValue = FinancialChartEntries()
         )
-
-    data class FinancialInfo(
-        val allSales: Float = 0F,
-        val stockSales: Float = 0F,
-        val ordersSales: Float = 0F,
-        val profit: Float = 0F,
-    )
 
     data class FinancialChartEntries(
         val allSalesEntries: Pair<Float, Float> = Pair(0F, 0F),
