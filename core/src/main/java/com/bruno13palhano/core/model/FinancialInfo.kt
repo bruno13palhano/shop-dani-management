d888cab0b51374d42595159f479000f2f7530e1b
bruno13palhano <bruno13palhano@gmail.com>
2024-02-11 20:52:46 -0300

    Move FinancialInfo class to core module

---
+++ core/src/main/java/com/bruno13palhano/core/model/FinancialInfo.kt
@@ -0,0 +1,8 @@
+package com.bruno13palhano.core.model
+
+data class FinancialInfo(
+    val allSales: Float = 0F,
+    val stockSales: Float = 0F,
+    val ordersSales: Float = 0F,
+    val profit: Float = 0F
+)
