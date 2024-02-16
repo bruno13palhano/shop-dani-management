d9125a859e9900fc2f8c14b2a6d66adf1bf5b365
bruno13palhano <bruno13palhano@gmail.com>
2024-02-16 20:24:51 -0300

    Add SaleInfoUseCase

---
+++ core/src/main/java/com/bruno13palhano/core/data/domain/SaleInfoUseCase.kt
@@ -0,0 +1,40 @@
+package com.bruno13palhano.core.data.domain
+
+import com.bruno13palhano.core.data.di.CustomerRep
+import com.bruno13palhano.core.data.di.SaleRep
+import com.bruno13palhano.core.data.repository.customer.CustomerRepository
+import com.bruno13palhano.core.data.repository.sale.SaleRepository
+import com.bruno13palhano.core.model.SaleInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import javax.inject.Inject
+
+class SaleInfoUseCase @Inject constructor(
+    @SaleRep private val saleRepository: SaleRepository,
+    @CustomerRep private val customerRepository: CustomerRepository
+) {
+    operator fun invoke(saleId: Long, customerId: Long): Flow<SaleInfo> {
+        return combine(
+            saleRepository.getById(id = saleId),
+            customerRepository.getById(id = customerId)
+        ) { sale, customer ->
+            if (sale.customerId == customer.id) {
+                SaleInfo(
+                    saleId = sale.id,
+                    customerId = customer.id,
+                    productName = sale.name,
+                    customerName = customer.name,
+                    productPhoto = sale.photo,
+                    customerPhoto = customer.photo,
+                    address = customer.address,
+                    phoneNumber = customer.phoneNumber,
+                    email = customer.email,
+                    salePrice = sale.salePrice,
+                    deliveryPrice = sale.deliveryPrice,
+                    quantity = sale.quantity,
+                    dateOfSale = sale.dateOfSale
+                )
+            } else { SaleInfo.emptySaleInfo() }
+        }
+    }
+}
