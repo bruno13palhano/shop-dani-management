a5fa8e64e2d0a789d2e49cd7115011964445e7b4
bruno13palhano <bruno13palhano@gmail.com>
2024-02-19 19:28:16 -0300

    Add properties for CustomerInfoChart

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomersViewModel.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/customers/viewmodel/CustomersViewModel.kt
@@ -2,10 +2,15 @@ package com.bruno13palhano.shopdanimanagement.ui.screens.customers.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.core.data.di.CustomerInformation
 import com.bruno13palhano.core.data.repository.customer.CustomerRepository
 import com.bruno13palhano.core.data.di.CustomerRep
+import com.bruno13palhano.core.data.domain.CustomerInfoUseCase
 import com.bruno13palhano.core.model.Customer
+import com.bruno13palhano.core.model.CustomerInfo
 import com.bruno13palhano.shopdanimanagement.ui.screens.common.CommonItem
+import com.bruno13palhano.shopdanimanagement.ui.screens.setChartEntries
+import com.bruno13palhano.shopdanimanagement.ui.screens.setQuantity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
@@ -16,7 +21,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CustomersViewModel @Inject constructor(
-    @CustomerRep private val customersRepository: CustomerRepository
+    @CustomerRep private val customersRepository: CustomerRepository,
+    @CustomerInformation private val customerInfoUseCase: CustomerInfoUseCase
 ): ViewModel() {
     private val _customerList = MutableStateFlow(emptyList<Customer>())
     val customerList = _customerList
@@ -35,6 +41,22 @@ class CustomersViewModel @Inject constructor(
             scope = viewModelScope,
             started = WhileSubscribed(5_000),
             initialValue = emptyList()
+        )
+
+    private var _customerInfo = MutableStateFlow(CustomerInfo.emptyCustomerInfo())
+    val customerInfo = _customerInfo
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = CustomerInfo.emptyCustomerInfo()
+        )
+
+    private val _entry = MutableStateFlow(listOf<Pair<String, Float>>())
+    val entry = _entry
+        .stateIn(
+            scope = viewModelScope,
+            started = WhileSubscribed(5_000),
+            initialValue = listOf()
         )
 
     fun getAllCustomers() {
@@ -58,6 +80,32 @@ class CustomersViewModel @Inject constructor(
             customersRepository.getOrderedByAddress(isOrderedAsc = isOrderedAsc).collect {
                 _customerList.value = it
             }
+        }
+    }
+
+    fun getCustomerInfo(customerId: Long) {
+        viewModelScope.launch {
+            customerInfoUseCase.getCustomerInfo(customerId = customerId).collect {
+                _customerInfo.value = it
+            }
+        }
+    }
+
+    fun getCustomerPurchases(customerId: Long) {
+        val days = Array(31) { 0 }
+        val chartEntries = mutableListOf<Pair<String, Float>>()
+
+        viewModelScope.launch {
+            customerInfoUseCase.getCustomerSales(customerId = customerId)
+                .map {
+                    it.map { sale -> setQuantity(days, sale.dateOfSale, sale.quantity) }
+                    setChartEntries(chartEntries, days)
+
+                    chartEntries
+                }
+                .collect {
+                    _entry.value = it
+                }
         }
     }
 }
