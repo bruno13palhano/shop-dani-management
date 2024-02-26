6e8b00a253d9d045e8d107692e4ad98dd5385ca4
bruno13palhano <bruno13palhano@gmail.com>
2024-02-26 17:54:00 -0300

    Add headers and map for Amazon spreadsheet

--- core/src/main/java/com/bruno13palhano/core/data/repository/ExcelSheetHelper.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/ExcelSheetHelper.kt
@@ -12,6 +12,21 @@ internal val salesSheetHeaders = listOf(
     "Delivery Price"
 )
 
+internal val amazonSalesSheetHeaders = listOf(
+    "Code",
+    "Request Number",
+    "Product",
+    "Customer",
+    "Company",
+    "Purchase Price",
+    "Amazon Price",
+    "Amazon Tax",
+    "Amazon Profit",
+    "Amazon SKU",
+    "Resale Profit",
+    "Total Profit"
+)
+
 internal fun mapSalesToSheet(sale: Sale): List<String> {
     return listOf(
         sale.customerName,
@@ -21,5 +36,22 @@ internal fun mapSalesToSheet(sale: Sale): List<String> {
         sale.purchasePrice.toString(),
         sale.salePrice.toString(),
         sale.deliveryPrice.toString()
+    )
+}
+
+internal fun mapAmazonSalesToSheet(sale: Sale): List<String> {
+    return listOf(
+        sale.amazonCode,
+        sale.amazonRequestNumber.toString(),
+        sale.name,
+        sale.customerName,
+        sale.company,
+        sale.purchasePrice.toString(),
+        sale.salePrice.toString(),
+        sale.amazonTax.toString(),
+        sale.amazonProfit.toString(),
+        sale.amazonSKU,
+        sale.resaleProfit.toString(),
+        sale.totalProfit.toString()
     )
 }
