fdf5b9fb4e16dc80bcd7f27ddf5477050e69331a
bruno13palhano <bruno13palhano@gmail.com>
2024-02-27 13:55:33 -0300

    Remove unused import

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/amazon/AmazonScreen.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/screens/amazon/AmazonScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.QrCodeScanner
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
