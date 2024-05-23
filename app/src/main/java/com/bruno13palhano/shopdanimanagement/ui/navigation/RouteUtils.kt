bb50d41c16621eda46708104a9871145648377e7
bruno13palhano <bruno13palhano@gmail.com>
2024-05-23 12:35:36 -0300

    Added new constants

--- app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/RouteUtils.kt
+++ app/src/main/java/com/bruno13palhano/shopdanimanagement/ui/navigation/RouteUtils.kt
@@ -3,3 +3,5 @@ package com.bruno13palhano.shopdanimanagement.ui.navigation
 const val ITEM_ID = "item_Id"
 const val EDITABLE = "item_editable"
 const val IS_ORDERED = "is_ordered"
+const val PRODUCT_ID = "product_id"
+const val SALE_ID = "sale_id"
