5c5e84bd13ee7aa31c1d33f0fe83dd02cfc0c8a1
bruno13palhano <bruno13palhano@gmail.com>
2024-05-29 14:52:41 -0300

    Remove the dependency between UserNetwork interface and Response class from Retrofit

--- core/src/main/java/com/bruno13palhano/core/network/access/UserNetwork.kt
+++ core/src/main/java/com/bruno13palhano/core/network/access/UserNetwork.kt
@@ -1,18 +1,33 @@
 package com.bruno13palhano.core.network.access
 
 import com.bruno13palhano.core.network.model.UserNet
-import retrofit2.Response
 
 interface UserNetwork {
-    suspend fun create(user: UserNet): Response<UserNet>
+    suspend fun create(
+        user: UserNet,
+        onError: (error: Int) -> Unit,
+        onSuccess: (userNet: UserNet) -> Unit,
+    )
 
-    suspend fun update(user: UserNet): Response<Int>
+    suspend fun update(
+        user: UserNet,
+        onError: (error: Int) -> Unit,
+        onSuccess: () -> Unit,
+    )
 
-    suspend fun login(user: UserNet): Response<Unit>
+    suspend fun login(
+        user: UserNet,
+        onError: (error: Int) -> Unit,
+        onSuccess: (token: String) -> Unit,
+    )
 
     suspend fun authenticated(token: String): Boolean
 
     suspend fun getByUsername(username: String): UserNet
 
-    suspend fun updateUserPassword(user: UserNet): Response<Int>
+    suspend fun updateUserPassword(
+        user: UserNet,
+        onError: (error: Int) -> Unit,
+        onSuccess: () -> Unit,
+    )
 }
