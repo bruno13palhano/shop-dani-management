8c4c418aab147bd42986ef53e2703dee6c38a1f8
bruno13palhano <bruno13palhano@gmail.com>
2024-05-31 16:25:34 -0300

    Add binds to inject NetworkFirebase implementation dependencies

--- core/src/main/java/com/bruno13palhano/core/network/di/NetworkModule.kt
+++ core/src/main/java/com/bruno13palhano/core/network/di/NetworkModule.kt
@@ -16,8 +16,20 @@ import com.bruno13palhano.core.network.access.impl.ProductNetworkRetrofit
 import com.bruno13palhano.core.network.access.impl.SaleNetworkRetrofit
 import com.bruno13palhano.core.network.access.impl.StockNetworkRetrofit
 import com.bruno13palhano.core.network.access.impl.UserNetworkRetrofit
+import com.bruno13palhano.core.network.access.impl.firebase.CatalogNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.CategoryNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.CustomerNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.DataVersionNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.ProductNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.SaleNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.StockNetworkFirebase
+import com.bruno13palhano.core.network.access.impl.firebase.UserNetworkFirebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Qualifier
@@ -27,25 +39,49 @@ import javax.inject.Singleton
 internal annotation class DefaultUserNet
 
 @Qualifier
+internal annotation class FirebaseUserNet
+
+@Qualifier
 internal annotation class DefaultCatalogNet
+
+@Qualifier
+internal annotation class FirebaseCatalogNet
 
 @Qualifier
 internal annotation class DefaultCategoryNet
 
 @Qualifier
+internal annotation class FirebaseCategoryNet
+
+@Qualifier
 internal annotation class DefaultCustomerNet
+
+@Qualifier
+internal annotation class FirebaseCustomerNet
 
 @Qualifier
 internal annotation class DefaultProductNet
 
 @Qualifier
+internal annotation class FirebaseProductNet
+
+@Qualifier
 internal annotation class DefaultSaleNet
+
+@Qualifier
+internal annotation class FirebaseSaleNet
 
 @Qualifier
 internal annotation class DefaultStockNet
 
 @Qualifier
+internal annotation class FirebaseStockNet
+
+@Qualifier
 internal annotation class DefaultVersionNet
+
+@Qualifier
+internal annotation class FirebaseVersionNet
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -55,38 +91,97 @@ internal abstract class NetworkModule {
     @Binds
     abstract fun bindUserNetwork(network: UserNetworkRetrofit): UserNetwork
 
+    @FirebaseUserNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseUserNetwork(network: UserNetworkFirebase): UserNetwork
+
     @DefaultCatalogNet
     @Singleton
     @Binds
     abstract fun bindCatalogNetwork(network: CatalogNetworkRetrofit): CatalogNetwork
+
+    @FirebaseCatalogNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseCatalogNetwork(network: CatalogNetworkFirebase): CatalogNetwork
 
     @DefaultCategoryNet
     @Singleton
     @Binds
     abstract fun bindCategoryNetwork(network: CategoryNetworkRetrofit): CategoryNetwork
 
+    @FirebaseCategoryNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseCategoryNetwork(network: CategoryNetworkFirebase): CategoryNetwork
+
     @DefaultCustomerNet
     @Singleton
     @Binds
     abstract fun bindCustomerNetwork(network: CustomerNetworkRetrofit): CustomerNetwork
+
+    @FirebaseCustomerNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseCustomerNetwork(network: CustomerNetworkFirebase): CustomerNetwork
 
     @DefaultProductNet
     @Singleton
     @Binds
     abstract fun bindProductNetwork(network: ProductNetworkRetrofit): ProductNetwork
 
+    @FirebaseProductNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseProductNetwork(network: ProductNetworkFirebase): ProductNetwork
+
     @DefaultSaleNet
     @Singleton
     @Binds
     abstract fun bindSaleNetwork(network: SaleNetworkRetrofit): SaleNetwork
+
+    @FirebaseSaleNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseSaleNetwork(network: SaleNetworkFirebase): SaleNetwork
 
     @DefaultStockNet
     @Singleton
     @Binds
     abstract fun bindStockNetwork(network: StockNetworkRetrofit): StockNetwork
 
+    @FirebaseStockNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseStockNetwork(network: StockNetworkFirebase): StockNetwork
+
     @DefaultVersionNet
     @Singleton
     @Binds
     abstract fun bindVersionNetwork(network: DataVersionNetworkRetrofit): VersionNetwork
+
+    @FirebaseVersionNet
+    @Singleton
+    @Binds
+    abstract fun bindFirebaseVersionNetwork(network: DataVersionNetworkFirebase): VersionNetwork
+}
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal object FirebaseProvidersModule {
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth {
+        return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Provides
+    fun provideFirebaseStorage(): FirebaseStorage {
+        return FirebaseStorage.getInstance()
+    }
 }
