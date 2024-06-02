a2af9217250de77bac26cf4876225531ca380dea
bruno13palhano <bruno13palhano@gmail.com>
2024-06-02 19:57:48 -0300

    Remnove interfaces

--- core/src/main/java/com/bruno13palhano/core/model/Customer.kt
+++ core/src/main/java/com/bruno13palhano/core/model/Customer.kt
@@ -11,21 +11,7 @@ data class Customer(
     val gender: String,
     val age: Int,
     override val timestamp: String,
-) : Model(id = id, timestamp = timestamp), Buyable {
-    override fun buy(
-        salable: Salable,
-        purchase: Purchase,
-    ): Receipt {
-        return salable.sell(purchase = purchase)
-    }
-
-    override fun cancel(
-        reversible: Reversible,
-        purchase: Purchase,
-    ): Receipt {
-        return reversible.reverse(purchase = purchase)
-    }
-
+) : Model(id = id, timestamp = timestamp) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
