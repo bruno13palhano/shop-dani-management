093141f339d3cf2bc4d2409a21afd9310e61eaad
bruno13palhano <bruno13palhano@gmail.com>
2024-06-02 21:41:36 -0300

    Remove all new classes

--- core/src/main/java/com/bruno13palhano/core/model/Reversible.kt
+++
@@ -1,5 +0,0 @@
-package com.bruno13palhano.core.model
-
-interface Reversible {
-    fun reverse(purchase: Purchase): Receipt
-}
--- core/src/main/java/com/bruno13palhano/core/model/Salable.kt
+++
@@ -1,5 +0,0 @@
-package com.bruno13palhano.core.model
-
-interface Salable {
-    fun sell(purchase: Purchase): Receipt
-}
